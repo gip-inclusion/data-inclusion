ee8e0eba3b407003d43a26fe236d7306ab0774bf
Valentin Matton <taq@protonmail.ch>
2022-10-13 10:01:13 +0200

    feat: ajout sous-thématiques

--- src/data_inclusion/api/schema.py
+++ src/data_inclusion/api/schema.py
@@ -285,11 +285,120 @@ class LabelNational(str, Enum):
 
 class Thematique(str, Enum):
     CHOISIR_UN_METIER = "choisir-un-metier"
+    CHOISIR_UN_METIER_IDENTIFIER_SES_POINTS_FORTS_ET_SES_COMPETENCES = (
+        "choisir-un-metier-identifier-ses-points-forts-et-ses-competences"
+    )
+    CHOISIR_UN_METIER_CONNAITRE_LES_OPPORTUNITES_DEMPLOI = (
+        "choisir-un-metier-connaitre-les-opportunites-demploi"
+    )
+    CHOISIR_UN_METIER_DECOUVRIR_UN_METIER_OU_UN_SECTEUR_DACTIVITE = (
+        "choisir-un-metier-decouvrir-un-metier-ou-un-secteur-dactivite"
+    )
+    CHOISIR_UN_METIER_CONFIRMER_SON_CHOIX_DE_METIER = (
+        "choisir-un-metier-confirmer-son-choix-de-metier"
+    )
+
     CREATION_ACTIVITE = "creation-activite"
+    CREATION_ACTIVITE_DE_L_IDEE_AU_PROJET = "creation-activite-de-l-idee-au-projet"
+    CREATION_ACTIVITE_DEMARRER_SON_ACTIVITE = "creation-activite-demarrer-son-activite"
+    CREATION_ACTIVITE_ELABORER_SON_PROJET = "creation-activite-elaborer-son-projet"
+    CREATION_ACTIVITE_RESEAUTAGE_POUR_CREATEURS_DENTREPRISE = (
+        "creation-activite-reseautage-pour-createurs-dentreprise"
+    )
+    CREATION_ACTIVITE_FINANCER_SON_PROJET = "creation-activite-financer-son-projet"
+
     MOBILITE = "mobilite"
+    MOBILITE_ETRE_ACCOMPAGNE_DANS_SON_PARCOURS_MOBILITE = (
+        "mobilite-etre-accompagne-dans-son-parcours-mobilite"
+    )
+    MOBILITE_ENTRETENIR_REPARER_SON_VEHICULE = (
+        "mobilite-entretenir-reparer-son-vehicule"
+    )
+    MOBILITE_LOUER_UN_VEHICULE = "mobilite-louer-un-vehicule"
+    MOBILITE_ACHETER_UN_VEHICULE_MOTORISE = "mobilite-acheter-un-vehicule-motorise"
+    MOBILITE_PREPARER_SON_PERMIS_DE_CONDUIRE_SE_REENTRAINER_A_LA_CONDUITE = (
+        "mobilite-preparer-son-permis-de-conduire-se-reentrainer-a-la-conduite"
+    )
+    MOBILITE_AIDES_A_LA_REPRISE_DEMPLOI_OU_A_LA_FORMATION = (
+        "mobilite-aides-a-la-reprise-demploi-ou-a-la-formation"
+    )
+    MOBILITE_COMPRENDRE_ET_UTILISER_LES_TRANSPORTS_EN_COMMUN = (
+        "mobilite-comprendre-et-utiliser-les-transports-en-commun"
+    )
+    MOBILITE_APPRENDRE_A_UTILISER_UN_DEUX_ROUES = (
+        "mobilite-apprendre-a-utiliser-un-deux-roues"
+    )
+    MOBILITE_FINANCER_MON_PROJET_MOBILITE = "mobilite-financer-mon-projet-mobilite"
+    MOBILITE_ACHETER_UN_VELO = "mobilite-acheter-un-velo"
+
     NUMERIQUE = "numerique"
+    NUMERIQUE_DEVENIR_AUTONOME_DANS_LES_DEMARCHES_ADMINISTRATIVES = (
+        "numerique-devenir-autonome-dans-les-demarches-administratives"
+    )
+    NUMERIQUE_REALISER_DES_DEMARCHES_ADMINISTRATIVES_AVEC_UN_ACCOMPAGNEMENT = (
+        "numerique-realiser-des-demarches-administratives-avec-un-accompagnement"
+    )
+    NUMERIQUE_PRENDRE_EN_MAIN_UN_SMARTPHONE_OU_UNE_TABLETTE = (
+        "numerique-prendre-en-main-un-smartphone-ou-une-tablette"
+    )
+    NUMERIQUE_PRENDRE_EN_MAIN_UN_ORDINATEUR = "numerique-prendre-en-main-un-ordinateur"
+    NUMERIQUE_UTILISER_LE_NUMERIQUE_AU_QUOTIDIEN = (
+        "numerique-utiliser-le-numerique-au-quotidien"
+    )
+    NUMERIQUE_APPROFONDIR_MA_CULTURE_NUMERIQUE = (
+        "numerique-approfondir-ma-culture-numerique"
+    )
+    NUMERIQUE_FAVORISER_MON_INSERTION_PROFESSIONNELLE = (
+        "numerique-favoriser-mon-insertion-professionnelle"
+    )
+    NUMERIQUE_ACCEDER_A_UNE_CONNEXION_INTERNET = (
+        "numerique-acceder-a-une-connexion-internet"
+    )
+    NUMERIQUE_ACCEDER_A_DU_MATERIEL = "numerique-acceder-a-du-materiel"
+    NUMERIQUE_S_EQUIPER_EN_MATERIEL_INFORMATIQUE = (
+        "numerique-s-equiper-en-materiel-informatique"
+    )
+    NUMERIQUE_CREER_ET_DEVELOPPER_MON_ENTREPRIES = (
+        "numerique-creer-et-developper-mon-entrepries"
+    )
+    NUMERIQUE_CREER_AVEC_LE_NUMERIQUE = "numerique-creer-avec-le-numerique"
+    NUMERIQUE_ACCOMPAGNER_LES_DEMARCHES_DE_SANTE = (
+        "numerique-accompagner-les-demarches-de-sante"
+    )
+    NUMERIQUE_PROMOUVOIR_LA_CITOYENNETE_NUMERIQUE = (
+        "numerique-promouvoir-la-citoyennete-numerique"
+    )
+    NUMERIQUE_SOUTENIR_LA_PARENTALITE_ET_L_EDUCATION_AVEC_LE_NUMERIQUE = (
+        "numerique-soutenir-la-parentalite-et-l-education-avec-le-numerique"
+    )
+
     PREPARER_SA_CANDIDATURE = "preparer-sa-candidature"
+    PREPARER_SA_CANDIDATURE_VALORISER_SES_COMPETENCES = (
+        "preparer-sa-candidature-valoriser-ses-competences"
+    )
+    PREPARER_SA_CANDIDATURE_REALISER_UN_CV_ET_OU_UNE_LETTRE_DE_MOTIVATION = (
+        "preparer-sa-candidature-realiser-un-cv-et-ou-une-lettre-de-motivation"
+    )
+    PREPARER_SA_CANDIDATURE_DEVELOPPER_SON_RESEAU = (
+        "preparer-sa-candidature-developper-son-reseau"
+    )
+    PREPARER_SA_CANDIDATURE_ORGANISER_SES_DEMARCHES_DE_RECHERCHE_DEMPLOI = (
+        "preparer-sa-candidature-organiser-ses-demarches-de-recherche-demploi"
+    )
+
     TROUVER_UN_EMPLOI = "trouver-un-emploi"
+    TROUVER_UN_EMPLOI_REPONDRE_A_DES_OFFRES_DEMPLOI = (
+        "trouver-un-emploi-repondre-a-des-offres-demploi"
+    )
+    TROUVER_UN_EMPLOI_FAIRE_DES_CANDIDATURES_SPONTANEES = (
+        "trouver-un-emploi-faire-des-candidatures-spontanees"
+    )
+    TROUVER_UN_EMPLOI_SUIVRE_SES_CANDIDATURES_ET_RELANCER_LES_EMPLOYEURS = (
+        "trouver-un-emploi-suivre-ses-candidatures-et-relancer-les-employeurs"
+    )
+    TROUVER_UN_EMPLOI_CONVAINCRE_UN_RECRUTEUR_EN_ENTRETIEN = (
+        "trouver-un-emploi-convaincre-un-recruteur-en-entretien"
+    )
 
 
 class TypologieService(str, Enum):
