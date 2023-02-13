9ef48e7eb57da5a4ccb9439954e5a25a5f1751f4
Valentin Matton <taq@protonmail.ch>
2023-02-13 11:12:18 +0100

    fix(sql)

--- pipeline/dbt/models/intermediate/dora/int_dora__services.sql
+++ pipeline/dbt/models/intermediate/dora/int_dora__services.sql
@@ -5,16 +5,16 @@ WITH services AS (
 final AS (
     SELECT
         id,
-        'dora'                                        AS "source",
-        name                                          AS "nom",
-        short_desc                                    AS "presentation_resume",
-        kinds                                         AS "types",
-        online_form                                   AS "prise_rdv",
-        NULL::TEXT[]                                  AS "frais",
-        fee_details                                   AS "frais_autres",
-        NULL::TEXT[]                                  AS "profils",
-        SPLIT_PART(TRIM('/' FROM structure), '/', -1) AS "structure_id",
-        (categories || subcategories)                 AS "thematiques"
+        'dora'                                                  AS "source",
+        name                                                    AS "nom",
+        short_desc                                              AS "presentation_resume",
+        kinds                                                   AS "types",
+        online_form                                             AS "prise_rdv",
+        NULL::TEXT[]                                            AS "frais",
+        fee_details                                             AS "frais_autres",
+        NULL::TEXT[]                                            AS "profils",
+        SPLIT_PART(TRIM('/' FROM structure), '/structures/', 2) AS "structure_id",
+        (categories || subcategories)                           AS "thematiques"
     FROM services
 )
 
