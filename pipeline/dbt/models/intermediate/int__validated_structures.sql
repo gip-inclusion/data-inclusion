4c23973df4683c5b09dd0a60ecd246aa08c4207c
Valentin Matton <taq@protonmail.ch>
2023-02-14 11:22:51 +0100

    fix(dbt): validation (bis)

--- pipeline/dbt/models/intermediate/int__validated_structures.sql
+++ pipeline/dbt/models/intermediate/int__validated_structures.sql
@@ -19,18 +19,18 @@ final AS (
     FROM structures
     WHERE
         id IS NOT NULL
-        AND siret ~ '^\d{14}$'
-        AND rna ~ '^W\d{9}$'
+        AND (siret IS NULL OR siret ~ '^\d{14}$')
+        AND (rna IS NULL OR rna ~ '^W\d{9}$')
         AND nom IS NOT NULL
         AND commune IS NOT NULL
         AND code_postal ~ '^\d{5}$'
-        AND code_insee ~ '^.{5}$'
+        AND (code_insee IS NULL OR code_insee ~ '^.{5}$')
         AND adresse IS NOT NULL
         AND date_maj IS NOT NULL
         AND (typologie IS NULL OR typologie IN (SELECT value FROM typologies_de_structures))
         AND (labels_nationaux IS NULL OR labels_nationaux <@ ARRAY(SELECT value FROM labels_nationaux))
         AND (thematiques IS NULL OR thematiques <@ ARRAY(SELECT value FROM thematiques))
-        AND LENGTH(presentation_resume) <= 280
+        AND (presentation_resume IS NULL OR LENGTH(presentation_resume) <= 280)
 )
 
 SELECT * FROM final
