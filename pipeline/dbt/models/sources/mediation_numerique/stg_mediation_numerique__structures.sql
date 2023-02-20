95e3b9292b30f53a3e53f0c3f9a1dcc5eb74ade7
Valentin Matton <taq@protonmail.ch>
2023-02-20 11:52:52 +0100

    feat(mednum): use labels nationaux

--- pipeline/dbt/models/sources/mediation_numerique/stg_mediation_numerique__structures.sql
+++ pipeline/dbt/models/sources/mediation_numerique/stg_mediation_numerique__structures.sql
@@ -9,21 +9,22 @@ WITH source AS (
 
 final AS (
     SELECT
-        ARRAY(SELECT * FROM JSONB_ARRAY_ELEMENTS_TEXT(NULLIF(data -> 'thematiques', 'null')))::TEXT[] AS "thematiques",
-        (data ->> 'longitude')::FLOAT                                                                 AS "longitude",
-        (data ->> 'latitude')::FLOAT                                                                  AS "latitude",
-        (data ->> 'date_maj')::TIMESTAMP WITH TIME ZONE                                               AS "date_maj",
-        data ->> 'id'                                                                                 AS "id",
-        data ->> 'nom'                                                                                AS "nom",
-        NULLIF(data ->> 'siret', REPEAT('0', 14))                                                     AS "siret",
-        data ->> 'source'                                                                             AS "source",
-        data ->> 'adresse'                                                                            AS "adresse",
-        data ->> 'commune'                                                                            AS "commune",
-        data ->> 'courriel'                                                                           AS "courriel",
-        data ->> 'site_web'                                                                           AS "site_web",
-        data ->> 'telephone'                                                                          AS "telephone",
-        data ->> 'code_postal'                                                                        AS "code_postal",
-        data ->> 'horaires_ouverture'                                                                 AS "horaires_ouverture"
+        ARRAY(SELECT * FROM JSONB_ARRAY_ELEMENTS_TEXT(NULLIF(data -> 'thematiques', 'null')))::TEXT[]       AS "thematiques",
+        (data ->> 'longitude')::FLOAT                                                                       AS "longitude",
+        (data ->> 'latitude')::FLOAT                                                                        AS "latitude",
+        (data ->> 'date_maj')::TIMESTAMP WITH TIME ZONE                                                     AS "date_maj",
+        ARRAY(SELECT * FROM JSONB_ARRAY_ELEMENTS_TEXT(NULLIF(data -> 'labels_nationaux', 'null')))::TEXT[] AS "labels_nationaux",
+        data ->> 'id'                                                                                       AS "id",
+        data ->> 'nom'                                                                                      AS "nom",
+        NULLIF(data ->> 'siret', REPEAT('0', 14))                                                           AS "siret",
+        data ->> 'source'                                                                                   AS "source",
+        data ->> 'adresse'                                                                                  AS "adresse",
+        data ->> 'commune'                                                                                  AS "commune",
+        data ->> 'courriel'                                                                                 AS "courriel",
+        data ->> 'site_web'                                                                                 AS "site_web",
+        data ->> 'telephone'                                                                                AS "telephone",
+        data ->> 'code_postal'                                                                              AS "code_postal",
+        data ->> 'horaires_ouverture'                                                                       AS "horaires_ouverture"
     FROM source
 )
 
