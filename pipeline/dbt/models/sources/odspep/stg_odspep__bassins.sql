052bc141e5c544b351267eae54704dadeffcad04
Colin Maudry <colin@maudry.com>
2023-02-21 22:00:19 +0100

    feature(pipeline): add zone_diffusion to odspep mapping (only com and bas) #27

---
+++ pipeline/dbt/models/sources/odspep/stg_odspep__bassins.sql
@@ -0,0 +1,17 @@
+WITH source AS (
+    SELECT *
+    FROM {{ source('odspep', 'DD009_BASSIN_RESSOURCE') }}
+),
+
+
+final AS (
+    SELECT
+        "ID_BAS"          AS "id",
+        "ID_BAS"          AS "id_bas",
+        "ID_RES"          AS "id_res",
+        "CODE_BASSIN_BAS" AS "code_bassin_bas"
+
+    FROM source
+)
+
+SELECT * FROM final
---
+++ pipeline/dbt/models/sources/odspep/stg_odspep__communes.sql
@@ -0,0 +1,17 @@
+WITH source AS (
+    SELECT *
+    FROM {{ source('odspep', 'DD009_COMMUNE_RESSOURCE') }}
+),
+
+
+final AS (
+    SELECT
+        "ID_COM"           AS "id",
+        "ID_COM"           AS "id_com",
+        "ID_RES"           AS "id_res",
+        "CODE_COMMUNE_COM" AS "code_commune_com"
+
+    FROM source
+)
+
+SELECT * FROM final
