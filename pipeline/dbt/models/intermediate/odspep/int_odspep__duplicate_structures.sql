31a47660486c2a14e711bdb63d434ee46b674d5e
Colin Maudry <colin@maudry.com>
2023-02-27 16:11:37 +0100

    feat(odspep): start with deduplicated structures on id_res, order by duplicate groups #36_

--- pipeline/dbt/models/intermediate/odspep/int_odspep__duplicate_structures.sql
+++ pipeline/dbt/models/intermediate/odspep/int_odspep__duplicate_structures.sql
@@ -2,26 +2,29 @@ WITH ressources_partenariales AS (
     SELECT * FROM {{ ref('int_odspep__enhanced_res_partenariales') }}
 ),
 
-
-
-select_duplicates AS (
-    SELECT
+unique_id_res AS (
+    SELECT DISTINCT ON (id_res)
         id_res,
         nom_structure,
         l1_identification_dest_adr,
         l3_complement_adr,
         l4_numero_lib_voie_adr,
-        code_commune_adr,
+        code_commune_adr
+    FROM ressources_partenariales
+),
+
+select_duplicates AS (
+    SELECT *,
         '1'::BOOLEAN                                                                                                                                                     AS "duplicate",
         COUNT(id_res) OVER(PARTITION BY UNACCENT(LOWER(CONCAT(nom_structure, l1_identification_dest_adr, l3_complement_adr, l4_numero_lib_voie_adr, code_commune_adr)))) AS "count_duplicates"
-
-    FROM ressources_partenariales
+ 
+    FROM unique_id_res
 ),
 
 final AS (
     SELECT * FROM select_duplicates
     WHERE "count_duplicates" > 1
+    ORDER BY UNACCENT(LOWER(CONCAT(count_duplicates, nom_structure, l1_identification_dest_adr, l3_complement_adr, l4_numero_lib_voie_adr, code_commune_adr))) DESC
 
 )
-
 SELECT * FROM final
