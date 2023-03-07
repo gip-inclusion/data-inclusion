dd4d6f2ff788adc0312853968883bc6f5ed6fe8c
Valentin Matton <taq@protonmail.ch>
2023-03-07 13:01:04 +0100

    fix: exclude communes déléguées

--- pipeline/dbt/models/sources/odspep/stg_odspep__communes.sql
+++ pipeline/dbt/models/sources/odspep/stg_odspep__communes.sql
@@ -7,16 +7,23 @@ communes AS (
     SELECT * FROM {{ source('insee', 'communes') }}
 ),
 
+-- exclude communes déléguées (duplicated codes)
+filtered_communes AS (
+    SELECT *
+    FROM communes
+    WHERE "TYPECOM" != 'COMD'
+),
+
 final AS (
     SELECT
-        source."ID_COM"           AS "id",
-        source."ID_COM"           AS "id_com",
-        source."ID_RES"           AS "id_res",
-        source."CODE_COMMUNE_COM" AS "code_commune_com",
-        communes."LIBELLE"        AS "libelle"
+        source."ID_COM"             AS "id",
+        source."ID_COM"             AS "id_com",
+        source."ID_RES"             AS "id_res",
+        source."CODE_COMMUNE_COM"   AS "code_commune_com",
+        filtered_communes."LIBELLE" AS "libelle"
 
     FROM source
-    LEFT JOIN communes ON source."CODE_COMMUNE_COM" = communes."COM"
+    LEFT JOIN filtered_communes ON source."CODE_COMMUNE_COM" = filtered_communes."COM"
 )
 
 SELECT * FROM final
