b3b2ecc9daeb082d46642887e79fe378604bfc3b
vmttn <taq@protonmail.ch>
2023-05-02 15:27:33 +0200

    feat(dora): update mapping (#88)

--- pipeline/dbt/models/intermediate/dora/int_dora__services.sql
+++ pipeline/dbt/models/intermediate/dora/int_dora__services.sql
@@ -2,30 +2,39 @@ WITH services AS (
     SELECT * FROM {{ ref('stg_dora__services') }}
 ),
 
+di_frais_by_dora_fee_condition AS (
+    SELECT x.*
+    FROM (
+        VALUES
+        ('1', 'gratuit'),
+        ('2', 'gratuit-sous-conditions'),
+        ('3', 'payant'),
+        ('4', 'adhesion'),
+        ('5', 'pass-numerique')
+    ) AS x (fee_condition, frais)
+),
+
+
 final AS (
     SELECT
         id,
         _di_source_id                                           AS "source",
         name                                                    AS "nom",
         short_desc                                              AS "presentation_resume",
-        NULL                                                    AS "presentation_detail",
         kinds                                                   AS "types",
         online_form                                             AS "prise_rdv",
-        NULL::TEXT []                                           AS "frais",
+        ARRAY(
+            SELECT di_frais_by_dora_fee_condition.frais
+            FROM di_frais_by_dora_fee_condition
+            WHERE services.fee_condition = di_frais_by_dora_fee_condition.fee_condition
+        )::TEXT []                                              AS "frais",
         fee_details                                             AS "frais_autres",
         NULL::TEXT []                                           AS "profils",
         NULL                                                    AS "pre_requis",
         NULL                                                    AS "cumulable",
         NULL                                                    AS "justificatifs",
-        NULL                                                    AS "formulaire_en_ligne",
-        NULL                                                    AS "commune",
-        NULL                                                    AS "code_postal",
-        NULL                                                    AS "code_insee",
-        NULL                                                    AS "adresse",
-        NULL                                                    AS "complement_adresse",
-        NULL                                                    AS "longitude",
-        NULL                                                    AS "latitude",
-        NULL                                                    AS "recurrence",
+        longitude                                               AS "longitude",
+        latitude                                                AS "latitude",
         NULL                                                    AS "date_creation",
         NULL                                                    AS "date_suspension",
         NULL                                                    AS "lien_source",
@@ -37,6 +46,14 @@ final AS (
         NULL                                                    AS "zone_diffusion_type",
         NULL                                                    AS "zone_diffusion_code",
         NULL                                                    AS "zone_diffusion_nom",
+        NULLIF(full_desc, '')                                   AS "presentation_detail",
+        NULLIF(online_form, '')                                 AS "formulaire_en_ligne",
+        NULLIF(city, '')                                        AS "commune",
+        NULLIF(postal_code, '')                                 AS "code_postal",
+        NULLIF(city_code, '')                                   AS "code_insee",
+        NULLIF(address_1, '')                                   AS "adresse",
+        NULLIF(address_2, '')                                   AS "complement_adresse",
+        NULLIF(recurrence, '')                                  AS "recurrence",
         SPLIT_PART(TRIM('/' FROM structure), '/structures/', 2) AS "structure_id",
         (categories || subcategories)                           AS "thematiques"
     FROM services
--- pipeline/dbt/models/sources/dora/stg_dora__services.sql
+++ pipeline/dbt/models/sources/dora/stg_dora__services.sql
@@ -4,7 +4,7 @@ WITH source AS (
 
 final AS (
     SELECT
-        _di_source_id         AS "_di_source_id",
+        _di_source_id                 AS "_di_source_id",
         ARRAY(
             SELECT * FROM
                 JSONB_ARRAY_ELEMENTS_TEXT(
@@ -13,7 +13,7 @@ final AS (
                         FROM JSONB_PATH_QUERY_ARRAY(data, '$.kinds[*].value')
                     )
                 )
-        )::TEXT []            AS "kinds",
+        )::TEXT []                    AS "kinds",
         ARRAY(
             SELECT * FROM
                 JSONB_ARRAY_ELEMENTS_TEXT(
@@ -22,7 +22,7 @@ final AS (
                         FROM JSONB_PATH_QUERY_ARRAY(data, '$.categories[*].value')
                     )
                 )
-        )::TEXT []            AS "categories",
+        )::TEXT []                    AS "categories",
         ARRAY(
             SELECT * FROM
                 JSONB_ARRAY_ELEMENTS_TEXT(
@@ -31,13 +31,23 @@ final AS (
                         FROM JSONB_PATH_QUERY_ARRAY(data, '$.subcategories[*].value')
                     )
                 )
-        )::TEXT []            AS "subcategories",
-        data ->> 'id'         AS "id",
-        data ->> 'structure'  AS "structure",
-        data ->> 'name'       AS "name",
-        data ->> 'shortDesc'  AS "short_desc",
-        data ->> 'onlineForm' AS "online_form",
-        data ->> 'feeDetails' AS "fee_details"
+        )::TEXT []                    AS "subcategories",
+        (data ->> 'longitude')::FLOAT AS "longitude",
+        (data ->> 'latitude')::FLOAT  AS "latitude",
+        data ->> 'id'                 AS "id",
+        data ->> 'structure'          AS "structure",
+        data ->> 'name'               AS "name",
+        data ->> 'fullDesc'           AS "full_desc",
+        data ->> 'shortDesc'          AS "short_desc",
+        data ->> 'onlineForm'         AS "online_form",
+        data ->> 'feeDetails'         AS "fee_details",
+        data ->> 'feeCondition'       AS "fee_condition",
+        data ->> 'postalCode'         AS "postal_code",
+        data ->> 'cityCode'           AS "city_code",
+        data ->> 'city'               AS "city",
+        data ->> 'address1'           AS "address_1",
+        data ->> 'address2'           AS "address_2",
+        data ->> 'recurrence'         AS "recurrence"
     FROM source
 )
 
