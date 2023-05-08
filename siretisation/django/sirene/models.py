4e467e3bfab282d207f09eabc920c88eacbf40fe
Valentin Matton <taq@protonmail.ch>
2023-05-08 17:37:42 +0200

    refactor: clean up

--- siretisation/django/sirene/models.py
+++ siretisation/django/sirene/models.py
@@ -1,38 +1,6 @@
 import enum
 
-from django.contrib.postgres.indexes import GinIndex
 from django.db import models
-
-
-class Establishment(models.Model):
-    siret = models.CharField(verbose_name="Siret", max_length=14, primary_key=True)
-    siren = models.CharField(verbose_name="Siren", max_length=9)
-    name = models.CharField(verbose_name="Nom", max_length=255)
-
-    ape = models.ForeignKey("CodeNAF", on_delete=models.PROTECT, related_name="establishments", null=True)
-    address1 = models.CharField(max_length=255, blank=True)
-    address2 = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    city_code = models.CharField(max_length=5, db_index=True)
-    postal_code = models.CharField(max_length=5)
-    is_siege = models.BooleanField()
-    longitude = models.FloatField(blank=True, null=True)
-    latitude = models.FloatField(blank=True, null=True)
-
-    class Meta:
-        indexes = [
-            # https://www.postgresql.org/docs/current/pgtrgm.html#id-1.11.7.40.8
-            GinIndex(
-                name="full_text_name_trgm_idx",
-                fields=("name",),
-                opclasses=("gin_trgm_ops",),
-            ),
-            GinIndex(
-                name="full_text_address_trgm_idx",
-                fields=("address1",),
-                opclasses=("gin_trgm_ops",),
-            ),
-        ]
 
 
 class CodeNAF(models.Model):
