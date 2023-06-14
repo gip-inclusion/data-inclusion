f5f32bd64364621d40f18e58218169e392a50038
Valentin Matton <taq@protonmail.ch>
2023-06-14 12:16:14 +0200

    fix(soliguide): proper coordinates

--- pipeline/dbt/models/sources/soliguide/stg_soliguide__lieux.sql
+++ pipeline/dbt/models/sources/soliguide/stg_soliguide__lieux.sql
@@ -5,22 +5,22 @@ WITH source AS (
 
 final AS (
     SELECT
-        _di_source_id                                         AS "_di_source_id",
-        (data ->> 'updatedAt')::DATE                          AS "updated_at",
-        (data #>> '{position,position,coordinates,x}')::FLOAT AS "position_coordinates_x",
-        (data #>> '{position,position,coordinates,y}')::FLOAT AS "position_coordinates_y",
-        data ->> 'lieu_id'                                    AS "id",
-        data ->> 'lieu_id'                                    AS "lieu_id",
-        data ->> 'name'                                       AS "name",
-        data #>> '{position,ville}'                           AS "position_ville",
-        data ->> 'description'                                AS "description",
-        data ->> 'seo_url'                                    AS "seo_url",
+        _di_source_id                                          AS "_di_source_id",
+        (data ->> 'updatedAt')::DATE                           AS "updated_at",
+        (data #>> '{position,location,coordinates[0]}')::FLOAT AS "position_coordinates_x",
+        (data #>> '{position,location,coordinates[1]}')::FLOAT AS "position_coordinates_y",
+        data ->> 'lieu_id'                                     AS "id",
+        data ->> 'lieu_id'                                     AS "lieu_id",
+        data ->> 'name'                                        AS "name",
+        data #>> '{position,ville}'                            AS "position_ville",
+        data ->> 'description'                                 AS "description",
+        data ->> 'seo_url'                                     AS "seo_url",
         -- TODO: entity.phones
-        data #>> '{position,codePostal}'                      AS "position_code_postal",
-        data #>> '{position,adresse}'                         AS "position_adresse",
-        data #>> '{position,complementAdresse}'               AS "position_complement_adresse",
-        NULLIF(data #>> '{entity,mail}', '')                  AS "entity_mail",
-        data #>> '{entity,website}'                           AS "entity_website"
+        data #>> '{position,codePostal}'                       AS "position_code_postal",
+        data #>> '{position,adresse}'                          AS "position_adresse",
+        data #>> '{position,complementAdresse}'                AS "position_complement_adresse",
+        NULLIF(data #>> '{entity,mail}', '')                   AS "entity_mail",
+        data #>> '{entity,website}'                            AS "entity_website"
     FROM source
 )
 
