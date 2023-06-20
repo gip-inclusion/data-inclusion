85b547b7edeb19d141b5e31f22509f27345c7151
Valentin Matton <taq@protonmail.ch>
2023-06-20 12:11:35 +0200

    refactor(mednum): split staging models

--- pipeline/dbt/macros/domain/mediation_numerique/stg_mediation_numerique_structures_model.sql
+++ pipeline/dbt/macros/domain/mediation_numerique/stg_mediation_numerique_structures_model.sql
@@ -1,33 +1,7 @@
+{% macro stg_mediation_numerique_structures_model() %}
+
 WITH source AS (
-    {{
-        dbt_utils.union_relations(
-            relations=[
-                source('mediation_numerique_angers', 'structures'),
-                source('mediation_numerique_assembleurs', 'structures'),
-                source('mediation_numerique_cd23', 'structures'),
-                source('mediation_numerique_cd33', 'structures'),
-                source('mediation_numerique_cd40', 'structures'),
-                source('mediation_numerique_cd44', 'structures'),
-                source('mediation_numerique_cd49', 'structures'),
-                source('mediation_numerique_cd87', 'structures'),
-                source('mediation_numerique_conseiller_numerique', 'structures'),
-                source('mediation_numerique_conumm', 'structures'),
-                source('mediation_numerique_cr93', 'structures'),
-                source('mediation_numerique_fibre_64', 'structures'),
-                source('mediation_numerique_france_services', 'structures'),
-                source('mediation_numerique_france_tiers_lieux', 'structures'),
-                source('mediation_numerique_francilin', 'structures'),
-                source('mediation_numerique_hinaura', 'structures'),
-                source('mediation_numerique_hub_antilles', 'structures'),
-                source('mediation_numerique_hub_lo', 'structures'),
-                source('mediation_numerique_mulhouse', 'structures'),
-                source('mediation_numerique_res_in', 'structures'),
-                source('mediation_numerique_rhinocc', 'structures'),
-                source('mediation_numerique_ultra_numerique', 'structures'),
-            ],
-            source_column_name=None
-        )
-    }}
+    SELECT * FROM {{ source('mediation_numerique_' ~ model.fqn[-2], 'structures') }}
 ),
 
 final AS (
@@ -53,3 +27,5 @@ final AS (
 )
 
 SELECT * FROM final
+
+{% endmacro %}
