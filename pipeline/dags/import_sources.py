1586a2d6c3074a9d417102c3b804327c51999c14
Valentin Matton <taq@protonmail.ch>
2023-06-20 18:02:41 +0200

    chore(pipeline): remove code duplication

--- pipeline/dags/import_sources.py
+++ pipeline/dags/import_sources.py
@@ -1,15 +1,14 @@
 import logging
-from typing import Optional
 
 import airflow
 import pendulum
-from airflow.models import Variable
-from airflow.operators import bash, empty, python
+from airflow.operators import empty, python
 from airflow.utils.task_group import TaskGroup
 
+from dags.dbt import dbt_operator_factory
 from dags.notifications import format_failure, notify_webhook
 from dags.settings import SOURCES_CONFIGS
-from dags.virtualenvs import DBT_PYTHON_BIN_PATH, PYTHON_BIN_PATH
+from dags.virtualenvs import PYTHON_BIN_PATH
 
 logger = logging.getLogger(__name__)
 
@@ -194,36 +193,6 @@ def _load(
                     "_di_logical_date": sqla.Date,
                 },
             )
-
-
-def dbt_operator_factory(
-    task_id: str,
-    command: str,
-    select: Optional[str] = None,
-    exclude: Optional[str] = None,
-) -> bash.BashOperator:
-    """A basic factory for bash operators operating dbt commands."""
-
-    dbt_args = command
-    if select is not None:
-        dbt_args += f" --select {select}"
-    if exclude is not None:
-        dbt_args += f" --exclude {exclude}"
-
-    return bash.BashOperator(
-        task_id=task_id,
-        bash_command=f"{DBT_PYTHON_BIN_PATH.parent / 'dbt'} {dbt_args}",
-        append_env=True,
-        env={
-            "DBT_PROFILES_DIR": Variable.get("DBT_PROJECT_DIR"),
-            "POSTGRES_HOST": "{{ conn.pg.host }}",
-            "POSTGRES_PORT": "{{ conn.pg.port }}",
-            "POSTGRES_USER": "{{ conn.pg.login }}",
-            "POSTGRES_PASSWORD": "{{ conn.pg.password }}",
-            "POSTGRES_DB": "{{ conn.pg.schema }}",
-        },
-        cwd=Variable.get("DBT_PROJECT_DIR"),
-    )
 
 
 # generate a dedicated DAG for every configured sources
--- pipeline/dags/main.py
+++ pipeline/dags/main.py
@@ -1,12 +1,10 @@
-from typing import Optional
-
 import airflow
 import pendulum
-from airflow.models import Variable
-from airflow.operators import bash, empty, python
+from airflow.operators import empty, python
 
+from dags.dbt import dbt_operator_factory
 from dags.notifications import format_failure, notify_webhook
-from dags.virtualenvs import DBT_PYTHON_BIN_PATH, PYTHON_BIN_PATH
+from dags.virtualenvs import PYTHON_BIN_PATH
 
 default_args = {
     "on_failure_callback": lambda context: notify_webhook(
@@ -67,36 +65,6 @@ def _geocode():
                     "result_score": sqla.Float,
                 },
             )
-
-
-def dbt_operator_factory(
-    task_id: str,
-    command: str,
-    select: Optional[str] = None,
-    exclude: Optional[str] = None,
-) -> bash.BashOperator:
-    """A basic factory for bash operators operating dbt commands."""
-
-    dbt_args = command
-    if select is not None:
-        dbt_args += f" --select {select}"
-    if exclude is not None:
-        dbt_args += f" --exclude {exclude}"
-
-    return bash.BashOperator(
-        task_id=task_id,
-        bash_command=f"{DBT_PYTHON_BIN_PATH.parent / 'dbt'} {dbt_args}",
-        append_env=True,
-        env={
-            "DBT_PROFILES_DIR": Variable.get("DBT_PROJECT_DIR"),
-            "POSTGRES_HOST": "{{ conn.pg.host }}",
-            "POSTGRES_PORT": "{{ conn.pg.port }}",
-            "POSTGRES_USER": "{{ conn.pg.login }}",
-            "POSTGRES_PASSWORD": "{{ conn.pg.password }}",
-            "POSTGRES_DB": "{{ conn.pg.schema }}",
-        },
-        cwd=Variable.get("DBT_PROJECT_DIR"),
-    )
 
 
 with airflow.DAG(
