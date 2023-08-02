867f8f651aa7f20fe2f10e51de85a5e2bad7ad62
Valentin Matton <taq@protonmail.ch>
2023-08-02 19:51:13 +0200

    fix(dbt): staging services

--- pipeline/dbt/models/staging/agefiph/stg_agefiph__services.sql
+++ pipeline/dbt/models/staging/agefiph/stg_agefiph__services.sql
@@ -4,27 +4,22 @@ WITH source AS (
 
 final AS (
     SELECT
-        _di_source_id                  AS "_di_source_id",
-        data ->> 'structure_id'        AS "structure_id",
-        data ->> 'courriel'            AS "courriel",
-        data ->> 'telephone'           AS "telephone",
-        data ->> 'adresse'             AS "adresse",
-        data ->> 'commune'             AS "commune",
-        data ->> 'code_postal'         AS "code_postal",
-        data ->> 'code_insee'          AS "code_insee",
-        data ->> 'zone_diffusion_type' AS "zone_diffusion_type",
-        data ->> 'zone_diffusion_code' AS "zone_diffusion_code",
-        data ->> 'id'                  AS "id",
-        data ->> 'date_creation'       AS "date_creation",
-        data ->> 'date_maj'            AS "date_maj",
-        data ->> 'nom'                 AS "nom",
-        data ->> 'presentation_resume' AS "presentation_resume",
-        data ->> 'presentation_detail' AS "presentation_detail",
-        data ->> 'contact_public'      AS "contact_public",
-        data ->> 'id_thematiques'      AS "id_thematiques",
-        data ->> 'thematiques'         AS "thematiques"
-
-
+        _di_source_id                                                                          AS "_di_source_id",
+        data ->> 'structure_id'                                                                AS "structure_id",
+        data ->> 'courriel'                                                                    AS "courriel",
+        data ->> 'telephone'                                                                   AS "telephone",
+        data ->> 'adresse'                                                                     AS "adresse",
+        data ->> 'commune'                                                                     AS "commune",
+        data ->> 'code_postal'                                                                 AS "code_postal",
+        data ->> 'code_insee'                                                                  AS "code_insee",
+        data ->> 'id'                                                                          AS "id",
+        CAST(data ->> 'date_creation' AS DATE)                                                 AS "date_creation",
+        CAST(data ->> 'date_maj' AS DATE)                                                      AS "date_maj",
+        data ->> 'nom'                                                                         AS "nom",
+        data ->> 'presentation_resume'                                                         AS "presentation_resume",
+        data ->> 'presentation_detail'                                                         AS "presentation_detail",
+        CAST(data ->> 'contact_public' AS BOOLEAN)                                             AS "contact_public",
+        CAST(ARRAY(SELECT * FROM JSONB_ARRAY_ELEMENTS_TEXT(data -> 'thematiques')) AS TEXT []) AS "thematiques"
     FROM source
 )
 
