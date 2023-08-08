6805f15d19c6877b30de0c6b89402e1d839a4051
vmttn <taq@protonmail.ch>
2023-08-08 12:32:50 +0200

    feat(soliguide): enhance mapping (#113)

--- pipeline/dbt/models/staging/soliguide/stg_soliguide__services.sql
+++ pipeline/dbt/models/staging/soliguide/stg_soliguide__services.sql
@@ -5,13 +5,18 @@ WITH source AS (
 
 final AS (
     SELECT
-        source._di_source_id                        AS "_di_source_id",
-        (source.data ->> 'updatedAt')::DATE         AS "updated_at",
-        source.data ->> 'lieu_id'                   AS "lieu_id",
-        services.data ->> 'serviceObjectId'         AS "id",
-        NULLIF(services.data ->> 'name', '')        AS "name",
-        services.data ->> 'categorie'               AS "categorie",
-        NULLIF(services.data ->> 'description', '') AS "description"
+        source._di_source_id                                AS "_di_source_id",
+        CAST(source.data ->> 'updatedAt' AS DATE)           AS "updated_at",
+        source.data ->> 'lieu_id'                           AS "lieu_id",
+        services.data ->> 'serviceObjectId'                 AS "id",
+        NULLIF(services.data ->> 'name', '')                AS "name",
+        services.data ->> 'categorie'                       AS "categorie",
+        NULLIF(services.data ->> 'description', '')         AS "description",
+        services.data -> 'hours'                            AS "hours",
+        CAST(services.data ->> 'differentHours' AS BOOLEAN) AS "different_hours",
+        CAST(services.data #>> '{close,actif}' AS BOOLEAN)  AS "close__actif",
+        CAST(services.data #>> '{close,dateDebut}' AS DATE) AS "close__date_debut",
+        CAST(services.data #>> '{close,dateFin}' AS DATE)   AS "close__date_fin"
     FROM
         source,
         LATERAL(SELECT * FROM JSONB_PATH_QUERY(source.data, '$.services_all[*]')) AS services (data)
