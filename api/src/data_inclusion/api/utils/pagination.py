d60ab7ecba0a4ef1de7e35ebf8ca72448f592c48
Valentin Matton <taq@protonmail.ch>
2023-08-22 10:29:07 +0200

    fix(api): prevent validation error on page size

--- api/src/data_inclusion/api/utils/pagination.py
+++ api/src/data_inclusion/api/utils/pagination.py
@@ -1,21 +1,13 @@
-from typing import Generic, TypeVar
-
 import fastapi
 import fastapi_pagination
 
 from data_inclusion.api import settings
 
-T = TypeVar("T")
-
-
-class Params(fastapi_pagination.Params):
-    size: int = fastapi.Query(
-        settings.DEFAULT_PAGE_SIZE,
+Page = fastapi_pagination.Page.with_custom_options(
+    size=fastapi.Query(
+        default=settings.DEFAULT_PAGE_SIZE,
         ge=1,
         le=settings.MAX_PAGE_SIZE,
         description="Page size",
-    )
-
-
-class Page(fastapi_pagination.Page[T], Generic[T]):
-    __params_type__ = Params
+    ),
+)
