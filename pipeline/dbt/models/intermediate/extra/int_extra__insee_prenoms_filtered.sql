4b609268fadcb812f1b378399e1c1fe637bf5f72
Valentin Matton <taq@protonmail.ch>
2023-10-06 11:58:27 +0200

    chore(dbt): fake missing extra sources

--- pipeline/dbt/models/intermediate/extra/int_extra__insee_prenoms_filtered.sql
+++ pipeline/dbt/models/intermediate/extra/int_extra__insee_prenoms_filtered.sql
@@ -1,11 +1,29 @@
-WITH firstname_counts_by_year_and_date AS (
-    SELECT *
-    FROM {{ source('data_inclusion', 'external_insee_fichier_prenoms') }}
+{% set source_model = source('data_inclusion', 'external_insee_fichier_prenoms') %}
+
+{% set table_exists = adapter.get_relation(database=source_model.database, schema=source_model.schema, identifier=source_model.name) is not none %}
+
+{% if table_exists %}
+
+    WITH source AS (
+        SELECT * FROM {{ source_model }}
+    ),
+
+{% else %}
+
+WITH source AS (
+    SELECT
+        CAST(NULL AS BIGINT) AS "sexe",
+        NULL                 AS "preusuel",
+        NULL                 AS "annais",
+        CAST(NULL AS BIGINT) AS "nombre"
+    WHERE FALSE
 ),
+
+{% endif %}
 
 final AS (
     SELECT DISTINCT LOWER(preusuel) AS prenom
-    FROM firstname_counts_by_year_and_date
+    FROM source
     WHERE
         LENGTH(preusuel) > 2
         AND nombre > 100
