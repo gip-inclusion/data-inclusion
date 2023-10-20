10600a2321b4592f62afdf611b9a7d939ad993f2
Valentin Matton <taq@protonmail.ch>
2023-10-20 08:33:17 +0200

    chore(scalingo): clean up env

--- pipeline/entrypoint.sh
+++ pipeline/entrypoint.sh
@@ -6,23 +6,6 @@ set -e
 # Trace execution
 [[ "${DEBUG}" ]] && set -x
 
-BASE_DIR=$(pwd)
-
-# The `DATABASE_URL` env var is automatically set by Scalingo and uses the depreciated
-# scheme `postgres://`. Replace it.
-export DATABASE_URL="${DATABASE_URL/postgres\:\/\//postgresql\:\/\/}"
-
-export AIRFLOW_HOME="${BASE_DIR}/airflow"
-
-export AIRFLOW__API__AUTH_BACKENDS=airflow.api.auth.backend.basic_auth
-export AIRFLOW__CORE__DAGS_FOLDER="${BASE_DIR}/dags"
-export AIRFLOW__CORE__DEFAULT_TIMEZONE=Europe/Paris
-export AIRFLOW__CORE__EXECUTOR=LocalExecutor
-export AIRFLOW__CORE__FERNET_KEY="${SECRET_KEY}"
-export AIRFLOW__CORE__LOAD_DEFAULT_CONNECTIONS=False
-export AIRFLOW__CORE__LOAD_EXAMPLES=False
-export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN="${DATABASE_URL}"
-
 # Create additional virtualenvs for isolated task executions
 VIRTUAL_ENV="${AIRFLOW_HOME}/venvs/python/venv"
 python -m venv "${VIRTUAL_ENV}"
