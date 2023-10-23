89b479539d4b9574ffe848bad37afdb50a30a565
Valentin Matton <taq@protonmail.ch>
2023-10-23 12:26:51 +0200

    fix(scalingo): remove multiline commands

--- pipeline/entrypoint.sh
+++ pipeline/entrypoint.sh
@@ -6,26 +6,58 @@ set -e
 # Trace execution
 [[ "${DEBUG}" ]] && set -x
 
-# Create additional virtualenvs for isolated task executions
-VIRTUAL_ENV="${AIRFLOW_HOME}/venvs/python/venv"
-python -m venv "${VIRTUAL_ENV}"
-"${VIRTUAL_ENV}/bin/python" -m pip install -U pip setuptools wheel
-"${VIRTUAL_ENV}/bin/python" -m pip install -r requirements/tasks/python/requirements.txt
-"${VIRTUAL_ENV}/bin/python" -m pip install .
+if [[ $# -eq 0 ]]; then
+    echo "No service parameter provided.";
+    exit 1;
+fi
 
-# Create additional virtualenvs for isolated task executions
-VIRTUAL_ENV="${AIRFLOW_HOME}/venvs/pipx/venv"
-python -m venv "${VIRTUAL_ENV}"
-"${VIRTUAL_ENV}/bin/python" -m pip install -U pip setuptools wheel
-"${VIRTUAL_ENV}/bin/python" -m pip install -r requirements/tasks/pipx/requirements.txt
+COMMAND=$1
 
-# Create additional virtualenvs for isolated task executions
-VIRTUAL_ENV="${AIRFLOW_HOME}/venvs/dbt/venv"
-python -m venv "${VIRTUAL_ENV}"
-"${VIRTUAL_ENV}/bin/python" -m pip install -U pip setuptools wheel
-"${VIRTUAL_ENV}/bin/python" -m pip install -r requirements/tasks/dbt/requirements.txt
+# The `DATABASE_URL` env var is automatically set by Scalingo and uses the depreciated
+# scheme `postgres://`. Therefore it is replaced.
+export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN="${DATABASE_URL/postgres\:\/\//postgresql\:\/\/}"
 
-# Install dbt packages (not python packages)
-"${VIRTUAL_ENV}/bin/dbt" deps --project-dir "${AIRFLOW_VAR_DBT_PROJECT_DIR}"
+if [[ "${COMMAND}" = "webserver" ]]; then
+    export AIRFLOW__API__AUTH_BACKENDS=airflow.api.auth.backend.basic_auth
 
-airflow scheduler
+    airflow webserver --port "${PORT}"
+fi
+
+if [[ "${COMMAND}" = "migrate" ]]; then
+    airflow db migrate
+fi
+
+if [[ "${COMMAND}" = "scheduler" ]]; then
+    # Create additional virtualenvs for isolated task executions
+    VIRTUAL_ENV="${AIRFLOW_HOME}/venvs/python/venv"
+    python -m venv "${VIRTUAL_ENV}"
+    "${VIRTUAL_ENV}/bin/python" -m pip install -U pip setuptools wheel
+    "${VIRTUAL_ENV}/bin/python" -m pip install -r requirements/tasks/python/requirements.txt
+    "${VIRTUAL_ENV}/bin/python" -m pip install .
+
+    # Create additional virtualenvs for isolated task executions
+    VIRTUAL_ENV="${AIRFLOW_HOME}/venvs/pipx/venv"
+    python -m venv "${VIRTUAL_ENV}"
+    "${VIRTUAL_ENV}/bin/python" -m pip install -U pip setuptools wheel
+    "${VIRTUAL_ENV}/bin/python" -m pip install -r requirements/tasks/pipx/requirements.txt
+
+    # Create additional virtualenvs for isolated task executions
+    VIRTUAL_ENV="${AIRFLOW_HOME}/venvs/dbt/venv"
+    python -m venv "${VIRTUAL_ENV}"
+    "${VIRTUAL_ENV}/bin/python" -m pip install -U pip setuptools wheel
+    "${VIRTUAL_ENV}/bin/python" -m pip install -r requirements/tasks/dbt/requirements.txt
+
+    # Install dbt packages (not python packages)
+    "${VIRTUAL_ENV}/bin/dbt" deps --project-dir "${AIRFLOW_VAR_DBT_PROJECT_DIR}"
+
+    export AIRFLOW_HOME=./airflow
+    export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN="${DATABASE_URL/postgres\:\/\//postgresql\:\/\/}"
+    export AIRFLOW__CORE__LOAD_DEFAULT_CONNECTIONS=False
+    export AIRFLOW__CORE__LOAD_EXAMPLES=False
+    export AIRFLOW__CORE__EXECUTOR=LocalExecutor
+    export AIRFLOW__CORE__DEFAULT_TIMEZONE=Europe/Paris
+    export AIRFLOW__CORE__FERNET_KEY="${SECRET_KEY}"
+    export AIRFLOW__CORE__DAGS_FOLDER=./dags
+
+    airflow scheduler
+fi
