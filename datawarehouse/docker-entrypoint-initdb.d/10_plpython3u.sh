cae0d6469e09b838a10757ffcf8a4fed4f9dcac9
Jonathan Perret <j-github@jonathanperret.net>
2023-10-26 17:13:05 +0200

    fix: rename Postgres init scripts to match contents
    The names of 10_plpython3u.sh and 10_postgis.sh were swapped.

--- datawarehouse/docker-entrypoint-initdb.d/10_plpython3u.sh
+++ datawarehouse/docker-entrypoint-initdb.d/10_plpython3u.sh
@@ -6,11 +6,5 @@ set -e
 export PGUSER="$POSTGRES_USER"
 
 "${psql[@]}"  --dbname="$POSTGRES_DB" <<- 'EOSQL'
-CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS postgis_topology;
--- Reconnect to update pg_setting.resetval
--- See https://github.com/postgis/docker-postgis/issues/288
-\c
-CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
-CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+CREATE EXTENSION IF NOT EXISTS plpython3u;
 EOSQL
--- datawarehouse/docker-entrypoint-initdb.d/10_postgis.sh
+++ datawarehouse/docker-entrypoint-initdb.d/10_postgis.sh
@@ -6,5 +6,11 @@ set -e
 export PGUSER="$POSTGRES_USER"
 
 "${psql[@]}"  --dbname="$POSTGRES_DB" <<- 'EOSQL'
-CREATE EXTENSION IF NOT EXISTS plpython3u;
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
+-- Reconnect to update pg_setting.resetval
+-- See https://github.com/postgis/docker-postgis/issues/288
+\c
+CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
 EOSQL
