1f1c63733629a2dbc7c4ddf99e59745b1d697682
Valentin Matton <taq@protonmail.ch>
2023-11-17 09:26:23 +0100

    chore(siretisation): remove service

--- siretisation/django/data_inclusion/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from data_inclusion import models
-
-
-@admin.register(models.Structure)
-class DatasetAdmin(admin.ModelAdmin):
-    list_display = ["di_surrogate_id", "nom", "siret"]
