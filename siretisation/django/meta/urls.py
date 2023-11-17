a5574d65b3f61810d63a6e83fd553414e2d5a207
Valentin Matton <taq@protonmail.ch>
2023-11-17 09:26:23 +0100

    chore(siretisation): remove service

--- siretisation/django/meta/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from meta.api import views
-
-urlpatterns = [
-    path("api/version/", views.VersionEndpoint.as_view()),
-]
--- siretisation/tests/users/factories.py
+++
@@ -1,8 +0,0 @@
-import factory
-
-
-class UserFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = "users.User"
-
-    id = factory.Sequence(lambda n: f"user_id_{n:04d}")
