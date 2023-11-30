e55d5e00e6a8c4d4b9d638eaab37cdcbb6525a24
Victor Perron <victor@iso3103.net>
2023-11-30 10:29:00 +0100

    feat(pipeline): Test DAGs in a separate CI setting

---
+++ pipeline/tests/ci/test_dags.py
@@ -0,0 +1,11 @@
+import pytest
+from airflow.models import DagBag
+
+
+@pytest.mark.parametrize("dag_id", DagBag().dag_ids)
+def test_dags_generic(dag_id):
+    dagbag = DagBag()
+    dag = dagbag.get_dag(dag_id=dag_id)
+    assert dagbag.import_errors == {}
+    assert dag is not None
+    assert len(dag.tasks) >= 1
