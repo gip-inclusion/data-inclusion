7d467d8866c4df32fc8e0e032c49588f38dc4181
Victor Perron <victor@iso3103.net>
2023-12-22 10:02:11 +0100

    chore(pipeline) : Reorganize the source configurations

--- pipeline/dags/import_sources.py
+++ pipeline/dags/import_sources.py
@@ -1,18 +1,12 @@
-import logging
-
 import airflow
 import pendulum
 from airflow.operators import empty, python
 from airflow.utils.task_group import TaskGroup
 
-from dag_utils import date
+from dag_utils import date, sources
 from dag_utils.dbt import dbt_operator_factory
 from dag_utils.notifications import format_failure, notify_webhook
-from dag_utils.settings import SOURCES_CONFIGS
 from dag_utils.virtualenvs import PYTHON_BIN_PATH
-
-logger = logging.getLogger(__name__)
-
 
 default_args = {
     "on_failure_callback": lambda context: notify_webhook(
@@ -23,131 +17,70 @@ default_args = {
 }
 
 
-def extract_from_source_to_datalake_bucket(
-    stream_config: dict,
-    source_config: dict,
-    run_id: str,
-    logical_date,
-):
-    from data_inclusion.scripts.tasks import (
-        dora,
-        emplois_de_linclusion,
-        grist,
-        mediation_numerique,
-        mes_aides,
-        reseau_alpha,
-        soliguide,
-        un_jeune_une_solution,
-        utils,
+def extract_from_source_to_datalake_bucket(source_id, stream_id, run_id, logical_date):
+    import logging
+
+    from dag_utils import s3, sources
+
+    logger = logging.getLogger(__name__)
+    source = sources.SOURCES_CONFIGS[source_id]
+    stream = source["streams"][stream_id]
+    url = stream["url"]
+
+    logger.info("Fetching file from url=%s", url)
+
+    s3_file_path = s3.source_file_path(
+        source_id=source_id,
+        filename=stream["filename"],
+        run_id=run_id,
+        logical_date=logical_date,
     )
 
-    from dag_utils import s3
-
-    source_id = source_config["id"]
-
-    # Store this in settings or in a "sources.py" module.
-    EXTRACT_FN_BY_SOURCE_ID = {
-        "agefiph": utils.extract_http_content,
-        "annuaire-du-service-public": utils.extract_http_content,
-        "cd35": utils.extract_http_content,
-        "cd72": grist.extract,
-        "data-inclusion": utils.extract_http_content,
-        "dora": dora.extract,
-        "emplois-de-linclusion": emplois_de_linclusion.extract,
-        "finess": utils.extract_http_content,
-        "mes-aides": mes_aides.extract,
-        "siao": utils.extract_http_content,
-        "un-jeune-une-solution": un_jeune_une_solution.extract,
-        "soliguide": soliguide.extract,
-        "monenfant": utils.extract_http_content,
-        "reseau-alpha": {
-            "structures": reseau_alpha.extract_structures,
-            "formations": reseau_alpha.extract_formations,
-        },
-        "pole-emploi": dora.extract,
-        "mediation-numerique": mediation_numerique.extract,
-    }
-
-    if isinstance(EXTRACT_FN_BY_SOURCE_ID[source_id], dict):
-        extract_fn = EXTRACT_FN_BY_SOURCE_ID[source_id][stream_config["id"]]
-    else:
-        extract_fn = EXTRACT_FN_BY_SOURCE_ID[source_id]
+    # FIXME(vperron) : Not a great fan of those "extractors" that accept ids
+    # and tokens, but it's not my main focus atm.
+    extract_fn = sources.get_extractor(source_id, stream_id)
 
     s3.store_content(
-        path=s3.source_file_path(
-            source_id=source_id,
-            filename=stream_config["filename"],
-            run_id=run_id,
-            logical_date=logical_date,
-        ),
-        content=extract_fn(**stream_config),
+        path=s3_file_path,
+        content=extract_fn(url=url, token=stream.get("token"), id=stream_id),
     )
 
 
-def load_from_s3_to_data_warehouse(
-    stream_config: dict,
-    source_config: dict,
-    run_id: str,
-    logical_date,
-):
+def load_from_s3_to_data_warehouse(source_id, stream_id, run_id, logical_date):
+    import logging
+
     import pandas as pd
     import sqlalchemy as sqla
     from sqlalchemy.dialects.postgresql import JSONB
 
-    from data_inclusion.scripts.tasks import (
-        agefiph,
-        annuaire_du_service_public,
-        monenfant,
-        reseau_alpha,
-        soliguide,
-        utils,
+    from dag_utils import pg, s3, sources
+
+    logger = logging.getLogger(__name__)
+    source = sources.SOURCES_CONFIGS[source_id]
+    stream = source["streams"][stream_id]
+    url = stream["url"]
+
+    s3_file_path = s3.source_file_path(
+        source_id=source_id,
+        filename=stream["filename"],
+        run_id=run_id,
+        logical_date=logical_date,
     )
 
-    from dag_utils import pg, s3
+    # FIXME(vperron) : Re-load the file as a dataframe. This seems a bit unefficient.
+    tmp_file_path = s3.download_file(s3_file_path)
 
-    # Meme remarque.
-    READ_FN_BY_SOURCE_ID = {
-        "annuaire-du-service-public": annuaire_du_service_public.read,
-        "cd35": lambda path: utils.read_csv(path, sep=";"),
-        "cd72": lambda path: utils.read_csv(path, sep=","),
-        "data-inclusion": lambda path: utils.read_csv(path, sep=","),
-        "dora": utils.read_json,
-        "emplois-de-linclusion": utils.read_json,
-        "finess": lambda path: utils.read_csv(path, sep=","),
-        "mes-aides": utils.read_json,
-        "siao": utils.read_excel,
-        "un-jeune-une-solution": utils.read_json,
-        "soliguide": soliguide.read,
-        "monenfant": monenfant.read,
-        "reseau-alpha": {
-            "structures": reseau_alpha.read_structures,
-            "formations": reseau_alpha.read_formations,
-        },
-        "agefiph": agefiph.read,
-        "pole-emploi": utils.read_json,
-        "mediation-numerique": utils.read_json,
-    }
+    logger.info("Downloading file s3_path=%s tmp_path=%s", s3_file_path, tmp_file_path)
 
-    source_id = source_config["id"]
-    stream_id = stream_config["id"]
-
-    if isinstance(READ_FN_BY_SOURCE_ID[source_id], dict):
-        read_fn = READ_FN_BY_SOURCE_ID[source_id][stream_id]
-    else:
-        read_fn = READ_FN_BY_SOURCE_ID[source_id]
-
-    s3_path = s3.source_file_path(
-        source_id, stream_config["filename"], run_id, logical_date
-    )
-    tmp_filename = s3.download_file(s3_path)
-    df = read_fn(path=tmp_filename)
+    read_fn = sources.get_reader(source_id, stream_id)
+    df = read_fn(path=tmp_file_path)
 
     df = pd.DataFrame().assign(data=df.apply(lambda row: row.to_dict(), axis="columns"))
     df = df.assign(_di_batch_id=run_id)
     df = df.assign(_di_source_id=source_id)
     df = df.assign(_di_stream_id=stream_id)
-    df = df.assign(_di_source_url=stream_config["url"])
-    df = df.assign(_di_stream_s3_key=s3_path)
+    df = df.assign(_di_source_url=url)
+    df = df.assign(_di_stream_s3_key=s3_file_path)
     df = df.assign(_di_logical_date=logical_date)
 
     schema_name = source_id.replace("-", "_")
@@ -186,53 +119,31 @@ def load_from_s3_to_data_warehouse(
         )
 
 
-# generate a dedicated DAG for every configured sources
-for source_config in SOURCES_CONFIGS:
-    dag_id = f"import_{source_config['id']}".replace("-", "_")
-    dag = airflow.DAG(
+for source_id, source_config in sources.SOURCES_CONFIGS.items():
+    model_name = source_id.replace("-", "_")
+    dag_id = f"import_{model_name}"
+
+    with airflow.DAG(
         dag_id=dag_id,
         start_date=pendulum.datetime(2022, 1, 1, tz=date.TIME_ZONE),
         default_args=default_args,
         schedule=source_config["schedule"],
         catchup=False,
         tags=["source"],
-    )
-
-    with dag:
+    ) as dag:
         start = empty.EmptyOperator(task_id="start")
         end = empty.EmptyOperator(task_id="end")
 
-        dbt_source_id = source_config["id"].replace("-", "_")
-
-        # tests here can detect impacting changes on the source data
-        # before anything happens to the previously existing data
-        dbt_test_source = dbt_operator_factory(
-            task_id="dbt_test_source",
-            command="test",
-            select=f"source:{dbt_source_id}",
-        )
-
-        # historization of the raw data, if that makes sense
-        if source_config["snapshot"]:
-            dbt_snapshot_source = dbt_operator_factory(
-                task_id="dbt_snapshot_source",
-                command="snapshot",
-                select=dbt_source_id,
-            )
-        else:
-            dbt_snapshot_source = None
-
-        # create dedicated embranchments for the extract/load of every streams in source
-        for stream_config in source_config["streams"]:
-            with TaskGroup(group_id=stream_config["id"]) as stream_task_group:
+        for stream_id in source_config["streams"]:
+            with TaskGroup(group_id=stream_id) as stream_task_group:
                 extract = python.ExternalPythonOperator(
                     task_id="extract",
                     python=str(PYTHON_BIN_PATH),
                     python_callable=extract_from_source_to_datalake_bucket,
                     retries=2,
                     op_kwargs={
-                        "stream_config": stream_config,
-                        "source_config": source_config,
+                        "source_id": source_id,
+                        "stream_id": stream_id,
                     },
                 )
                 load = python.ExternalPythonOperator(
@@ -240,18 +151,22 @@ for source_config in SOURCES_CONFIGS:
                     python=str(PYTHON_BIN_PATH),
                     python_callable=load_from_s3_to_data_warehouse,
                     op_kwargs={
-                        "stream_config": stream_config,
-                        "source_config": source_config,
+                        "source_id": source_id,
+                        "stream_id": stream_id,
                     },
                 )
 
                 start >> extract >> load
 
-            stream_task_group >> dbt_test_source
-
-            if dbt_snapshot_source is not None:
-                dbt_test_source >> dbt_snapshot_source >> end
-            else:
-                dbt_test_source >> end
+        # FIXME(vperron) : didn't Valentin say that snapshots aren't actually used ?
+        if source_config["snapshot"]:
+            dbt_snapshot_source = dbt_operator_factory(
+                task_id="dbt_snapshot_source",
+                command="snapshot",
+                select=model_name,
+            )
+            stream_task_group >> dbt_snapshot_source >> end
+        else:
+            stream_task_group >> end
 
     globals()[dag_id] = dag
