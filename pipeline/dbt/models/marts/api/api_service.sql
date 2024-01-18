e4cce4f980a3f87be9b7a7cdac2a5d3f5315be5d
Victor Perron <victor@iso3103.net>
2024-01-18 12:07:25 +0100

    feat(api,pipeline) : Add an index on the thematiques
    The API starts to be too slow.

--- pipeline/dbt/models/marts/api/api_service.sql
+++ pipeline/dbt/models/marts/api/api_service.sql
@@ -2,11 +2,13 @@
     config(
         pre_hook=[
             "DROP INDEX IF EXISTS service_modes_accueil_idx",
+            "DROP INDEX IF EXISTS service_thematiques_idx",
             "DROP INDEX IF EXISTS service_geography_idx",
         ],
         post_hook=[
             "ALTER TABLE {{ this }} ADD CONSTRAINT services_structure_surrogate_id_fk FOREIGN KEY (_di_structure_surrogate_id) REFERENCES {{ ref('api_structure') }} (_di_surrogate_id)",
             "CREATE INDEX IF NOT EXISTS service_modes_accueil_idx ON {{ this }} USING GIN (modes_accueil)",
+            "CREATE INDEX IF NOT EXISTS service_thematiques_idx ON {{ this }} USING GIN (thematiques)",
             "CREATE INDEX IF NOT EXISTS service_geography_idx ON {{ this }} USING GIST ((ST_MakePoint(longitude, latitude)::geography(geometry, 4326)))",
         ]
     )
