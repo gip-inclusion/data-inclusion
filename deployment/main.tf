582f6bbe70051c9867aeca6cbf3e3cc0fae2b897
Victor Perron <victor@iso3103.net>
2024-02-01 16:17:27 +0100

    feat(deployment) : Allow external database access
    Necessary especially for Metabase or any kind of quick inspection.
    
    The production database is exposed, why not this one ?

--- deployment/main.tf
+++ deployment/main.tf
@@ -19,6 +19,10 @@ resource "scaleway_instance_security_group" "main" {
     action = "accept"
     port   = 443
   }
+  inbound_rule {
+    action = "accept"
+    port   = 5432
+  }
 }
 
 resource "scaleway_instance_server" "main" {
