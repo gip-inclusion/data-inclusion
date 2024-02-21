64080f8d51446a758ce062fb544644a75f7d209a
Valentin Matton <taq@protonmail.ch>
2024-02-21 14:19:17 +0100

    chore: clean up indices creation

--- pipeline/dbt/models/marts/api/api_service.sql
+++ pipeline/dbt/models/marts/api/api_service.sql
@@ -3,7 +3,6 @@
         pre_hook=[
             "DROP INDEX IF EXISTS service_structure_surrogate_id_idx",
             "DROP INDEX IF EXISTS service_source_idx",
-            "DROP INDEX IF EXISTS structure_source_idx",
             "DROP INDEX IF EXISTS service_modes_accueil_idx",
             "DROP INDEX IF EXISTS service_thematiques_idx",
             "DROP INDEX IF EXISTS service_geography_idx",
@@ -11,8 +10,7 @@
         post_hook=[
             "ALTER TABLE {{ this }} ADD CONSTRAINT services_structure_surrogate_id_fk FOREIGN KEY (_di_structure_surrogate_id) REFERENCES {{ ref('api_structure') }} (_di_surrogate_id)",
             "CREATE INDEX IF NOT EXISTS service_structure_surrogate_id_idx ON service(_di_structure_surrogate_id);",
-            "CREATE INDEX IF NOT EXISTS service_source_idx ON service(source);",
-            "CREATE INDEX IF NOT EXISTS structure_source_idx ON structure(source);",
+            "CREATE INDEX IF NOT EXISTS service_source_idx ON {{ this }} (source);",
             "CREATE INDEX IF NOT EXISTS service_modes_accueil_idx ON {{ this }} USING GIN (modes_accueil)",
             "CREATE INDEX IF NOT EXISTS service_thematiques_idx ON {{ this }} USING GIN (thematiques)",
             "CREATE INDEX IF NOT EXISTS service_geography_idx ON {{ this }} USING GIST ((ST_MakePoint(longitude, latitude)::geography(geometry, 4326)))",
