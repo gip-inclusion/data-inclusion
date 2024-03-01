108b5c8e5aca3bab4e49816f57f2ef6e5c2936f3
Valentin Matton <taq@protonmail.ch>
2024-03-01 11:19:01 +0100

    chore(tests): cleanup db setup

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import faker
 import pytest
 import sqlalchemy as sqla
@@ -6,15 +8,21 @@ from alembic.config import Config
 
 from fastapi.testclient import TestClient
 
+from data_inclusion.api import settings
 from data_inclusion.api.core import db
 from data_inclusion.api.core.request.services import db as middleware_db
 from data_inclusion.api.entrypoints.fastapi import app
 
 from .inclusion import factories
 
+DEFAULT_DATABASE_URL = sqla.engine.make_url(settings.DATABASE_URL)
+TEST_DATABASE_URL = DEFAULT_DATABASE_URL.set(
+    database=f"{DEFAULT_DATABASE_URL.database}_test"
+)
+
 
 @pytest.fixture()
-def api_client(db_engine, test_session):
+def api_client(db_init, test_session):
     middleware_db.SessionLocal = factories.TestSession
     app.dependency_overrides[db.get_session] = lambda: test_session
 
@@ -43,64 +51,70 @@ def force_authenticate(request, api_client):
 
 
 @pytest.fixture(scope="session")
-def db_engine():
-    from data_inclusion.api import settings
-
-    default_database_url = sqla.engine.make_url(settings.DATABASE_URL)
-    test_database_url = default_database_url.set(
-        database=f"{default_database_url.database}_test"
+def db_init():
+    default_engine = sqla.create_engine(
+        DEFAULT_DATABASE_URL, isolation_level="AUTOCOMMIT"
     )
-
-    default_db_engine = sqla.create_engine(
-        default_database_url, isolation_level="AUTOCOMMIT"
-    )
+    test_engine = sqla.create_engine(TEST_DATABASE_URL)
 
     # Connect to the db and creates a new test database
-    with default_db_engine.connect() as default_db_conn:
-        default_db_conn.execute(
-            sqla.text(f'DROP DATABASE IF EXISTS "{test_database_url.database}";')
+    with default_engine.connect() as conn:
+        conn.execute(
+            sqla.text(f'DROP DATABASE IF EXISTS "{TEST_DATABASE_URL.database}"')
         )
-        default_db_conn.execute(
-            sqla.text(f'CREATE DATABASE "{test_database_url.database}";')
-        )
+        conn.execute(sqla.text(f'CREATE DATABASE "{TEST_DATABASE_URL.database}"'))
 
     # Create postgis extension in test database
-    with sqla.create_engine(
-        test_database_url, isolation_level="AUTOCOMMIT"
-    ).connect() as test_database_conn:
-        test_database_conn.execute(sqla.text("CREATE EXTENSION postgis;"))
+    with test_engine.connect() as conn:
+        conn.execute(sqla.text("CREATE EXTENSION postgis;"))
+        conn.commit()
 
     # Migrate the database
     config = Config()
     config.set_main_option("script_location", "src/alembic/")
     config.set_main_option(
-        "sqlalchemy.url", test_database_url.render_as_string(hide_password=False)
+        "sqlalchemy.url", TEST_DATABASE_URL.render_as_string(hide_password=False)
     )
 
     command.upgrade(config, "head")
 
-    db_engine = sqla.create_engine(test_database_url)
+    factories.TestSession.configure(bind=test_engine)
 
-    factories.TestSession.configure(bind=db_engine)
-
-    yield db_engine
+    yield test_engine
 
     factories.TestSession.remove()
 
-    # Downgrade the database
-    command.downgrade(config, "base")
-
-    # Teardown test database
-    with default_db_engine.connect() as default_db_conn:
-        default_db_conn.execute(
+    with default_engine.connect() as conn:
+        conn.execute(
             sqla.text(
-                f'DROP DATABASE IF EXISTS "{test_database_url.database}" WITH (FORCE);'
+                f'DROP DATABASE IF EXISTS "{TEST_DATABASE_URL.database}" WITH (FORCE)'
             )
         )
 
 
+@pytest.fixture(scope="session")
+def db_engine(db_init):
+    yield db_init
+
+
+@pytest.fixture(scope="session")
+def generate_communes_nord(db_engine):
+    import geopandas
+
+    df = geopandas.read_file(Path(__file__).parent / "communes_nord.sqlite")
+    df = df.rename_geometry("geom")
+
+    with db_engine.connect() as conn:
+        df.to_postgis(
+            "admin_express_communes",
+            con=conn,
+            if_exists="replace",
+            index=False,
+        )
+
+
 @pytest.fixture()
-def test_session(db_engine):
+def test_session(db_init):
     faker.Faker.seed(0)
     with factories.TestSession() as session:
         yield session
