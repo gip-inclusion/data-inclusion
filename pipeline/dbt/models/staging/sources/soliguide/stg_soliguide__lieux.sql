f73b74ec29a4f12a11f2f2b98873d33909e731d9
Valentin Matton <taq@protonmail.ch>
2024-03-08 15:31:57 +0100

    feat(pipeline): extract modalities

--- pipeline/dbt/models/staging/sources/soliguide/stg_soliguide__lieux.sql
+++ pipeline/dbt/models/staging/sources/soliguide/stg_soliguide__lieux.sql
@@ -4,24 +4,31 @@ WITH source AS (
 
 final AS (
     SELECT
-        _di_source_id                                         AS "_di_source_id",
-        (data ->> 'updatedAt')::DATE                          AS "updated_at",
-        (data #>> '{position,location,coordinates,0}')::FLOAT AS "position_coordinates_x",
-        (data #>> '{position,location,coordinates,1}')::FLOAT AS "position_coordinates_y",
-        data ->> 'lieu_id'                                    AS "id",
-        data ->> 'lieu_id'                                    AS "lieu_id",
-        data ->> 'name'                                       AS "name",
-        data #>> '{position,ville}'                           AS "position_ville",
-        data ->> 'description'                                AS "description",
-        data ->> 'seo_url'                                    AS "seo_url",
+        _di_source_id                                          AS "_di_source_id",
+        (data ->> 'updatedAt')::DATE                           AS "updated_at",
+        (data #>> '{position,location,coordinates,0}')::FLOAT  AS "position_coordinates_x",
+        (data #>> '{position,location,coordinates,1}')::FLOAT  AS "position_coordinates_y",
+        (data #>> '{modalities,inconditionnel}')::BOOLEAN      AS "modalities__inconditionnel",
+        (data #>> '{modalities,appointment,checked}')::BOOLEAN AS "modalities__appointment__checked",
+        (data #>> '{modalities,inscription,checked}')::BOOLEAN AS "modalities__inscription__checked",
+        (data #>> '{modalities,orientation,checked}')::BOOLEAN AS "modalities__orientation__checked",
+        data ->> 'lieu_id'                                     AS "id",
+        data ->> 'lieu_id'                                     AS "lieu_id",
         -- TODO: entity.phones
-        data #>> '{position,codePostal}'                      AS "position_code_postal",
-        data #>> '{position,adresse}'                         AS "position_adresse",
-        data #>> '{position,complementAdresse}'               AS "position_complement_adresse",
-        data #>> '{position,departement}'                     AS "departement",
-        NULLIF(data #>> '{entity,mail}', '')                  AS "entity_mail",
-        data #>> '{entity,website}'                           AS "entity_website",
-        data -> 'newhours'                                    AS "newhours"
+        data ->> 'name'                                        AS "name",
+        data #>> '{position,ville}'                            AS "position_ville",
+        data ->> 'description'                                 AS "description",
+        data ->> 'seo_url'                                     AS "seo_url",
+        data #>> '{position,codePostal}'                       AS "position_code_postal",
+        data #>> '{position,adresse}'                          AS "position_adresse",
+        data #>> '{position,complementAdresse}'                AS "position_complement_adresse",
+        data #>> '{position,departement}'                      AS "departement",
+        NULLIF(data #>> '{entity,mail}', '')                   AS "entity_mail",
+        data #>> '{entity,website}'                            AS "entity_website",
+        data -> 'newhours'                                     AS "newhours",
+        data #>> '{modalities,appointment,precisions}'         AS "modalities__appointment__precisions",
+        data #>> '{modalities,inscription,precisions}'         AS "modalities__inscription__precisions",
+        data #>> '{modalities,orientation,precisions}'         AS "modalities__orientation__precisions"
     FROM source
 )
 
--- pipeline/dbt/models/staging/sources/soliguide/stg_soliguide__services.sql
+++ pipeline/dbt/models/staging/sources/soliguide/stg_soliguide__services.sql
@@ -4,18 +4,25 @@ WITH source AS (
 
 final AS (
     SELECT
-        source._di_source_id                                AS "_di_source_id",
-        CAST(source.data ->> 'updatedAt' AS DATE)           AS "updated_at",
-        source.data ->> 'lieu_id'                           AS "lieu_id",
-        services.data ->> 'serviceObjectId'                 AS "id",
-        NULLIF(services.data ->> 'name', '')                AS "name",
-        services.data ->> 'categorie'                       AS "categorie",
-        NULLIF(services.data ->> 'description', '')         AS "description",
-        services.data -> 'hours'                            AS "hours",
-        CAST(services.data ->> 'differentHours' AS BOOLEAN) AS "different_hours",
-        CAST(services.data #>> '{close,actif}' AS BOOLEAN)  AS "close__actif",
-        CAST(services.data #>> '{close,dateDebut}' AS DATE) AS "close__date_debut",
-        CAST(services.data #>> '{close,dateFin}' AS DATE)   AS "close__date_fin"
+        source._di_source_id                                                  AS "_di_source_id",
+        CAST(source.data ->> 'updatedAt' AS DATE)                             AS "updated_at",
+        source.data ->> 'lieu_id'                                             AS "lieu_id",
+        services.data ->> 'serviceObjectId'                                   AS "id",
+        NULLIF(services.data ->> 'name', '')                                  AS "name",
+        services.data ->> 'categorie'                                         AS "categorie",
+        NULLIF(services.data ->> 'description', '')                           AS "description",
+        services.data -> 'hours'                                              AS "hours",
+        CAST(services.data ->> 'differentHours' AS BOOLEAN)                   AS "different_hours",
+        CAST(services.data #>> '{close,actif}' AS BOOLEAN)                    AS "close__actif",
+        CAST(services.data #>> '{close,dateDebut}' AS DATE)                   AS "close__date_debut",
+        CAST(services.data #>> '{close,dateFin}' AS DATE)                     AS "close__date_fin",
+        CAST(services.data #>> '{modalities,inconditionnel}' AS BOOLEAN)      AS "modalities__inconditionnel",
+        CAST(services.data #>> '{modalities,appointment,checked}' AS BOOLEAN) AS "modalities__appointment__checked",
+        services.data #>> '{modalities,appointment,precisions}'               AS "modalities__appointment__precisions",
+        CAST(services.data #>> '{modalities,inscription,checked}' AS BOOLEAN) AS "modalities__inscription__checked",
+        services.data #>> '{modalities,inscription,precisions}'               AS "modalities__inscription__precisions",
+        CAST(services.data #>> '{modalities,orientation,checked}' AS BOOLEAN) AS "modalities__orientation__checked",
+        services.data #>> '{modalities,orientation,precisions}'               AS "modalities__orientation__precisions"
     FROM
         source,
         LATERAL(SELECT * FROM JSONB_PATH_QUERY(source.data, '$.services_all[*]')) AS services (data)
