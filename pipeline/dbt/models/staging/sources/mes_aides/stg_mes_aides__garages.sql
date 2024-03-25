4b9beacc59061a3d13d37b337979a2d81cd559ab
Valentin Matton <taq@protonmail.ch>
2024-03-25 14:50:56 +0100

    chore(pipeline): clean up mes aides garages stg

--- pipeline/dbt/models/staging/sources/mes_aides/stg_mes_aides__garages.sql
+++ pipeline/dbt/models/staging/sources/mes_aides/stg_mes_aides__garages.sql
@@ -4,26 +4,26 @@ WITH source AS (
 
 final AS (
     SELECT
-        _di_source_id                                              AS "_di_source_id",
-        (data #>> '{fields,Créé le}')::TIMESTAMP WITH TIME ZONE    AS "cree_le",
-        (data #>> '{fields,Modifié le}')::TIMESTAMP WITH TIME ZONE AS "modifie_le",
-        (data #>> '{fields,En Ligne}')::BOOLEAN                    AS "en_ligne",
+        _di_source_id                                                    AS "_di_source_id",
+        data #>> '{fields,Adresse}'                                      AS "adresse",
+        data #>> '{fields,Code INSEE}'                                   AS "code_insee",
+        data #>> '{fields,Code Postal}'                                  AS "code_postal",
+        CAST(data #>> '{fields,Créé le}' AS DATE)                        AS "cree_le",
+        data #>> '{fields,Département Nom}'                              AS "departement_nom",
+        data #>> '{fields,Email}'                                        AS "email",
+        CAST(data #>> '{fields,En Ligne}' AS BOOLEAN)                    AS "en_ligne",
+        data #>> '{fields,ID}'                                           AS "id",
         -- some rows are formatted as `LAT, LAT`... use first value
-        SPLIT_PART(data #>> '{fields,Longitude}', ',', 1)::FLOAT   AS "ville_longitude",
-        SPLIT_PART(data #>> '{fields,Latitude}', ',', 1)::FLOAT    AS "ville_latitude",
-        data #>> '{fields,ID}'                                     AS "id",
-        data #>> '{fields,Nom}'                                    AS "nom",
-        data #>> '{fields,Ville Nom}'                              AS "ville_nom",
-        data #>> '{fields,Code Postal}'                            AS "code_postal",
-        data #>> '{fields,Code INSEE}'                             AS "code_insee",
-        data #>> '{fields,Adresse}'                                AS "adresse",
-        data #>> '{fields,Téléphone}'                              AS "telephone",
-        data #>> '{fields,SIRET}'                                  AS "siret",
-        data #>> '{fields,Email}'                                  AS "email",
-        data #>> '{fields,Url}'                                    AS "url",
-        data #>> '{fields,Département Nom}'                        AS "departement_nom",
-        data #>> '{fields,Région Nom}'                             AS "region_nom",
-        data #>> '{fields,Type}'                                   AS "type"
+        CAST(SPLIT_PART(data #>> '{fields,Latitude}', ',', 1) AS FLOAT)  AS "latitude",
+        CAST(SPLIT_PART(data #>> '{fields,Longitude}', ',', 1) AS FLOAT) AS "longitude",
+        CAST(data #>> '{fields,Modifié le}' AS DATE)                     AS "modifie_le",
+        data #>> '{fields,Nom}'                                          AS "nom",
+        data #>> '{fields,Région Nom}'                                   AS "region_nom",
+        data #>> '{fields,SIRET}'                                        AS "siret",
+        data #>> '{fields,Téléphone}'                                    AS "telephone",
+        data #>> '{fields,Type}'                                         AS "type",
+        data #>> '{fields,Url}'                                          AS "url",
+        data #>> '{fields,Ville Nom}'                                    AS "ville_nom"
     FROM source
 )
 
