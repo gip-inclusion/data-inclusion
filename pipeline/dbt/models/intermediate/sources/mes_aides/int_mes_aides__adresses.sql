8fba9d90f348e52ea1da1737fae020f0722cad08
Valentin Matton <taq@protonmail.ch>
2024-03-25 14:50:56 +0100

    chore(pipeline): clean up mes aides garages stg

--- pipeline/dbt/models/intermediate/sources/mes_aides/int_mes_aides__adresses.sql
+++ pipeline/dbt/models/intermediate/sources/mes_aides/int_mes_aides__adresses.sql
@@ -4,15 +4,15 @@ WITH garages AS (
 
 final AS (
     SELECT
-        id              AS "id",
-        ville_nom       AS "commune",
-        code_postal     AS "code_postal",
-        code_insee      AS "code_insee",
-        adresse         AS "adresse",
-        NULL            AS "complement_adresse",
-        ville_longitude AS "longitude",
-        ville_latitude  AS "latitude",
-        _di_source_id   AS "source"
+        id            AS "id",
+        ville_nom     AS "commune",
+        code_postal   AS "code_postal",
+        code_insee    AS "code_insee",
+        adresse       AS "adresse",
+        NULL          AS "complement_adresse",
+        longitude     AS "longitude",
+        latitude      AS "latitude",
+        _di_source_id AS "source"
     FROM garages
 )
 
