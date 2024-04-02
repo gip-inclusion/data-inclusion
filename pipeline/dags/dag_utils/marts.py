47b28bb792cb36d3dc88bead83fca31eb96f8acb
Victor Perron <victor@iso3103.net>
2024-04-02 11:49:49 +0200

    chore(dags) : Add logging to the S3 table exports

--- pipeline/dags/dag_utils/marts.py
+++ pipeline/dags/dag_utils/marts.py
@@ -4,10 +4,15 @@ from dag_utils.virtualenvs import PYTHON_BIN_PATH
 
 
 def _export_di_dataset_to_s3(logical_date, run_id):
+    import logging
+
     from airflow.providers.amazon.aws.hooks import s3
     from airflow.providers.postgres.hooks import postgres
 
     from dag_utils import date
+    from dag_utils.sources.utils import log_df_info
+
+    logger = logging.getLogger(__name__)
 
     pg_hook = postgres.PostgresHook(postgres_conn_id="pg")
     s3_hook = s3.S3Hook(aws_conn_id="s3")
@@ -20,8 +25,13 @@ def _export_di_dataset_to_s3(logical_date, run_id):
         key = f"{prefix}{ressource}.parquet"
         query = f"SELECT * FROM public.{table_name}"
 
+        logger.info("downloading data from query='%s'", query)
         df = pg_hook.get_pandas_df(sql=query)
+
+        log_df_info(df, logger=logger)
         bytes_data = df.to_parquet(compression="gzip")
+
+        logger.info("uploading data to bucket='%s'", key)
         s3_hook.load_bytes(bytes_data, key=key, replace=True)
 
 
