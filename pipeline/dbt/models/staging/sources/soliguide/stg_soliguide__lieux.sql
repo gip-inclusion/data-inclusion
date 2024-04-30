a6c1365ff8a33ecf44ca7cbba8865be0dcc1db15
Valentin Matton <taq@protonmail.ch>
2024-04-30 12:56:54 +0200

    chore(pipeline): update soliguide position fields
    * positions fields were translated to english
    * use new cityCode field
    * filter data by country code

--- pipeline/dbt/models/staging/sources/soliguide/stg_soliguide__lieux.sql
+++ pipeline/dbt/models/staging/sources/soliguide/stg_soliguide__lieux.sql
@@ -2,12 +2,12 @@ WITH source AS (
     {{ stg_source_header('soliguide', 'lieux') }}
 ),
 
-final AS (
+lieux AS (
     SELECT
         _di_source_id                                          AS "_di_source_id",
         (data ->> 'updatedAt')::DATE                           AS "updated_at",
-        (data #>> '{position,location,coordinates,0}')::FLOAT  AS "position_coordinates_x",
-        (data #>> '{position,location,coordinates,1}')::FLOAT  AS "position_coordinates_y",
+        (data #>> '{position,location,coordinates,0}')::FLOAT  AS "position__coordinates__x",
+        (data #>> '{position,location,coordinates,1}')::FLOAT  AS "position__coordinates__y",
         (data #>> '{modalities,inconditionnel}')::BOOLEAN      AS "modalities__inconditionnel",
         (data #>> '{modalities,appointment,checked}')::BOOLEAN AS "modalities__appointment__checked",
         (data #>> '{modalities,inscription,checked}')::BOOLEAN AS "modalities__inscription__checked",
@@ -16,13 +16,15 @@ final AS (
         data ->> 'lieu_id'                                     AS "lieu_id",
         -- TODO: entity.phones
         data ->> 'name'                                        AS "name",
-        data #>> '{position,ville}'                            AS "position_ville",
+        data #>> '{position,city}'                             AS "position__city",
+        data #>> '{position,cityCode}'                         AS "position__city_code",
+        data #>> '{position,country}'                          AS "position__country",
         data ->> 'description'                                 AS "description",
         data ->> 'seo_url'                                     AS "seo_url",
-        data #>> '{position,codePostal}'                       AS "position_code_postal",
-        data #>> '{position,adresse}'                          AS "position_adresse",
-        data #>> '{position,complementAdresse}'                AS "position_complement_adresse",
-        data #>> '{position,departement}'                      AS "departement",
+        data #>> '{position,postalCode}'                       AS "position__postal_code",
+        data #>> '{position,address}'                          AS "position__address",
+        data #>> '{position,additionalInformation}'            AS "position__additional_information",
+        data #>> '{position,department}'                       AS "position__department",
         NULLIF(data #>> '{entity,mail}', '')                   AS "entity_mail",
         NULLIF(data #>> '{entity,website}', '')                AS "entity_website",
         data -> 'newhours'                                     AS "newhours",
@@ -30,6 +32,12 @@ final AS (
         data #>> '{modalities,inscription,precisions}'         AS "modalities__inscription__precisions",
         data #>> '{modalities,orientation,precisions}'         AS "modalities__orientation__precisions"
     FROM source
+),
+
+final AS (
+    SELECT *
+    FROM lieux
+    WHERE position__country = 'fr'
 )
 
 SELECT * FROM final
