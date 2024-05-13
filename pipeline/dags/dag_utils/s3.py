a82416438e62d304b9762bc174ccf2203dcd9b05
Valentin Matton <taq@protonmail.ch>
2024-05-13 13:13:45 +0200

    chore(pipeline): log calls to s3

--- pipeline/dags/dag_utils/s3.py
+++ pipeline/dags/dag_utils/s3.py
@@ -1,9 +1,12 @@
 import io
+import logging
 from pathlib import Path
 
 from airflow.providers.amazon.aws.hooks import s3
 
 from . import date
+
+logger = logging.getLogger(__name__)
 
 
 def source_file_path(
@@ -26,6 +29,7 @@ def store_content(
     content: bytes,
 ):
     s3_hook = s3.S3Hook(aws_conn_id="s3")
+    logger.info("Storing content to %s", path)
     with io.BytesIO(content) as buf:
         s3_hook.load_file_obj(
             key=path,
@@ -36,4 +40,5 @@ def store_content(
 
 def download_file(path: str) -> Path:
     s3_hook = s3.S3Hook(aws_conn_id="s3")
+    logger.info("Downloading file from %s", path)
     return Path(s3_hook.download_file(key=path))
