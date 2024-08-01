072e796313aa78ddc1c0c2f886d182a70841b071
Hugo Lecuyer <hugo.lecuyer1@gmail.com>
2024-08-01 14:55:03 +0200

    feat(FT): add horaires in open street map format

--- pipeline/dbt/models/staging/sources/france_travail/stg_france_travail__agences.sql
+++ pipeline/dbt/models/staging/sources/france_travail/stg_france_travail__agences.sql
@@ -4,21 +4,22 @@ WITH source AS (
 
 final AS (
     SELECT
-        _di_source_id                                                                                                                                          AS "_di_source_id",
-        CURRENT_DATE                                                                                                                                           AS "date_maj",
-        CASE WHEN data ->> 'dispositifADEDA' = 'true' THEN 'https://www.francetravail.fr/actualites/a-laffiche/2022/adeda-un-dispositif-pour-mieux-a.html' END AS "accessibilite",
-        CAST(data #>> '{adressePrincipale,gpsLat}' AS FLOAT)                                                                                                   AS "latitude",
-        CAST(data #>> '{adressePrincipale,gpsLon}' AS FLOAT)                                                                                                   AS "longitude",
-        data #>> '{adressePrincipale,ligne4}'                                                                                                                  AS "adresse",
-        data #>> '{adressePrincipale,ligne3}'                                                                                                                  AS "complement_adresse",
-        data #>> '{adressePrincipale,communeImplantation}'                                                                                                     AS "code_insee",
-        data #>> '{adressePrincipale,bureauDistributeur}'                                                                                                      AS "code_postal",
-        data #>> '{contact,email}'                                                                                                                             AS "courriel",
-        data #>> '{contact,telephonePublic}'                                                                                                                   AS "telephone",
-        data ->> 'code'                                                                                                                                        AS "id",
-        data ->> 'libelleEtendu'                                                                                                                               AS "nom",
-        data ->> 'siret'                                                                                                                                       AS "siret",
-        data ->> 'type'                                                                                                                                        AS "typologie"
+        _di_source_id                                                                                                                                        AS "_di_source_id",
+        CURRENT_DATE                                                                                                                                         AS "date_maj",
+        CASE WHEN data ->> 'dispositifADEDA' = 'true' THEN 'https://www.pole-emploi.fr/actualites/a-laffiche/2022/adeda-un-dispositif-pour-mieux-a.html' END AS "accessibilite",
+        CAST(data #>> '{adressePrincipale,gpsLat}' AS FLOAT)                                                                                                 AS "latitude",
+        CAST(data #>> '{adressePrincipale,gpsLon}' AS FLOAT)                                                                                                 AS "longitude",
+        data #>> '{adressePrincipale,ligne4}'                                                                                                                AS "adresse",
+        data #>> '{adressePrincipale,ligne3}'                                                                                                                AS "complement_adresse",
+        data #>> '{adressePrincipale,communeImplantation}'                                                                                                   AS "code_insee",
+        data #>> '{adressePrincipale,bureauDistributeur}'                                                                                                    AS "code_postal",
+        data #>> '{contact,email}'                                                                                                                           AS "courriel",
+        data ->> 'horaires_open_street_map'                                                                                                                  AS "horaires_open_street_map",
+        data #>> '{contact,telephonePublic}'                                                                                                                 AS "telephone",
+        data ->> 'code'                                                                                                                                      AS "id",
+        data ->> 'libelleEtendu'                                                                                                                             AS "nom",
+        data ->> 'siret'                                                                                                                                     AS "siret",
+        data ->> 'type'                                                                                                                                      AS "typologie"
     FROM source
 )
 
