2dc4994b70ab19dba484c17cfc1371bacb6f527c
Victor Perron <victor@iso3103.net>
2024-08-05 10:40:23 +0200

    chore(pipeline) : Fix reseau_alpha adresses
    They are often on 3 lines, when they're not on one.
    
    	[ RECORD 1 ]---------------------------------------------------------------------------------------
    	source | cd72
    	adresse | 31, allée Claude-Debussy +
    	| Pôle associatif Coluche +
    	| 1er étage à droite +
    	| Porte n°102
    	-[ RECORD 2 ]---------------------------------------------------------------------------------------
    	source | reseau-alpha
    	adresse | Centre Social et Culturel des Fossés-Jean +
    	| 1, RUE JULES MICHELET +
    	| Espace Jacqes Chirac
    	-[ RECORD 3 ]---------------------------------------------------------------------------------------
    	source | reseau-alpha
    	adresse | c/o Office Municipal de la Culture (hors vacances scolaires) +
    	| 120, rue Roger François
    	-[ RECORD 4 ]---------------------------------------------------------------------------------------
    	source | reseau-alpha
    	adresse | Centre culturel Franco-berbères +
    	| 37, Avenue Paul Vaillant Couturier +
    	| Au dessus de la piscine municipale
    
    When they are on 3 lines:
    - the first one is usually the name of a place
    - the second one is the actual address
    - the third one, if present, is a complement
    
    We now split them and use the second line as address and third line
    as complement:
    
    	-[ RECORD 110 ]-------------+-------------------------------------------------------------------------------------------
    	id                          | 1158
    	content__adresse            | 37, Avenue Paul Vaillant Couturier
    	content__complement_adresse | Au dessus de la piscine municipale
    	-[ RECORD 111 ]-------------+-------------------------------------------------------------------------------------------
    	id                          | 116
    	content__adresse            | Secours Catholique 91240 21, bis rue Saint-Saens
    	content__complement_adresse |
    	-[ RECORD 112 ]-------------+-------------------------------------------------------------------------------------------
    	id                          | 1160
    	content__adresse            | ex-mairie de Paris 1° 2, rue Perrault
    	content__complement_adresse |
    	-[ RECORD 113 ]-------------+-------------------------------------------------------------------------------------------
    	id                          | 1161
    	content__adresse            | Rue de la forêt
    	content__complement_adresse |
    	-[ RECORD 114 ]-------------+-------------------------------------------------------------------------------------------
    	id                          | 1163
    	content__adresse            | Ecole Normale Supérieure de Paris-Saclay 4, Avenue des Sciences
    	content__complement_adresse |

--- pipeline/dbt/models/staging/sources/reseau_alpha/stg_reseau_alpha__structures.sql
+++ pipeline/dbt/models/staging/sources/reseau_alpha/stg_reseau_alpha__structures.sql
@@ -13,7 +13,7 @@ adresses AS (
         TRIM(SUBSTRING(source.data ->> 'content__adresse' FROM '^(.+)\s\d{5} - .+$')) AS "content__adresse"
     FROM
         source,
-        LATERAL(SELECT * FROM JSONB_PATH_QUERY(source.data, '$.adresses[*]')) AS adresses (data)
+        LATERAL (SELECT * FROM JSONB_PATH_QUERY(source.data, '$.adresses[*]')) AS adresses (data)
     WHERE
         -- a minority of structures have more than one addresses, which is not managed by
         -- the data·inclusion schema. Skip these addresses.
@@ -22,26 +22,27 @@ adresses AS (
 
 final AS (
     SELECT
-        source._di_source_id                                                                                 AS "_di_source_id",
-        adresses.adresses__ville                                                                             AS "adresses__ville",
-        adresses.adresses__latitude                                                                          AS "adresses__latitude",
-        adresses.adresses__longitude                                                                         AS "adresses__longitude",
-        adresses.adresses__code_postal                                                                       AS "adresses__code_postal",
-        adresses.content__adresse                                                                            AS "content__adresse",
-        CAST(ARRAY(SELECT * FROM JSONB_ARRAY_ELEMENTS_TEXT(source.data -> 'activitesFormation')) AS TEXT []) AS "activites_formation",
-        source.data ->> 'id'                                                                                 AS "id",
-        source.data ->> 'nom'                                                                                AS "nom",
-        source.data ->> 'url'                                                                                AS "url",
-        source.data ->> 'logo'                                                                               AS "logo",
-        source.data ->> 'type'                                                                               AS "type",
-        source.data ->> 'description'                                                                        AS "description",
+        source._di_source_id                                                                                   AS "_di_source_id",
+        adresses.adresses__ville                                                                               AS "adresses__ville",
+        adresses.adresses__latitude                                                                            AS "adresses__latitude",
+        adresses.adresses__longitude                                                                           AS "adresses__longitude",
+        adresses.adresses__code_postal                                                                         AS "adresses__code_postal",
+        COALESCE(NULLIF(TRIM(SPLIT_PART(adresses.content__adresse, E'\n', 2)), ''), adresses.content__adresse) AS "content__adresse",
+        NULLIF(TRIM(SPLIT_PART(adresses.content__adresse, E'\n', 3)), '')                                      AS "content__complement_adresse",
+        CAST(ARRAY(SELECT * FROM JSONB_ARRAY_ELEMENTS_TEXT(source.data -> 'activitesFormation')) AS TEXT [])   AS "activites_formation",
+        source.data ->> 'id'                                                                                   AS "id",
+        source.data ->> 'nom'                                                                                  AS "nom",
+        source.data ->> 'url'                                                                                  AS "url",
+        source.data ->> 'logo'                                                                                 AS "logo",
+        source.data ->> 'type'                                                                                 AS "type",
+        source.data ->> 'description'                                                                          AS "description",
         TO_DATE(
             SUBSTRING(source.data ->> 'content__date_maj' FROM 'Date de la dernière modification : (.*)'),
             'DD TMmonth YYYY'
-        )                                                                                                    AS "content__date_maj",
-        TRIM(source.data ->> 'content__telephone')                                                           AS "content__telephone",
-        TRIM(source.data ->> 'content__courriel')                                                            AS "content__courriel",
-        TRIM(source.data ->> 'content__site_web')                                                            AS "content__site_web"
+        )                                                                                                      AS "content__date_maj",
+        TRIM(source.data ->> 'content__telephone')                                                             AS "content__telephone",
+        TRIM(source.data ->> 'content__courriel')                                                              AS "content__courriel",
+        TRIM(source.data ->> 'content__site_web')                                                              AS "content__site_web"
     FROM source
     LEFT JOIN adresses ON source.data ->> 'id' = adresses.structure_id
 )
