5360d9377f47709fc0938137cfef252eba005aca
Victor Perron <victor@iso3103.net>
2024-08-15 11:27:11 +0200

    chore(pipeline) : Remove the condition about 0xad char in dora contacts
    This has since been fixed.

--- pipeline/dbt/models/intermediate/sources/dora/int_dora__contacts.sql
+++ pipeline/dbt/models/intermediate/sources/dora/int_dora__contacts.sql
@@ -1,17 +1,15 @@
 WITH structure_contacts AS (
     SELECT
-        -- For an unknown reason, some emails are polluted by soft hyphen characters
-        REPLACE(courriel, U&'\00ad', '') AS "courriel",
-        'dora:structures:' || id         AS contact_uid
+        courriel                 AS "courriel",
+        'dora:structures:' || id AS contact_uid
     FROM {{ ref('stg_dora__structures') }}
     WHERE courriel IS NOT NULL
 ),
 
 service_contacts AS (
     SELECT
-        -- For an unknown reason, some emails are polluted by soft hyphen characters
-        REPLACE(courriel, U&'\00ad', '') AS "courriel",
-        'dora:services:' || id           AS contact_uid
+        courriel               AS "courriel",
+        'dora:services:' || id AS contact_uid
     FROM {{ ref('stg_dora__services') }}
     WHERE courriel IS NOT NULL
 ),
