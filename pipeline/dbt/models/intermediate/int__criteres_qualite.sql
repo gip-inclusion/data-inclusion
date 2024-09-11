185779b4a8e94697f443d7aa89a9665c3d6762f4
Valentin Matton <taq@protonmail.ch>
2024-09-11 21:51:23 +0200

    feat(pipeline): add scoring model
    This implementation uses a plpython udf that calls
    the scoring api of the data inclusion schema.

---
+++ pipeline/dbt/models/intermediate/int__criteres_qualite.sql
@@ -0,0 +1,16 @@
+WITH services AS (
+    SELECT * FROM {{ ref('int__union_services__enhanced') }}
+),
+
+final AS (
+    SELECT
+        services._di_surrogate_id AS "service_id",
+        scores.score_critere      AS "score_critere",
+        scores.nom_critere        AS "nom_critere",
+        scores.score_ligne        AS "score_ligne"
+    FROM
+        services,
+        LATERAL (SELECT * FROM processings.score(TO_JSONB(services))) AS scores
+)
+
+SELECT * FROM final
