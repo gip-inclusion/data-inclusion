45fbbf3a43da5b0fc727d48f3ee3c11ac80b3b31
Valentin Matton <taq@protonmail.ch>
2024-10-18 16:07:13 +0200

    wip

--- pipeline/dbt/models/intermediate/int__union_services.sql
+++ pipeline/dbt/models/intermediate/int__union_services.sql
@@ -36,79 +36,13 @@ WITH services AS (
     }}
 ),
 
-unnested_brevo_contacts AS (
-    SELECT DISTINCT
-        UNNEST(contact_uids) AS id,
-        date_di_rgpd_opposition,
-        est_interdit
-    FROM {{ ref('int_brevo__contacts') }}
-),
-
-service_contacts AS (
-    SELECT
-        SPLIT_PART(id, ':', 1) AS source,
-        SPLIT_PART(id, ':', 3) AS id,
-        date_di_rgpd_opposition,
-        est_interdit
-    FROM unnested_brevo_contacts
-    WHERE SPLIT_PART(id, ':', 2) = 'services'
-),
-
-contacts_opposes AS (
-    SELECT
-        id,
-        source
-    FROM service_contacts
-    WHERE date_di_rgpd_opposition IS NOT NULL
-),
-
-contacts_hardbounced AS (
-    SELECT
-        id,
-        source
-    FROM service_contacts
-    WHERE est_interdit = TRUE
-),
-
-services_with_contacts AS (
-    SELECT
-        -- NOTE(vperron) : The int__dora__services below is a ruse, the FROM clause
-        -- still refers to the `services` CTE. But we need a dbt_utils.star() and we need
-        -- to reference "something" that has the same columns as int__union_services,
-        -- but not itself as it would create a cycle.
-        -- Also, directly referencing all the columns by name makes the tests super painful:
-        -- suddenly all the columns have to be emulated in the fixtures.
-        -- But we still need to remove some columns :'(
-        {{ dbt_utils.star(from=ref('int_dora__services'), relation_alias='services', except=["contact_nom_prenom", "courriel", "telephone"]) }},
-        CASE
-            WHEN contacts_opposes.id IS NULL
-                THEN services.contact_nom_prenom
-        END                                 AS "contact_nom_prenom",
-        CASE
-            WHEN contacts_opposes.id IS NULL OR contacts_hardbounced.id IS NULL
-                THEN services.courriel
-        END                                 AS "courriel",
-        CASE
-            WHEN contacts_opposes.id IS NULL
-                THEN services.telephone
-        END                                 AS "telephone",
-        contacts_opposes.id IS NOT NULL     AS "contact_opposes",
-        contacts_hardbounced.id IS NOT NULL AS "contact_hardbounced"
-    FROM services
-    -- Enable USING instead of ON for the JOIN condition to avoid duplicate columns
-    -- noqa: disable=structure.using
-    LEFT JOIN contacts_opposes USING (id, source)
-    LEFT JOIN contacts_hardbounced USING (id, source)
-    -- noqa: enable=structure.using
-),
-
 final AS (
     SELECT
-        services.*,
-        services.source || '-' || services.id           AS "_di_surrogate_id",
-        services.source || '-' || services.structure_id AS "_di_structure_surrogate_id",
-        services.source || '-' || services.adresse_id   AS "_di_adresse_surrogate_id"
-    FROM services_with_contacts AS services
+        *,
+        source || '-' || id           AS "_di_surrogate_id",
+        source || '-' || structure_id AS "_di_structure_surrogate_id",
+        source || '-' || adresse_id   AS "_di_adresse_surrogate_id"
+    FROM services
 )
 
 SELECT * FROM final
--- pipeline/dbt/models/intermediate/sources/mediation_numerique/int_mediation_numerique__contacts.sql
+++ pipeline/dbt/models/intermediate/sources/mediation_numerique/int_mediation_numerique__contacts.sql
@@ -1,7 +1,11 @@
 WITH final AS (
     SELECT
+        id                                      AS "id",
+        _di_source_id                           AS "source",
         courriel                                AS "courriel",
-        'mediation-numerique:structures:' || id AS contact_uid
+        telephone                               AS "telephone",
+        contact_nom_prenom                      AS "contact_nom_prenom",
+        'mediation-numerique:structures:' || id AS "contact_uid"
     FROM {{ ref('stg_mediation_numerique__structures') }}
     WHERE courriel IS NOT NULL
 )
