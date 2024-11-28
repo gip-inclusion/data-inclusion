5464eb05a48838602b7d92eadfdd270a1424b4a9
Victor Perron <victor@iso3103.net>
2024-11-28 16:04:14 +0100

    fix(pipeline) : Ignore dora structures in Soliguide
    We now can use the "sources" field to void duplicates.
    
    --------------- BEFORE
    data-inclusion=# select count(*) from public_staging.stg_soliguide__lieux ;
    -[ RECORD 1 ]
    count | 24701
    
    --------------- AFTER
    data-inclusion=# select count(*) from public_staging.stg_soliguide__lieux ;
    -[ RECORD 1 ]
    count | 22878

--- pipeline/dbt/models/staging/sources/soliguide/stg_soliguide__phones.sql
+++ pipeline/dbt/models/staging/sources/soliguide/stg_soliguide__phones.sql
@@ -2,15 +2,22 @@ WITH source AS (
     {{ stg_source_header('soliguide', 'lieux') }}
 ),
 
-final AS (
+phones AS (
     SELECT
-        source._di_source_id                AS "_di_source_id",
-        source.data ->> 'lieu_id'           AS "lieu_id",
-        NULLIF(phones.data ->> 'label', '') AS "label",
-        phones.data ->> 'phoneNumber'       AS "phone_number"
+        source._di_source_id                      AS "_di_source_id",
+        source.data ->> 'lieu_id'                 AS "lieu_id",
+        NULLIF(inline_phone.data ->> 'label', '') AS "label",
+        inline_phone.data ->> 'phoneNumber'       AS "phone_number",
+        source.data -> 'sources'                  AS "sources"
     FROM
         source,
-        LATERAL(SELECT * FROM JSONB_PATH_QUERY(source.data, '$.entity.phones[*]')) AS phones (data)
+        LATERAL (SELECT * FROM JSONB_PATH_QUERY(source.data, '$.entity.phones[*]')) AS inline_phone (data)
+),
+
+final AS (
+    SELECT *
+    FROM phones
+    WHERE NOT sources @> '[{"name": "dora"}]'
 )
 
 SELECT * FROM final
