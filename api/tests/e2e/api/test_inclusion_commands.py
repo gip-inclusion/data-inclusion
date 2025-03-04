defe455722300bd8d8cc1433d588a0f908ac1778
Victor Perron <victor@iso3103.net>
2025-03-04 17:22:58 +0100

    chore(api) : Fix the test_inclusion_commands DB query
    The test was not using the test db session, because of a little commit
    mishap.

--- api/tests/e2e/api/test_inclusion_commands.py
+++ api/tests/e2e/api/test_inclusion_commands.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-from data_inclusion.api.core import db
 from data_inclusion.api.inclusion_data import commands, models
 
 
@@ -30,41 +29,40 @@ def test_store_inclusion_data_scores_doublons(db_session):
 
     commands.store_inclusion_data(db_session, structures_df, services_df)
 
-    with db.SessionLocal() as session:
-        db_structures = session.query(models.Structure).all()
-        structures_by_id = {s.id: s for s in db_structures}
-        assert structures_by_id["id1"].score_qualite == 0.75  # average of 0.7 and 0.8
-        assert structures_by_id["id1"].doublons == [
-            {
-                "accessibilite": None,
-                "adresse": None,
-                "antenne": None,
-                "code_insee": None,
-                "code_postal": None,
-                "commune": None,
-                "complement_adresse": None,
-                "courriel": None,
-                "date_maj": "2023-01-01",
-                "horaires_ouverture": None,
-                "id": "id2",
-                "labels_autres": None,
-                "labels_nationaux": None,
-                "latitude": None,
-                "lien_source": None,
-                "longitude": None,
-                "nom": "Structure 2",
-                "presentation_detail": None,
-                "presentation_resume": None,
-                "rna": None,
-                "siret": None,
-                "site_web": None,
-                "source": "source2",
-                "telephone": None,
-                "thematiques": None,
-                "typologie": None,
-            }
-        ]
-        assert structures_by_id["id2"].score_qualite == 0.5
-        assert structures_by_id["id2"].doublons[0]["id"] == "id1"
-        assert structures_by_id["id3"].score_qualite == 0.0  # no services, default to 0
-        assert structures_by_id["id3"].doublons == []
+    db_structures = db_session.query(models.Structure).all()
+    structures_by_id = {s.id: s for s in db_structures}
+    assert structures_by_id["id1"].score_qualite == 0.75  # average of 0.7 and 0.8
+    assert structures_by_id["id1"].doublons == [
+        {
+            "accessibilite": None,
+            "adresse": None,
+            "antenne": None,
+            "code_insee": None,
+            "code_postal": None,
+            "commune": None,
+            "complement_adresse": None,
+            "courriel": None,
+            "date_maj": "2023-01-01",
+            "horaires_ouverture": None,
+            "id": "id2",
+            "labels_autres": None,
+            "labels_nationaux": None,
+            "latitude": None,
+            "lien_source": None,
+            "longitude": None,
+            "nom": "Structure 2",
+            "presentation_detail": None,
+            "presentation_resume": None,
+            "rna": None,
+            "siret": None,
+            "site_web": None,
+            "source": "source2",
+            "telephone": None,
+            "thematiques": None,
+            "typologie": None,
+        }
+    ]
+    assert structures_by_id["id2"].score_qualite == 0.5
+    assert structures_by_id["id2"].doublons[0]["id"] == "id1"
+    assert structures_by_id["id3"].score_qualite == 0.0  # no services, default to 0
+    assert structures_by_id["id3"].doublons == []
