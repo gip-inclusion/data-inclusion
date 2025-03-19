bef50481450524995fe2e530af0d422c606fd134
Victor Perron <victor@iso3103.net>
2025-03-19 15:21:30 +0100

    feat(pipeline): Sync Brevo emails through pl/Python

--- pipeline/dbt/models/intermediate/int__union_contacts__enhanced.sql
+++ pipeline/dbt/models/intermediate/int__union_contacts__enhanced.sql
@@ -2,43 +2,43 @@ WITH contacts AS (
     SELECT * FROM {{ ref('int__union_contacts') }}
 ),
 
-rgpd_notices AS (
+courriels_verifies AS (
     SELECT
         courriel,
         has_hardbounced,
         was_objected_to
-    FROM {{ ref('int_brevo__contacts') }}
+    FROM {{ ref('int__courriels_verifies') }}
 ),
 
 final AS (
     SELECT
-        contacts._di_surrogate_id    AS "_di_surrogate_id",
-        contacts.id                  AS "id",
-        contacts.source              AS "source",
+        contacts._di_surrogate_id          AS "_di_surrogate_id",
+        contacts.id                        AS "id",
+        contacts.source                    AS "source",
         CASE
             WHEN
-                rgpd_notices.courriel IS NULL
-                OR NOT rgpd_notices.was_objected_to
+                courriels_verifies.courriel IS NULL
+                OR NOT courriels_verifies.was_objected_to
                 THEN contacts.contact_nom_prenom
-        END                          AS "contact_nom_prenom",
+        END                                AS "contact_nom_prenom",
         CASE
             WHEN
-                rgpd_notices.courriel IS NULL
-                OR (NOT rgpd_notices.was_objected_to AND NOT rgpd_notices.has_hardbounced)
+                courriels_verifies.courriel IS NULL
+                OR (NOT courriels_verifies.was_objected_to AND NOT courriels_verifies.has_hardbounced)
                 THEN contacts.courriel
-        END                          AS "courriel",
+        END                                AS "courriel",
         CASE
             WHEN
-                rgpd_notices.courriel IS NULL
-                OR NOT rgpd_notices.was_objected_to
+                courriels_verifies.courriel IS NULL
+                OR NOT courriels_verifies.was_objected_to
                 THEN contacts.telephone
-        END                          AS "telephone",
-        rgpd_notices.was_objected_to AS "rgpd_notice_was_objected_to",
-        rgpd_notices.has_hardbounced AS "rgpd_notice_has_hardbounced"
+        END                                AS "telephone",
+        courriels_verifies.was_objected_to AS "rgpd_notice_was_objected_to",
+        courriels_verifies.has_hardbounced AS "rgpd_notice_has_hardbounced"
     FROM contacts
-    LEFT JOIN rgpd_notices
+    LEFT JOIN courriels_verifies
         ON
-            contacts.courriel = rgpd_notices.courriel
+            contacts.courriel = courriels_verifies.courriel
 )
 
 SELECT * FROM final
