cecfd79a87e763f552337f5a5693581b6904d6b9
Valentin Matton <taq@protonmail.ch>
2025-04-01 16:13:32 +0200

    chore(pipeline): clean up soliguide mapping

--- pipeline/dbt/models/intermediate/sources/soliguide/int_soliguide__services.sql
+++ pipeline/dbt/models/intermediate/sources/soliguide/int_soliguide__services.sql
@@ -24,81 +24,8 @@ publics AS (
     SELECT * FROM {{ ref('stg_soliguide__lieux__publics__other') }}
 ),
 
-thematiques AS (
-    SELECT * FROM {{ ref('thematiques') }}
-),
-
-di_thematique_by_soliguide_category_code AS (
-    SELECT x.*
-    FROM (
-        VALUES
-        -- Mapping: https://grist.incubateur.net/o/datainclusion/mDqdLZVGVmER/Mappings/p/4
-        ('access_to_housing', ARRAY['logement-hebergement--etre-accompagne-pour-se-loger']),
-        ('accomodation_and_housing', ARRAY['logement-hebergement']),
-        ('addiction', ARRAY['sante--faire-face-a-une-situation-daddiction']),
-        ('administrative_assistance', ARRAY['acces-aux-droits-et-citoyennete--connaitre-ses-droits', 'acces-aux-droits-et-citoyennete--accompagnement-dans-les-demarches-administratives']),
-        ('baby_parcel', ARRAY['famille--soutien-a-la-parentalite']),
-        ('babysitting', ARRAY['famille--garde-denfants']),
-        ('budget_advice', ARRAY(
-            SELECT thematiques.value FROM thematiques
-            WHERE thematiques.value ~ '^gestion-financiere--'
-        )),
-        ('carpooling', ARRAY['mobilite--comprendre-et-utiliser-les-transports-en-commun']),
-        ('chauffeur_driven_transport', ARRAY['mobilite--comprendre-et-utiliser-les-transports-en-commun']),
-        ('child_care', ARRAY['sante--accompagnement-de-la-femme-enceinte-du-bebe-et-du-jeune-enfant']),
-        ('citizen_housing', ARRAY['logement-hebergement']),
-        ('clothing', ARRAY['equipement-et-alimentation--habillement']),
-        ('computers_at_your_disposal', ARRAY['numerique--acceder-a-du-materiel']),
-        ('cooking_workshop', ARRAY['equipement-et-alimentation--alimentation']),
-        ('day_hosting', ARRAY['remobilisation--lien-social']),
-        ('dental_care', ARRAY['sante--acces-aux-soins']),
-        ('digital_tools_training', ARRAY(
-            SELECT thematiques.value FROM thematiques
-            WHERE thematiques.value ~ '^numerique--'
-        )),
-        ('disability_advice', ARRAY['handicap']),
-        ('domiciliation', ARRAY['acces-aux-droits-et-citoyennete--accompagnement-dans-les-demarches-administratives']),
-        ('emergency_accommodation', ARRAY['logement-hebergement--mal-loges-sans-logis']),
-        ('family_area', ARRAY['famille--soutien-a-la-parentalite']),
-        ('fountain', ARRAY['equipement-et-alimentation--alimentation']),
-        ('food_distribution', ARRAY['equipement-et-alimentation--alimentation']),
-        ('food_packages', ARRAY['equipement-et-alimentation--alimentation']),
-        ('food_voucher', ARRAY['equipement-et-alimentation--alimentation']),
-        ('food', ARRAY['equipement-et-alimentation--alimentation']),
-        ('french_course', ARRAY['apprendre-francais--suivre-formation']),
-        ('general_practitioner', ARRAY['sante--acces-aux-soins']),
-        ('health', ARRAY['sante']),
-        ('health_specialists', ARRAY['sante--acces-aux-soins']),
-        ('infirmary', ARRAY['sante--acces-aux-soins']),
-        ('integration_through_economic_activity', ARRAY['accompagnement-social-et-professionnel-personnalise--parcours-d-insertion-socioprofessionnel']),
-        ('job_coaching', ARRAY['trouver-un-emploi']),
-        ('legal_advice', ARRAY['acces-aux-droits-et-citoyennete--accompagnement-juridique']),
-        ('libraries', ARRAY['remobilisation--decouvrir-son-potentiel-via-le-sport-et-la-culture']),
-        ('long_term_accomodation', ARRAY['logement-hebergement--mal-loges-sans-logis']),
-        ('mobility', ARRAY['mobilite']),
-        ('mobility_assistance', ARRAY['mobilite--etre-accompagne-dans-son-parcours-mobilite']),
-        ('museums', ARRAY['remobilisation--decouvrir-son-potentiel-via-le-sport-et-la-culture']),
-        ('other_activities', ARRAY['remobilisation--decouvrir-son-potentiel-via-le-sport-et-la-culture']),
-        ('overnight_stop', ARRAY['logement-hebergement--mal-loges-sans-logis']),
-        ('parent_assistance', ARRAY['famille--information-et-accompagnement-des-parents', 'famille--soutien-a-la-parentalite', 'famille--soutien-aux-familles']),
-        ('pregnancy_care', ARRAY['sante--accompagnement-de-la-femme-enceinte-du-bebe-et-du-jeune-enfant']),
-        ('provision_of_vehicles', ARRAY['mobilite--louer-un-vehicule']),
-        ('psychological_support', ARRAY['sante--bien-etre-psychologique']),
-        ('public_writer', ARRAY['acces-aux-droits-et-citoyennete--accompagnement-dans-les-demarches-administratives', 'numerique--realiser-des-demarches-administratives-avec-un-accompagnement']),
-        ('seated_catering', ARRAY['equipement-et-alimentation--alimentation']),
-        ('shared_kitchen', ARRAY['equipement-et-alimentation--alimentation']),
-        ('social_accompaniment', ARRAY['accompagnement-social-et-professionnel-personnalise']),
-        ('social_grocery_stores', ARRAY['equipement-et-alimentation--alimentation']),
-        ('solidarity_fridge', ARRAY['equipement-et-alimentation--alimentation']),
-        ('solidarity_store', ARRAY['equipement-et-alimentation--habillement']),
-        ('sport_activities', ARRAY['remobilisation--decouvrir-son-potentiel-via-le-sport-et-la-culture']),
-        ('std_testing', ARRAY['sante--prevention-et-acces-aux-soins']),
-        ('telephone_at_your_disposal', ARRAY['equipement-et-alimentation--acces-a-un-telephone-et-un-abonnement']),
-        ('tutoring', ARRAY['trouver-un-emploi']),
-        ('vaccination', ARRAY['sante--prevention-et-acces-aux-soins']),
-        ('wellness', ARRAY['remobilisation--bien-etre']),
-        ('wifi', ARRAY['numerique--acceder-a-une-connexion-internet'])
-    ) AS x (category, thematique)
+mapping_thematiques AS (
+    SELECT * FROM {{ ref('_map_soliguide__thematiques') }}
 ),
 
 profils AS (
@@ -144,20 +71,11 @@ filtered_phones AS (
     FROM phones
 ),
 
--- remove services without mapped thematiques which are assumed irrelevant
-relevant_services AS (
-    SELECT *
-    FROM services
-    WHERE category IN (
-        SELECT c.category FROM di_thematique_by_soliguide_category_code AS c
-    )
-),
-
 -- remove temporarily suspended services from downstream data
 -- FIXME: these services should ideally be in the downstream but flagged as unavailable in some way
 open_services AS (
     SELECT *
-    FROM relevant_services
+    FROM services
     WHERE
         NOT close__actif
         OR
@@ -210,11 +128,7 @@ final AS (
         NULL                                                          AS "zone_diffusion_nom",  -- will be overridden after geocoding
         NULL                                                          AS "formulaire_en_ligne",
         open_services.lieu_id                                         AS "structure_id",
-        CAST((
-            SELECT di_thematique_by_soliguide_category_code.thematique
-            FROM di_thematique_by_soliguide_category_code
-            WHERE open_services.category = di_thematique_by_soliguide_category_code.category
-        ) AS TEXT [])                                                 AS "thematiques",
+        ARRAY[mapping_thematiques.thematique]                         AS "thematiques",
         ARRAY['en-presentiel']                                        AS "modes_accueil",
         categories.label || COALESCE(' : ' || open_services.name, '') AS "nom",
         'https://soliguide.fr/fr/fiche/' || lieux.seo_url             AS "lien_source",
@@ -286,7 +200,10 @@ final AS (
     LEFT JOIN lieux ON open_services.lieu_id = lieux.id
     LEFT JOIN categories ON open_services.category = categories.code
     LEFT JOIN filtered_phones ON open_services.lieu_id = filtered_phones.lieu_id
-    LEFT JOIN profils AS profils ON lieux.id = profils.lieu_id
+    LEFT JOIN profils ON lieux.id = profils.lieu_id
+    LEFT JOIN mapping_thematiques ON open_services.category = mapping_thematiques.category
+    -- remove services without mapped thematiques which are assumed irrelevant
+    WHERE mapping_thematiques.thematique IS NOT NULL
     ORDER BY open_services.id
 )
 
