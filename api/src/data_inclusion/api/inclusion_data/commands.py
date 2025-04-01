ea0cc9f0386abebbf4eeb74e2698d61fce4353cd
Hugo Lecuyer <hugo.lecuyer1@gmail.com>
2025-04-01 16:45:48 +0200

    feat(api): add new env sandbox

--- api/src/data_inclusion/api/inclusion_data/commands.py
+++ api/src/data_inclusion/api/inclusion_data/commands.py
@@ -125,6 +125,10 @@ def load_inclusion_data():
 
     structure_errors_df = validate_df(structures_df, model_schema=schema.Structure)
     service_errors_df = validate_df(services_df, model_schema=schema.Service)
+    if settings.ENV == "sandbox":
+        services_df = services_df["telephone"].apply(lambda x: "0000000000")
+        services_df = services_df["courriel"].apply(lambda x: "john.doe@exemple.com")
+        services_df = services_df["contact_nom_prenom"].apply(lambda x: "John Doe")
 
     logger.info("Structure validation errors:")
     log_errors(structure_errors_df)
