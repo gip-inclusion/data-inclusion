f1c9ec074c565ac1d32b5a45e538c017c123d85e
Valentin Matton <taq@protonmail.ch>
2025-04-15 16:43:44 +0200

    chore(pipeline): remove pipx venv

--- pipeline/dags/dag_utils/virtualenvs.py
+++ pipeline/dags/dag_utils/virtualenvs.py
@@ -5,5 +5,4 @@ from airflow import configuration
 VIRTUALENVS_DIR_PATH = Path(configuration.get_airflow_home()) / "venvs"
 
 PYTHON_BIN_PATH = VIRTUALENVS_DIR_PATH / "python" / "venv/bin/python"
-PIPX_PYTHON_BIN_PATH = VIRTUALENVS_DIR_PATH / "pipx" / "venv/bin/python"
 DBT_PYTHON_BIN_PATH = VIRTUALENVS_DIR_PATH / "dbt" / "venv/bin/python"
