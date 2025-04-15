46d281bfbd954e59a09dc4b317bdbb4d1e5a584f
Valentin Matton <taq@protonmail.ch>
2025-04-15 19:14:47 +0200

    chore(soliguide): clean up phone numbers

--- pipeline/dbt/models/staging/sources/soliguide/stg_soliguide__phones.sql
+++ pipeline/dbt/models/staging/sources/soliguide/stg_soliguide__phones.sql
@@ -7,11 +7,15 @@ phones AS (
         source._di_source_id                      AS "_di_source_id",
         source.data ->> 'lieu_id'                 AS "lieu_id",
         NULLIF(inline_phone.data ->> 'label', '') AS "label",
-        inline_phone.data ->> 'phoneNumber'       AS "phone_number",
+        CASE
+            WHEN inline_phone.data ->> 'phoneNumber' ~ '^[1-9]\d{8}$'
+                THEN '0' || inline_phone.data ->> 'phoneNumber'
+            ELSE inline_phone.data ->> 'phoneNumber'
+        END                                       AS "phone_number",
         source.data -> 'sources'                  AS "sources"
     FROM
         source,
-        LATERAL (SELECT * FROM JSONB_PATH_QUERY(source.data, '$.entity.phones[*]')) AS inline_phone (data)
+        LATERAL JSONB_PATH_QUERY(source.data, '$.entity.phones[*]') AS inline_phone (data)
 ),
 
 final AS (
