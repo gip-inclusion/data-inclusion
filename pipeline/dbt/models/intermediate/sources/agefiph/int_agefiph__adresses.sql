19d4b4b005fa90c55176527142b1dc37653eb643
Hugo Lecuyer <hugo.lecuyer1@gmail.com>
2025-04-24 15:37:30 +0200

    feat(pipeline): add agefiph structures

--- pipeline/dbt/models/intermediate/sources/agefiph/int_agefiph__adresses.sql
+++ pipeline/dbt/models/intermediate/sources/agefiph/int_agefiph__adresses.sql
@@ -1,15 +1,15 @@
 WITH structures AS (
-    SELECT * FROM {{ ref('int_agefiph__structures') }}
+    SELECT * FROM {{ ref('stg_agefiph__structures') }}
 )
 
 SELECT
-    'agefiph'                    AS "source",
-    structures.id                AS "id",
-    'Bagneux'                    AS "commune",
-    '92220'                      AS "code_postal",
-    '92007'                      AS "code_insee",
-    '192 avenue Aristide Briand' AS "adresse",
-    NULL                         AS "complement_adresse",
-    2.32097                      AS "longitude",
-    48.794432                    AS "latitude"
+    'agefiph'                                           AS "source",
+    structures.id                                       AS "id",
+    structures.attributes__field_adresse__locality      AS "commune",
+    structures.attributes__field_adresse__postal_code   AS "code_postal",
+    NULL                                                AS "code_insee",
+    structures.attributes__field_adresse__address_line1 AS "adresse",
+    structures.attributes__field_adresse__address_line2 AS "complement_adresse",
+    structures.attributes__field_geolocalisation__lng   AS "longitude",
+    structures.attributes__field_geolocalisation__lat   AS "latitude"
 FROM structures
