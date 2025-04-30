bd73085f70dbb079038f137dd635e93b4051bcef
Victor Perron <victor@iso3103.net>
2025-04-30 18:04:02 +0200

    chore(pipeline) : Improve sentry issue description & separation

--- pipeline/dags/dag_utils/sentry.py
+++ pipeline/dags/dag_utils/sentry.py
@@ -3,6 +3,28 @@ import os
 from sentry_sdk import configure_scope
 
 
+def before_send(event, hint):
+    if (
+        "exception" in event
+        and "values" in event["exception"]
+        and event["exception"]["values"]
+    ):
+        exception_str = (
+            # Airflow wraps any exception under `AirflowException`, we want to get the
+            # original one. Thankfully the exception string is formatted as such:
+            # 'value': 'Process [...]\nname "SomeException" is not defined'
+            # We want to keep the last line of the exception string (the original exc
+            # object is not directly accessible unfortunately)
+            event["exception"]["values"][0].get("value", "exception").split("\n")[-1]
+        )
+        dag = event["tags"].get("dag_id")
+        task = event["tags"].get("task_id")
+        if dag and task:
+            exception_str = f"{dag=} {task=} : {exception_str}"
+        event["exception"]["values"][0]["type"] = exception_str
+    return event
+
+
 def fill_sentry_scope(context):
     github_base_url = "https://github.com/gip-inclusion/data-inclusion"
 
@@ -13,6 +35,9 @@ def fill_sentry_scope(context):
         commit_sha = os.getenv("AIRFLOW__SENTRY__RELEASE", None)
         scope.set_tag("dag_id", dag_id)
         scope.set_tag("task_id", task_id)
+        # Fine-tune the fingerprint to force de-grouping issues if not from
+        # the same DAG
+        scope.fingerprint = ["{{ default }}", dag_id]
         scope.set_context(
             "custom",
             {
