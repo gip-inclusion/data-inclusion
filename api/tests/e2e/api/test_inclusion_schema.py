0ebbdf0362d0805549e2d1aa154a4e75f7f22af4
Valentin Matton <taq@protonmail.ch>
2025-06-04 18:34:37 +0200

    fix(api): set proper model for each doc endpoint

--- api/tests/e2e/api/test_inclusion_schema.py
+++ api/tests/e2e/api/test_inclusion_schema.py
@@ -2,6 +2,27 @@ from unittest.mock import ANY
 
 import pytest
 
+from data_inclusion.schema import v0, v1
+
+
+@pytest.fixture
+def model(schema_version, framework):
+    schema = v1 if schema_version == "v1" else v0
+    return getattr(
+        schema,
+        {
+            "labels-nationaux": "LabelNational",
+            "thematiques": "Thematique",
+            "typologies-services": "TypologieService",
+            "frais": "Frais",
+            "profils": "Profil",
+            "typologies-structures": "TypologieStructure",
+            "modes-accueil": "ModeAccueil",
+            "modes-orientation-accompagnateur": "ModeOrientationAccompagnateur",
+            "modes-orientation-beneficiaire": "ModeOrientationBeneficiaire",
+        }[framework],
+    )
+
 
 @pytest.mark.parametrize("schema_version", ["v0", "v1"])
 @pytest.mark.parametrize(
@@ -19,7 +40,7 @@ import pytest
     ],
 )
 @pytest.mark.with_token
-def test_list_framework(api_client, schema_version, framework):
+def test_list_framework(api_client, schema_version, framework, model):
     url = f"/api/{schema_version}/doc/{framework}/"
 
     response = api_client.get(url)
@@ -27,3 +48,4 @@ def test_list_framework(api_client, schema_version, framework):
     assert response.status_code == 200
     resp_data = response.json()
     assert resp_data[0] == {"value": ANY, "label": ANY, "description": ANY}
+    assert set(v.value for v in list(model)) == set(d["value"] for d in resp_data)
