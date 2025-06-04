397055d699c5bd0e9456de80250e95ff769e0871
Valentin Matton <taq@protonmail.ch>
2025-06-04 12:41:40 +0200

    chore(pipeline): clean up main dag
    - merge dag_utils.marts module
    - use task flow api

--- pipeline/dags/main.py
+++ pipeline/dags/main.py
@@ -1,24 +1,62 @@
 import pendulum
 
-import airflow
+from airflow.decorators import dag, task
 from airflow.operators import empty
 
-from dag_utils import date, marts
+from dag_utils import date
 from dag_utils.dbt import (
     dbt_operator_factory,
     get_intermediate_tasks,
     get_staging_tasks,
 )
 from dag_utils.sentry import notify_failure_args
+from dag_utils.virtualenvs import PYTHON_BIN_PATH
 
-with airflow.DAG(
-    dag_id="main",
+
+@task.external_python(
+    python=str(PYTHON_BIN_PATH),
+)
+def export_di_dataset_to_s3(
+    logical_date,
+    run_id,
+):
+    import logging
+
+    from airflow.providers.amazon.aws.hooks import s3
+    from airflow.providers.postgres.hooks import postgres
+
+    from dag_utils import date
+    from dag_utils.sources.utils import log_df_info
+
+    logger = logging.getLogger(__name__)
+
+    pg_hook = postgres.PostgresHook(postgres_conn_id="pg")
+    s3_hook = s3.S3Hook(aws_conn_id="s3")
+
+    prefix = f"data/marts/{date.local_date_str(logical_date)}/{run_id}/"
+
+    for ressource in ["structures", "services"]:
+        key = f"{prefix}{ressource}.parquet"
+        query = f"SELECT * FROM public_marts.marts_inclusion__{ressource}"
+
+        logger.info("downloading data from query='%s'", query)
+        df = pg_hook.get_pandas_df(sql=query)
+
+        log_df_info(df, logger=logger)
+        bytes_data = df.to_parquet(compression="gzip")
+
+        logger.info("uploading data to bucket='%s'", key)
+        s3_hook.load_bytes(bytes_data, key=key, replace=True)
+
+
+@dag(
     start_date=pendulum.datetime(2022, 1, 1, tz=date.TIME_ZONE),
     default_args=notify_failure_args(),
     schedule="@hourly",
     catchup=False,
     concurrency=4,
-) as dag:
+)
+def main():
     start = empty.EmptyOperator(task_id="start")
     end = empty.EmptyOperator(task_id="end")
 
@@ -45,6 +83,9 @@ with airflow.DAG(
         >> get_staging_tasks()
         >> get_intermediate_tasks()
         >> snapshot_deduplicate_stats
-        >> marts.export_di_dataset_to_s3()
+        >> export_di_dataset_to_s3()
         >> end
     )
+
+
+main()
