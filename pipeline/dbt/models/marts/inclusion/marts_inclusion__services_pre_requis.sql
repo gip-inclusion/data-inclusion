b94a8cbd33e5c2e25152c2f8bf0e8a311f07dcf3
Valentin Matton <taq@protonmail.ch>
2025-06-04 12:33:00 +0200

    chore(pipeline): cleanup unnest macro

--- pipeline/dbt/models/marts/inclusion/marts_inclusion__services_pre_requis.sql
+++ pipeline/dbt/models/marts/inclusion/marts_inclusion__services_pre_requis.sql
@@ -1,1 +1,6 @@
-{{ unnest_jsonb_list_to_table('service', 'pre_requis') }}
+{{ unnest(
+    from=ref('marts_inclusion__services_v0'),
+    column='pre_requis',
+    foreign_key='_di_surrogate_id',
+    fk_alias='service_surrogate_id'
+) }}
--- pipeline/dbt/models/marts/inclusion/marts_inclusion__services_profils.sql
+++ pipeline/dbt/models/marts/inclusion/marts_inclusion__services_profils.sql
@@ -1,1 +1,6 @@
-{{ unnest_jsonb_list_to_table('service', 'profils') }}
+{{ unnest(
+    from=ref('marts_inclusion__services_v0'),
+    column='profils',
+    foreign_key='_di_surrogate_id',
+    fk_alias='service_surrogate_id'
+) }}
