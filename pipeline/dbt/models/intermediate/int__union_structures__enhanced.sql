28b1d381d47d4a4fa1a61961b34a09f0b6287e4c
Valentin Matton <taq@protonmail.ch>
2025-06-06 10:23:11 +0200

    chore(pipeline): validation with dedicated model and macros
    - macros are less error-prone since they do not require to pass
    arguments to a pl/pgsql function...
    - move invalid resource filtering in marts models

--- pipeline/dbt/models/intermediate/int__union_structures__enhanced.sql
+++ pipeline/dbt/models/intermediate/int__union_structures__enhanced.sql
@@ -12,72 +12,39 @@ valid_site_web AS (
         "url"
     FROM {{ ref('int__union_urls__enhanced') }}
     WHERE status_code > 0
-),
-
-structures_to_validate AS (
-    SELECT
-        {{
-            dbt_utils.star(
-                from=ref('int__union_structures'),
-                relation_alias='structures',
-                except=[
-                    "nom",
-                    "telephone",
-                    "site_web",
-                ]
-            )
-        }},
-        processings.format_phone_number(structures.telephone) AS "telephone",
-        valid_site_web.url                                    AS "site_web",
-        CASE
-            WHEN LENGTH(structures.nom) <= 150 THEN structures.nom
-            ELSE LEFT(structures.nom, 149) || '…'
-        END                                                   AS "nom"
-    FROM structures
-    LEFT JOIN valid_site_web ON structures.site_web = valid_site_web.input_url
-),
-
-valid_structures AS (
-    SELECT structures.*
-    FROM structures_to_validate AS structures
-    LEFT JOIN
-        LATERAL
-        LIST_STRUCTURE_ERRORS(
-            structures.accessibilite,
-            structures.antenne,
-            structures.courriel,
-            structures.date_maj,
-            structures.horaires_ouverture,
-            structures.id,
-            structures.labels_autres,
-            structures.labels_nationaux,
-            structures.lien_source,
-            structures.nom,
-            structures.presentation_detail,
-            structures.presentation_resume,
-            structures.rna,
-            structures.siret,
-            structures.site_web,
-            structures.source,
-            structures.telephone,
-            structures.thematiques,
-            structures.typologie
-        ) AS errors ON TRUE
-    WHERE errors.field IS NULL
-),
-
-final AS (
-    SELECT
-        structures.*,
-        adresses.longitude          AS "longitude",
-        adresses.latitude           AS "latitude",
-        adresses.complement_adresse AS "complement_adresse",
-        adresses.commune            AS "commune",
-        adresses.adresse            AS "adresse",
-        adresses.code_postal        AS "code_postal",
-        adresses.code_insee         AS "code_insee"
-    FROM valid_structures AS structures
-    LEFT JOIN adresses ON structures._di_adresse_surrogate_id = adresses._di_surrogate_id
 )
 
-SELECT * FROM final
+SELECT
+    structures._di_surrogate_id                           AS "_di_surrogate_id",
+    structures.accessibilite                              AS "accessibilite",
+    structures.antenne                                    AS "antenne",
+    structures.courriel                                   AS "courriel",
+    structures.horaires_ouverture                         AS "horaires_ouverture",
+    structures.id                                         AS "id",
+    structures.labels_autres                              AS "labels_autres",
+    structures.labels_nationaux                           AS "labels_nationaux",
+    structures.lien_source                                AS "lien_source",
+    structures.presentation_detail                        AS "presentation_detail",
+    structures.presentation_resume                        AS "presentation_resume",
+    structures.rna                                        AS "rna",
+    structures.siret                                      AS "siret",
+    structures.source                                     AS "source",
+    structures.typologie                                  AS "typologie",
+    structures.date_maj                                   AS "date_maj",
+    structures.thematiques                                AS "thematiques",
+    processings.format_phone_number(structures.telephone) AS "telephone",
+    valid_site_web.url                                    AS "site_web",
+    CASE
+        WHEN LENGTH(structures.nom) <= 150 THEN structures.nom
+        ELSE LEFT(structures.nom, 149) || '…'
+    END                                                   AS "nom",
+    adresses.longitude                                    AS "longitude",
+    adresses.latitude                                     AS "latitude",
+    adresses.complement_adresse                           AS "complement_adresse",
+    adresses.commune                                      AS "commune",
+    adresses.adresse                                      AS "adresse",
+    adresses.code_postal                                  AS "code_postal",
+    adresses.code_insee                                   AS "code_insee"
+FROM structures
+LEFT JOIN valid_site_web ON structures.site_web = valid_site_web.input_url
+LEFT JOIN adresses ON structures._di_adresse_surrogate_id = adresses._di_surrogate_id
