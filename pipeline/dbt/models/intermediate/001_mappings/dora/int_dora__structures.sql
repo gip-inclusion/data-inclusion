b851d759f79319e92f5d64e3d733f85bb76b4f07
Valentin Matton <taq@protonmail.ch>
2025-06-14 16:54:52 +0200

    chore(dora): remove unkwown labels nationaux

--- pipeline/dbt/models/intermediate/001_mappings/dora/int_dora__structures.sql
+++ pipeline/dbt/models/intermediate/001_mappings/dora/int_dora__structures.sql
@@ -2,29 +2,41 @@ WITH structures AS (
     SELECT * FROM {{ ref('stg_dora__structures') }}
 ),
 
+labels_nationaux AS (
+    SELECT
+        structures.id                     AS structure_id,
+        ARRAY_AGG(labels_nationaux.value) AS labels_nationaux
+    FROM structures,
+        UNNEST(structures.labels_nationaux) AS label_dora
+    LEFT JOIN {{ ref('labels_nationaux') }} AS labels_nationaux ON label_dora = labels_nationaux.value
+    WHERE labels_nationaux.value IS NOT NULL
+    GROUP BY 1
+),
+
 final AS (
     SELECT
-        accessibilite       AS "accessibilite",
-        id                  AS "adresse_id",
-        antenne             AS "antenne",
-        courriel            AS "courriel",
-        date_maj::DATE      AS "date_maj",
-        horaires_ouverture  AS "horaires_ouverture",
-        id                  AS "id",
-        labels_autres       AS "labels_autres",
-        labels_nationaux    AS "labels_nationaux",
-        lien_source         AS "lien_source",
-        nom                 AS "nom",
-        presentation_detail AS "presentation_detail",
-        presentation_resume AS "presentation_resume",
-        rna                 AS "rna",
-        siret               AS "siret",
-        site_web            AS "site_web",
-        _di_source_id       AS "source",
-        telephone           AS "telephone",
-        NULL::TEXT []       AS "thematiques",
-        typologie           AS "typologie"
+        structures.accessibilite          AS "accessibilite",
+        structures.id                     AS "adresse_id",
+        structures.antenne                AS "antenne",
+        structures.courriel               AS "courriel",
+        CAST(structures.date_maj AS DATE) AS "date_maj",
+        structures.horaires_ouverture     AS "horaires_ouverture",
+        structures.id                     AS "id",
+        structures.labels_autres          AS "labels_autres",
+        labels_nationaux.labels_nationaux AS "labels_nationaux",
+        structures.lien_source            AS "lien_source",
+        structures.nom                    AS "nom",
+        structures.presentation_detail    AS "presentation_detail",
+        structures.presentation_resume    AS "presentation_resume",
+        structures.rna                    AS "rna",
+        structures.siret                  AS "siret",
+        structures.site_web               AS "site_web",
+        structures._di_source_id          AS "source",
+        structures.telephone              AS "telephone",
+        CAST(NULL AS TEXT [])             AS "thematiques",
+        structures.typologie              AS "typologie"
     FROM structures
+    LEFT JOIN labels_nationaux ON structures.id = labels_nationaux.structure_id
 )
 
 SELECT * FROM final
