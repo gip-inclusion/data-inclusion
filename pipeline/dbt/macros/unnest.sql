98a7309574a99dc7bad261ed02558252f9b89338
Hugo Lecuyer <hugo.lecuyer1@gmail.com>
2025-06-16 15:37:50 +0200

    fix(pipeline): remove null value from marts unnested values

--- pipeline/dbt/macros/unnest.sql
+++ pipeline/dbt/macros/unnest.sql
@@ -1,8 +1,13 @@
 {% macro unnest(from, column, foreign_key, fk_alias) %}
-
+WITH source AS (
+    SELECT
+        {{ foreign_key }}    AS {{ fk_alias }},
+        UNNEST({{ column }}) AS "value"
+    FROM {{ from }}
+)
 SELECT
-    {{ foreign_key }}    AS {{ fk_alias }},
-    UNNEST({{ column }}) AS "value"
-FROM {{ from }}
-
+    {{ fk_alias }},
+    value
+FROM source
+WHERE value IS NOT NULL
 {% endmacro %}
