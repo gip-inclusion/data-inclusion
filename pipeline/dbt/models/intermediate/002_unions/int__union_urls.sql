8b0cb55393d69b44a83639f5c64d584c41172c84
Victor Perron <victor@iso3103.net>
2025-06-17 12:14:51 +0200

    feat(pipeline): Validate URLs from structures.accessibilite

--- pipeline/dbt/models/intermediate/002_unions/int__union_urls.sql
+++ pipeline/dbt/models/intermediate/002_unions/int__union_urls.sql
@@ -1,8 +1,20 @@
-WITH final AS (
+WITH site_web AS (
     SELECT DISTINCT site_web AS "url"
     FROM {{ ref('int__union_structures') }}
     WHERE site_web IS NOT NULL
-    ORDER BY site_web
+),
+
+accessibilite AS (
+    SELECT DISTINCT accessibilite AS "url"
+    FROM {{ ref('int__union_structures') }}
+    WHERE accessibilite IS NOT NULL
+),
+
+final AS (
+    SELECT * FROM site_web
+    UNION
+    SELECT * FROM accessibilite
+    ORDER BY url
 )
 
 SELECT * FROM final
