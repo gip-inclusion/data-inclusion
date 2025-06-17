43fb6658af33c303d883d27c83ea482ea3bcf3b5
Victor Perron <victor@iso3103.net>
2025-06-17 12:14:51 +0200

    feat(pipeline): Validate URLs from structures.accessibilite

--- pipeline/dbt/models/intermediate/004_finals/int__structures.sql
+++ pipeline/dbt/models/intermediate/004_finals/int__structures.sql
@@ -16,7 +16,6 @@ valid_site_web AS (
 
 SELECT
     structures._di_surrogate_id                           AS "_di_surrogate_id",
-    structures.accessibilite                              AS "accessibilite",
     structures.courriel                                   AS "courriel",
     structures.horaires_ouverture                         AS "horaires_ouverture",
     structures.id                                         AS "id",
@@ -37,7 +36,8 @@ SELECT
     structures.date_maj                                   AS "date_maj",
     structures.thematiques                                AS "thematiques",
     processings.format_phone_number(structures.telephone) AS "telephone",
-    valid_site_web.url                                    AS "site_web",
+    valid_site_web_1.url                                  AS "site_web",
+    valid_site_web_2.url                                  AS "accessibilite",
     CASE
         WHEN LENGTH(structures.nom) <= 150 THEN structures.nom
         ELSE LEFT(structures.nom, 149) || '…'
@@ -50,5 +50,6 @@ SELECT
     adresses.code_postal                                  AS "code_postal",
     adresses.code_insee                                   AS "code_insee"
 FROM structures
-LEFT JOIN valid_site_web ON structures.site_web = valid_site_web.input_url
+LEFT JOIN valid_site_web AS valid_site_web_1 ON structures.site_web = valid_site_web_1.input_url
+LEFT JOIN valid_site_web AS valid_site_web_2 ON structures.accessibilite = valid_site_web_2.input_url
 LEFT JOIN adresses ON structures._di_adresse_surrogate_id = adresses._di_surrogate_id
