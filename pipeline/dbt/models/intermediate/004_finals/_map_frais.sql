fc119ac5bca289d9fabcb12a30eb6dcf57b93729
Valentin Matton <taq@protonmail.ch>
2025-07-20 21:20:55 +0200

    feat(pipeline): add reseaux_porteurs

--- pipeline/dbt/models/intermediate/004_finals/_map_frais.sql
+++ pipeline/dbt/models/intermediate/004_finals/_map_frais.sql
@@ -1,5 +1,4 @@
 {{ config(materialized='ephemeral') }}
--- Mapping https://www.notion.so/gip-inclusion/champs-li-s-service-frais-1fb5f321b604804fba89e86358258c78
 SELECT
     x.frais_v0,
     x.frais_v1
