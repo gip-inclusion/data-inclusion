332e8f86d6144850b06dd1256a0de9ed077b5bce
Valentin Matton <taq@protonmail.ch>
2025-08-12 16:18:26 +0200

    refactor(pipeline): materialize sirene models as tables
    This way downstream models are not explicitely dependent.
    And now the tables can be simply updated, without cascades.
    Another possible way would have been to load with a tmp table,
    truncate and copy.

---
+++ pipeline/dags/common/tasks.py
@@ -0,0 +1,22 @@
+from airflow.decorators import task
+from airflow.providers.postgres.hooks import postgres
+
+
+@task.python
+def create_schema(name: str):
+    """Create a schema in the `pg` database and grant public access.
+
+    Args:
+        name (str): The name of the schema to create. Should be a valid schema name (no
+            hyphens, etc.).
+    """
+    hook = postgres.PostgresHook(postgres_conn_id="pg")
+    engine = hook.get_sqlalchemy_engine()
+    with engine.begin() as conn:
+        conn.execute(
+            f"""\
+            CREATE SCHEMA IF NOT EXISTS {name};
+            GRANT USAGE ON SCHEMA {name} TO PUBLIC;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA {name}
+            GRANT SELECT ON TABLES TO PUBLIC;"""
+        )
