860fe9778e2e2b731249bbb90d47a72633a3f35f
Valentin Matton <taq@protonmail.ch>
2025-08-19 20:01:56 +0200

    chore(pipeline): revert auto grant due to concurrency issue

--- pipeline/dbt/macros/grant_select_on_schemas.sql
+++
@@ -1,7 +0,0 @@
-{% macro grant_select_on_schemas_to_public(schemas) %}
-{% for schema in schemas %}
-GRANT USAGE ON SCHEMA {{ schema }} TO PUBLIC;
-GRANT SELECT ON ALL TABLES IN SCHEMA {{ schema }} TO PUBLIC;
-ALTER DEFAULT PRIVILEGES IN SCHEMA {{ schema }} GRANT SELECT ON TABLES TO PUBLIC;
-{% endfor %}
-{% endmacro %}
