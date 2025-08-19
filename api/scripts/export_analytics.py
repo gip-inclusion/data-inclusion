bb060d0cd6e4dd8563ef2e8393cda5e8c7af8b87
Valentin Matton <taq@protonmail.ch>
2025-08-19 16:43:47 +0200

    refactor: load analytics without ssh tunneling

---
+++ api/scripts/export_analytics.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+import tempfile
+import textwrap
+from pathlib import Path
+
+import dotenv
+import pendulum
+import s3fs
+
+OUTPUT_FILE = "analytics.dump"
+BASE_KEY = Path(os.environ["DATALAKE_BUCKET_NAME"]) / "data" / "api"
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+
+def export():
+    s3fs_client = s3fs.S3FileSystem()
+    key = str(
+        BASE_KEY
+        / pendulum.now().date().isoformat()
+        / pendulum.now().isoformat()
+        / OUTPUT_FILE
+    )
+
+    tmpdir = tempfile.TemporaryDirectory()
+    file = Path(tmpdir.name) / OUTPUT_FILE
+
+    command = textwrap.dedent(f"""\
+        pg_dump $DATABASE_URL
+        --format=custom
+        --clean
+        --if-exists
+        --no-owner
+        --no-privileges
+        --section=pre-data
+        --section=data
+        --table api__*_events
+        --file {file}
+        """).replace("\n", " ")
+
+    print(command)
+    subprocess.run(command, shell=True, check=True)
+
+    print(f"Storing to {key}")
+    s3fs_client.put_file(file, key)
+
+    tmpdir.cleanup()
+
+
+if __name__ == "__main__":
+    export()
