8606927fe6195c947c2e5490d6e00e41b9dce561
Valentin Matton <taq@protonmail.ch>
2025-08-19 12:02:37 +0200

    chore(pipeline): update mes aides urls

--- pipeline/dags/dag_utils/sources/mes_aides.py
+++ pipeline/dags/dag_utils/sources/mes_aides.py
@@ -2,12 +2,15 @@ import json
 
 
 def extract(url: str, token: str, **kwargs) -> bytes:
+    """Extract from airtable table.
+
+    Args:
+        url (str): URL with format https://airtable.com/app***/tbl***
+    """
     from pyairtable import api as pyairtable
 
-    # extract airtable's base_id and table_name from url
-    # cf https://support.airtable.com/docs/understanding-airtable-ids
-    base_id = url.split("/")[-3]
-    table_name = url.split("/")[-2]
+    base_id = url.split("/")[-2]
+    table_name = url.split("/")[-1]
 
     api = pyairtable.Api(api_key=token)
     table = api.table(base_id=base_id, table_name=table_name)
