834872128fcd0fb14ef874ad4e1eb1a295ab1de8
Victor Perron <victor@iso3103.net>
2025-08-21 19:09:41 +0200

    fix(deployment): Separate application env vars and deployment vars
    This also means that we can't use terraform logic as easily as before to
    generate postgres URLs or S3 urls derived from the created bucket.
    
    Thus I chose to slightly simplify the bucket name to not need a suffix
    anymore. Why was it necessary in the first place ?
    
    We can now simply define a secret in the env file format containing all
    the application secrets at once.

--- deployment/variables.tf
+++ deployment/variables.tf
@@ -1,19 +1,7 @@
-variable "airflow__core__fernet_key" {
-  description = "Secret key to save connection passwords in the db"
-  type        = string
-  sensitive   = true
-}
-
 variable "airflow_access_key" {
   description = "Scaleway access key for Airflow"
   type        = string
   sensitive   = false
-}
-
-variable "airflow_admin_password" {
-  description = "Password for airflow admin panel"
-  type        = string
-  sensitive   = true
 }
 
 variable "airflow_application_id" {
@@ -22,66 +10,10 @@ variable "airflow_application_id" {
   sensitive   = false
 }
 
-variable "airflow_secret_key" {
-  description = "Scaleway secret key for Airflow"
-  type        = string
-  sensitive   = true
-}
-
 variable "api_scw_application_id" {
   description = "Used to manage access rights on the datalake"
   type        = string
   sensitive   = false
-}
-
-variable "carif_oref_url" {
-  description = "Used in extraction tasks orchestrated by airflow"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "datagouv_api_key" {
-  description = "Used in extraction tasks orchestrated by airflow"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "brevo_api_key" {
-  description = "Used in notifications for RGPD users"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "data_inclusion_api_probe_token" {
-  description = "JWT token used to be authenticated on data inclusion api for probing purposes"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "datawarehouse_di_database" {
-  description = "Identifier for the data inclusion database"
-  type        = string
-}
-
-variable "datawarehouse_di_password" {
-  description = "Password for the main user of the postgres datawarehouse"
-  type        = string
-  sensitive   = true
-}
-
-variable "datawarehouse_di_username" {
-  description = "Identifier for the main user of the postgres datawarehouse"
-  type        = string
-}
-
-variable "dns_subdomain" {
-  description = "DNS subdomain where the public hostnames will be created within dns_zone (optional)"
-  type        = string
-  default     = ""
 }
 
 variable "dns_zone" {
@@ -89,68 +21,15 @@ variable "dns_zone" {
   type        = string
 }
 
-variable "dora_api_token" {
-  description = "Used in extraction tasks orchestrated by airflow"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "dora_preprod_api_token" {
-  description = "Used in extraction tasks orchestrated by airflow"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "dora_api_url" {
-  description = "Used in extraction tasks orchestrated by airflow"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "ft_api_token" {
-  description = "Used in extraction tasks orchestrated by airflow"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "emplois_api_token" {
-  description = "Used in extraction tasks orchestrated by airflow"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
 variable "environment" {
   description = "Identifier of the target environment"
   type        = string
 }
 
-variable "mes_aides_airtable_key" {
-  description = "Used in extraction tasks orchestrated by airflow"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "scaleway_access_key" {
-  description = "Scaleway access key (https://console.scaleway.com/iam/api-keys)"
-  type        = string
-  sensitive   = true
-}
 
 variable "scaleway_application_id" {
   description = "ID of the application owning the api keys"
   type        = string
-}
-
-variable "scaleway_secret_key" {
-  description = "Scaleway secret key (https://console.scaleway.com/iam/api-keys)"
-  type        = string
-  sensitive   = true
 }
 
 variable "scaleway_project_id" {
@@ -174,13 +53,6 @@ variable "app_secrets_secret_key" {
   sensitive   = true
 }
 
-variable "soliguide_api_token" {
-  description = "Used in extraction tasks orchestrated by airflow"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
 variable "ssh_private_key" {
   description = "The associated public key will be deployed to the instance"
   type        = string
@@ -190,45 +62,4 @@ variable "ssh_private_key" {
 variable "stack_version" {
   description = "Version (e.g. sha or semver) of the stack services to deploy"
   type        = string
-}
-
-variable "airflow__sentry__sentry_dsn" {
-  description = "Sentry DSN for airflow monitoring"
-  type        = string
-  default     = ""
-}
-
-variable "airflow_conn_pg_api" {
-  description = "Postgres URI similar to the api scalingo app SCALINGO_POSTGRESQL_URL, but with a dedicated read-only credentials"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "twocaptcha_api_key" {
-  description = "Used in extraction tasks orchestrated by airflow"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "fredo_api_token" {
-  description = "Used in extraction tasks orchestrated by airflow"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "mission_locale_api_secret" {
-  description = "Used in extraction tasks orchestrated by airflow"
-  type        = string
-  sensitive   = true
-  default     = ""
-}
-
-variable "ma_boussole_aidants_api_key" {
-  description = "Used in extraction tasks orchestrated by airflow"
-  type        = string
-  sensitive   = true
-  default     = ""
 }
