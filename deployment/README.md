278070e5f0b376fd6a2e537a32d0c35f96044abc
Victor Perron <victor@iso3103.net>
2025-08-21 19:09:41 +0200

    fix(deployment): Separate application env vars and deployment vars
    This also means that we can't use terraform logic as easily as before to
    generate postgres URLs or S3 urls derived from the created bucket.
    
    Thus I chose to slightly simplify the bucket name to not need a suffix
    anymore. Why was it necessary in the first place ?
    
    We can now simply define a secret in the env file format containing all
    the application secrets at once.

--- deployment/README.md
+++ deployment/README.md
@@ -100,19 +100,7 @@ terraform apply
 
 ### prod and staging on github
 
-Each environment has a dedicated github environment. In each of these gh environment, a `TF_VARS_BASE64` secret contains a base64 encoded version of `terraform.tfvars.json`, with all the variables for a given environment.
-
-The files are stored on bitwarden as `<ENVIRONMENT>.terraform.tfvars.json`.
-
-Use the following gh cli command to update this file on github:
-
-```bash
-# using gh cli
-# replace <ENVIRONMENT>
-base64 -w0 <ENVIRONMENT>.terraform.tfvars.json | gh secret --repo gip-inclusion/data-inclusion --env <ENVIRONMENT> set TF_VARS_BASE64
-```
-
-Make sure to update the config on bitwarden.
+Each environment has a dedicated github environment containing a collection of deployment secrets.
 
 ### formatting `.tf` files
 
--- deployment/main.tf
+++ deployment/main.tf
@@ -55,11 +55,11 @@ resource "scaleway_instance_server" "main" {
   EOF
 }
 
+# TODO(vperron): some day, replace the bucket with a fixed name one
 resource "random_pet" "datalake_bucket_suffix" {}
 
 resource "scaleway_object_bucket" "main" {
   name = "data-inclusion-datalake-${var.environment}-${random_pet.datalake_bucket_suffix.id}"
-
   lifecycle_rule {
     id      = "archive-raw-data-after-30-days"
     prefix  = "data/raw"
@@ -234,39 +234,8 @@ resource "scaleway_object_bucket_policy" "main" {
 }
 
 locals {
-  airflow_conn_pg = format(
-    "postgresql://%s:%s@%s:%s/%s",
-    var.datawarehouse_di_username,
-    var.datawarehouse_di_password,
-    "datawarehouse",
-    "5432",
-    var.datawarehouse_di_database
-  )
-
-  s3_endpoint = replace(scaleway_object_bucket.main.endpoint, "${scaleway_object_bucket.main.name}.", "")
-
-  airflow_conn_s3 = jsonencode(
-    {
-      conn_type = "aws"
-      extra = {
-        "region_name" : "${scaleway_object_bucket.main.region}",
-        "endpoint_url" : "${local.s3_endpoint}",
-        "aws_access_key_id" : "${var.airflow_access_key}",
-        "aws_secret_access_key" : "${var.airflow_secret_key}",
-        "service_config" : {
-          "s3" : {
-            "bucket_name" : "${scaleway_object_bucket.main.name}",
-          }
-        }
-      }
-    }
-  )
-
-  base_hostname = "${var.dns_subdomain != "" ? "${var.dns_subdomain}." : ""}${var.dns_zone}"
-
-  airflow_hostname = "airflow.${local.base_hostname}"
-
-  work_dir = "/root/data-inclusion"
+  airflow_hostname = "airflow.${var.dns_zone}"
+  work_dir         = "/root/data-inclusion"
 }
 
 resource "scaleway_domain_record" "dns" {
@@ -304,41 +273,10 @@ resource "terraform_data" "up" {
   provisioner "file" {
     content = sensitive(<<-EOT
     STACK_VERSION='${var.stack_version}'
-    AIRFLOW_HOSTNAME='${local.airflow_hostname}'
-
-    # Datawarehouse
-    DATAWAREHOUSE_DI_DATABASE='${var.datawarehouse_di_database}'
-    DATAWAREHOUSE_DI_PASSWORD='${var.datawarehouse_di_password}'
-    DATAWAREHOUSE_DI_USERNAME='${var.datawarehouse_di_username}'
-
-    # Airflow settings
-    AIRFLOW_WWW_USER_PASSWORD='${var.airflow_admin_password}'
-    AIRFLOW__CORE__FERNET_KEY='${var.airflow__core__fernet_key}'
-    AIRFLOW__SENTRY__SENTRY_DSN='${var.airflow__sentry__sentry_dsn}'
     AIRFLOW__SENTRY__RELEASE='${var.stack_version}'
+    AIRFLOW_HOSTNAME='${local.airflow_hostname}'
     AIRFLOW__WEBSERVER__BASE_URL='https://${local.airflow_hostname}'
-
-    # Airflow connections
-    AIRFLOW_CONN_PG_API='${var.airflow_conn_pg_api}'
-    AIRFLOW_CONN_PG='${local.airflow_conn_pg}'
-    AIRFLOW_CONN_S3='${local.airflow_conn_s3}'
-
-    # Airflow variables
-    AIRFLOW_VAR_BREVO_API_KEY='${var.brevo_api_key}'
-    AIRFLOW_VAR_CARIF_OREF_URL='${var.carif_oref_url}'
-    AIRFLOW_VAR_DATA_INCLUSION_API_PROBE_TOKEN='${var.data_inclusion_api_probe_token}'
-    AIRFLOW_VAR_DATAGOUV_API_KEY='${var.datagouv_api_key}'
-    AIRFLOW_VAR_DORA_API_TOKEN='${var.dora_api_token}'
-    AIRFLOW_VAR_DORA_API_URL='${var.dora_api_url}'
-    AIRFLOW_VAR_EMPLOIS_API_TOKEN='${var.emplois_api_token}'
     AIRFLOW_VAR_ENVIRONMENT='${var.environment}'
-    AIRFLOW_VAR_FREDO_API_TOKEN='${var.fredo_api_token}'
-    AIRFLOW_VAR_MISSION_LOCALE_API_SECRET='${var.mission_locale_api_secret}'
-    AIRFLOW_VAR_FT_API_TOKEN='${var.ft_api_token}'
-    AIRFLOW_VAR_MES_AIDES_AIRTABLE_KEY='${var.mes_aides_airtable_key}'
-    AIRFLOW_VAR_SOLIGUIDE_API_TOKEN='${var.soliguide_api_token}'
-    AIRFLOW_VAR_TWOCAPTCHA_API_KEY='${var.twocaptcha_api_key}'
-    AIRFLOW_VAR_MA_BOUSSOLE_AIDANTS_API_KEY='${var.ma_boussole_aidants_api_key}'
     EOT
     )
     destination = "${local.work_dir}/.env"
@@ -347,11 +285,6 @@ resource "terraform_data" "up" {
   provisioner "file" {
     source      = "${path.root}/docker-compose.yml"
     destination = "${local.work_dir}/docker-compose.yml"
-  }
-
-  provisioner "file" {
-    source      = "${path.root}/../pipeline/defaults.env"
-    destination = "${local.work_dir}/defaults.env"
   }
 
   provisioner "file" {
@@ -378,10 +311,10 @@ resource "terraform_data" "up" {
       "systemctl enable cleanup.timer",
       "systemctl start cleanup.timer",
       "cd ${local.work_dir}",
+      "scw secret version access-by-path secret-name=pipeline secret-path=/ revision=latest -o template=\"{{ printf \\\"%s\\\" .Data }}\" >> .env",
       "docker compose --progress=plain up --pull=always --force-recreate --remove-orphans --wait --wait-timeout 1200 --quiet-pull --detach",
       # FIXME: ideally this file should be removed
       # "rm -f ${local.work_dir}/.env",
     ]
   }
-
 }
