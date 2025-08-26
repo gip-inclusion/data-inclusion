e1481d07a2907e5c139b12800ab971f651e51e01
Victor Perron <victor@iso3103.net>
2025-08-26 12:01:59 +0200

    feat(deployment): Use TF_VAR_xxx instead of base64 encoded ones
    Let's save the tf state file beforehand, better safe than sorry.
    
    With 'terraform plan' it has been seen that tf wants to destroy our old
    bucket.
    
    Thus, we HAVE to run:
    
        terraform state rm 'scaleway_object_bucket.old_bucket_name'
    
    ...before applying, to make tf forget about it. It will then create the
    new bucket and we'll have all the time int he world to rsync the data to
    it, if necessary.
    
    TODO in Github vars & secrets, for each environment:
    
    - create the new values (TF_VAR_xxx)
    - delete TF_VARS_BASE64
    
    The only secrets will automatically be masked since they have either
    SECRET or KEY in their name (verify it !!!)

--- deployment/outputs.tf
+++ deployment/outputs.tf
@@ -1,20 +1,3 @@
-output "airflow_conn_pg" {
-  description = "Connection string to the datawarehouse for airflow"
-  value       = local.airflow_conn_pg
-  sensitive   = true
-}
-
-output "airflow_conn_s3" {
-  description = "Connection string to the datalake for airflow"
-  value       = local.airflow_conn_s3
-  sensitive   = true
-}
-
-output "airflow_url" {
-  description = "Airflow public URL"
-  value       = "https://${local.airflow_hostname}"
-}
-
 output "public_ip" {
   description = "Publicly reachable IP (with `ssh root@<public_ip>`)"
   value       = scaleway_instance_server.main.public_ips[0].address
