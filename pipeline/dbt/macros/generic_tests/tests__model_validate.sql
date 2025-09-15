b52b2e456c0e518dbb75a2e1c616b5431fb8cc85
Victor Perron <victor@iso3103.net>
2025-09-15 21:51:12 +0200

    chore(pipeline): Fix model validation

--- pipeline/dbt/macros/generic_tests/tests__model_validate.sql
+++ pipeline/dbt/macros/generic_tests/tests__model_validate.sql
@@ -7,12 +7,20 @@
             model_class: data_inclusion.schema.v0.Structure
 #}
 
-{% test model_validate(model, model_class) %}
+{% test model_validate(model, model_class, where) %}
+    WITH filtered_model AS (
+        SELECT *
+        FROM {{ model }}
+        {% if where %}
+        WHERE {{ where }}
+        {% endif %}
+    )
+
     SELECT
         resources.id,
         resources.source,
         errors.*
     FROM
-        {{ model }} AS resources,
+        filtered_model AS resources,
         processings.model_validate('{{ model_class }}', TO_JSONB(resources)) AS errors
 {% endtest %}
