35894facae12b5519c8bb86df488e95b73b254eb
Victor Perron <victor@iso3103.net>
2025-10-15 15:03:22 +0200

    chore(pipeline): Complete URLs v1 various sources

--- pipeline/dbt/models/intermediate/v1/002_unions/int__union_urls_v1.sql
+++ pipeline/dbt/models/intermediate/v1/002_unions/int__union_urls_v1.sql
@@ -3,9 +3,21 @@ WITH final AS (
     FROM {{ ref('int__union_structures_v1') }}
     WHERE accessibilite_lieu IS NOT NULL
     UNION
+    SELECT DISTINCT lien_source AS "url"
+    FROM {{ ref('int__union_structures_v1') }}
+    WHERE lien_source IS NOT NULL
+    UNION
+    SELECT DISTINCT site_web AS "url"
+    FROM {{ ref('int__union_structures_v1') }}
+    WHERE site_web IS NOT NULL
+    UNION
     SELECT DISTINCT lien_mobilisation AS "url"
     FROM {{ ref('int__union_services_v1') }}
     WHERE lien_mobilisation IS NOT NULL
+    UNION
+    SELECT DISTINCT lien_source AS "url"
+    FROM {{ ref('int__union_services_v1') }}
+    WHERE lien_source IS NOT NULL
 )
 
 SELECT * FROM final
