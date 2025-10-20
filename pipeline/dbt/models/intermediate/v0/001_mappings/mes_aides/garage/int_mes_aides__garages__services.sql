3b400055d29042f8366e4d3876ed2fc29b5e9160
Valentin Matton <taq@protonmail.ch>
2025-10-20 15:40:17 +0200

    feat(pipeline): add mes aides garages mapping v1

--- pipeline/dbt/models/intermediate/v0/001_mappings/mes_aides/garage/int_mes_aides__garages__services.sql
+++ pipeline/dbt/models/intermediate/v0/001_mappings/mes_aides/garage/int_mes_aides__garages__services.sql
@@ -4,37 +4,37 @@ WITH garages AS (
     SELECT * FROM {{ ref('stg_mes_aides__garages') }}
 ),
 
-service_types_by_garage AS (
-    SELECT
-        garages.id,
-        service_type
-    FROM garages,
-        UNNEST(garages.services) AS service_type
+garages__services AS (
+    SELECT * FROM {{ ref('stg_mes_aides__garages__services') }}
+),
+
+garages__types_de_vehicule AS (
+    SELECT * FROM {{ ref('stg_mes_aides__garages__types_de_vehicule') }}
 ),
 
 final AS (
     SELECT
-        garages.id                                                                                  AS "adresse_id",
-        garages.modifie_le                                                                          AS "date_maj",
-        NULL                                                                                        AS "formulaire_en_ligne",
-        '{{ check_structure_str }}'                                    AS "frais_autres",
-        CAST((UUID(MD5(garages.id || COALESCE(service_types_by_garage.service_type, '')))) AS TEXT) AS "id",
-        CAST(NULL AS TEXT [])                                                                       AS "justificatifs",
-        'https://mes-aides.francetravail.fr/transport-et-mobilite/garages-solidaires'               AS "lien_source",
-        ARRAY['en-presentiel']                                                                      AS "modes_accueil",
+        garages.id                                                                    AS "adresse_id",
+        garages.modifie_le                                                            AS "date_maj",
+        NULL                                                                          AS "formulaire_en_ligne",
+        '{{ check_structure_str }}'                      AS "frais_autres",
+        CAST((UUID(MD5(garages.id || COALESCE(garages__services.item, '')))) AS TEXT) AS "id",
+        CAST(NULL AS TEXT [])                                                         AS "justificatifs",
+        'https://mes-aides.francetravail.fr/transport-et-mobilite/garages-solidaires' AS "lien_source",
+        ARRAY['en-presentiel']                                                        AS "modes_accueil",
         ARRAY_REMOVE(
             ARRAY[
                 CASE WHEN garages.telephone IS NOT NULL THEN 'telephoner' END,
                 CASE WHEN garages.email IS NOT NULL THEN 'envoyer-un-mail' END
             ],
             NULL
-        )                                                                                           AS "modes_orientation_accompagnateur",
+        )                                                                             AS "modes_orientation_accompagnateur",
         CASE
             WHEN
                 garages.telephone IS NULL
                 AND garages.email IS NULL
                 THEN '{{ check_structure_str }}'
-        END                                                                                         AS "modes_orientation_accompagnateur_autres",
+        END                                                                           AS "modes_orientation_accompagnateur_autres",
         ARRAY_REMOVE(
             ARRAY[
                 'se-presenter',
@@ -42,32 +42,32 @@ final AS (
                 CASE WHEN garages.email IS NOT NULL THEN 'envoyer-un-mail' END
             ],
             NULL
-        )                                                                                           AS "modes_orientation_beneficiaire",
-        NULL                                                                                        AS "modes_orientation_beneficiaire_autres",
-        CAST(NULL AS TEXT [])                                                                       AS "modes_mobilisation",
-        CAST(NULL AS TEXT [])                                                                       AS "mobilisable_par",
-        NULL                                                                                        AS "mobilisation_precisions",
+        )                                                                             AS "modes_orientation_beneficiaire",
+        NULL                                                                          AS "modes_orientation_beneficiaire_autres",
+        CAST(NULL AS TEXT [])                                                         AS "modes_mobilisation",
+        CAST(NULL AS TEXT [])                                                         AS "mobilisable_par",
+        NULL                                                                          AS "mobilisation_precisions",
         FORMAT(
             '%s de %s',
-            COALESCE(service_types_by_garage.service_type, 'Réparation, vente et location'),
-            LOWER(COALESCE(NULLIF(ARRAY_TO_STRING(garages.types_de_vehicule, ', '), ''), 'véhicule'))
-        )                                                                                           AS "nom",
+            COALESCE(garages__services.item, 'Réparation, vente et location'),
+            LOWER(COALESCE(NULLIF(ARRAY_TO_STRING(garages__types_de_vehicule.items, ', '), ''), 'véhicule'))
+        )                                                                             AS "nom",
         FORMAT(
             '%s de %s à tarif solidaire pour les personnes en difficulté, et selon leur situation',
-            COALESCE(service_types_by_garage.service_type, 'Réparation, vente et location'),
-            LOWER(COALESCE(NULLIF(ARRAY_TO_STRING(garages.types_de_vehicule, ', '), ''), 'véhicule'))
-        )                                                                                           AS "presentation_resume",
-        NULL                                                                                        AS "presentation_detail",
-        NULL                                                                                        AS "prise_rdv",
-        NULL                                                                                        AS "lien_mobilisation",
-        CAST(NULL AS TEXT [])                                                                       AS "profils",
-        LEFT(garages.criteres_eligibilite_raw, 500)                                                 AS "profils_precisions",
-        NULL                                                                                        AS "recurrence",
-        'mes-aides'                                                                                 AS "source",
-        garages.id                                                                                  AS "structure_id",
+            COALESCE(garages__services.item, 'Réparation, vente et location'),
+            LOWER(COALESCE(NULLIF(ARRAY_TO_STRING(garages__types_de_vehicule.items, ', '), ''), 'véhicule'))
+        )                                                                             AS "presentation_resume",
+        NULL                                                                          AS "presentation_detail",
+        NULL                                                                          AS "prise_rdv",
+        NULL                                                                          AS "lien_mobilisation",
+        CAST(NULL AS TEXT [])                                                         AS "profils",
+        LEFT(garages.criteres_eligibilite, 500)                                       AS "profils_precisions",
+        NULL                                                                          AS "recurrence",
+        'mes-aides'                                                                   AS "source",
+        garages.id                                                                    AS "structure_id",
         ARRAY_REMOVE(
             ARRAY[
-                CASE service_types_by_garage.service_type
+                CASE garages__services.item
                     WHEN 'Achat' THEN 'mobilite--acheter-un-vehicule-motorise'
                     WHEN 'Location' THEN 'mobilite--louer-un-vehicule'
                     WHEN 'Réparation' THEN 'mobilite--entretenir-reparer-son-vehicule'
@@ -75,30 +75,39 @@ final AS (
                 END,
                 CASE
                     WHEN
-                        service_types_by_garage.service_type = 'Achat'
-                        AND 'Vélo' = ANY(garages.types_de_vehicule)
+                        garages__services.item = 'Achat'
+                        AND 'Vélo' = ANY(garages__types_de_vehicule.items)
                         THEN 'mobilite--acheter-un-velo'
                 END
             ],
             NULL
-        )                                                                                           AS "thematiques",
-        ARRAY['aide-materielle', 'aide-financiere']                                                 AS "types",
+        )                                                                             AS "thematiques",
+        ARRAY['aide-materielle', 'aide-financiere']                                   AS "types",
         CASE LEFT(garages.code_insee, 2)
             WHEN '97' THEN LEFT(garages.code_insee, 3)
             ELSE LEFT(garages.code_insee, 2)
-        END                                                                                         AS "zone_diffusion_code",
-        NULL                                                                                        AS "zone_diffusion_nom",
-        'departement'                                                                               AS "zone_diffusion_type",
-        CAST(NULL AS FLOAT)                                                                         AS "volume_horaire_hebdomadaire",
-        CAST(NULL AS INT)                                                                           AS "nombre_semaines",
-        garages.criteres_eligibilite                                                                AS "pre_requis",
-        NULL                                                                                        AS "contact_nom_prenom",
-        garages.email                                                                               AS "courriel",
-        SUBSTRING(garages.telephone FROM '\+?\d[\d\.\-\s]*\d')                                      AS "telephone",
-        CAST(NULL AS TEXT [])                                                                       AS "frais",
-        NULL                                                                                        AS "page_web"
+        END                                                                           AS "zone_diffusion_code",
+        NULL                                                                          AS "zone_diffusion_nom",
+        'departement'                                                                 AS "zone_diffusion_type",
+        CAST(NULL AS FLOAT)                                                           AS "volume_horaire_hebdomadaire",
+        CAST(NULL AS INT)                                                             AS "nombre_semaines",
+        CASE
+            WHEN garages.criteres_eligibilite IS NOT NULL
+                THEN ARRAY_REMOVE(
+                    ARRAY(
+                        SELECT (REGEXP_MATCHES(fields ->> 'Critères d''éligibilité', '^\W (.*?)( )*?$', 'gn'))[1]
+                    ),
+                    ''
+                )
+        END                                                                           AS "pre_requis",
+        NULL                                                                          AS "contact_nom_prenom",
+        garages.email                                                                 AS "courriel",
+        SUBSTRING(garages.telephone FROM '\+?\d[\d\.\-\s]*\d')                        AS "telephone",
+        CAST(NULL AS TEXT [])                                                         AS "frais",
+        NULL                                                                          AS "page_web"
     FROM garages
-    LEFT JOIN service_types_by_garage ON garages.id = service_types_by_garage.id
+    LEFT JOIN garages__services ON garages.id = garages__services.garage_id
+    LEFT JOIN garages__types_de_vehicule ON garages.id = garages__types_de_vehicule.garage_id
     WHERE
         garages.en_ligne
 )
---
+++ pipeline/dbt/models/intermediate/v1/001_mappings/mes_aides/int_mes_aides__structures_v1.sql
@@ -0,0 +1,11 @@
+{{
+    dbt_utils.union_relations(
+        relations=[
+            ref('int_mes_aides__garages__structures_v1'),
+        ],
+        column_override={
+            "reseaux_porteurs": "TEXT[]",
+        },
+        source_column_name=None
+    )
+}}
