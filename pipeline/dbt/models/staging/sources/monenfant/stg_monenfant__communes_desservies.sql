de66fef56814a1311327dc97f590dd92206a235b
Valentin Matton <taq@protonmail.ch>
2025-11-07 15:11:29 +0100

    fix(monenfant): swap post code and city name

--- pipeline/dbt/models/staging/sources/monenfant/stg_monenfant__communes_desservies.sql
+++ pipeline/dbt/models/staging/sources/monenfant/stg_monenfant__communes_desservies.sql
@@ -3,9 +3,10 @@ WITH source AS (
 
 final AS (
     SELECT
-        NULLIF(TRIM(source.data ->> 'structureId'), '')             AS "structure_id",
-        NULLIF(TRIM(communes_desservies.data ->> 'commune'), '')    AS "commune",
-        NULLIF(TRIM(communes_desservies.data ->> 'codePostal'), '') AS "code_postal"
+        NULLIF(TRIM(source.data ->> 'structureId'), '')                                                                               AS "structure_id",
+        -- FIXME: monenfant has swapped the commune and code postal fields.. simply use the fields without regex once it has been fixed at source
+        TRIM(SUBSTRING((communes_desservies.data ->> 'commune') || ' ' || (communes_desservies.data ->> 'codePostal') FROM '[^\d]+')) AS "commune",
+        SUBSTRING((communes_desservies.data ->> 'commune') || ' ' || (communes_desservies.data ->> 'codePostal') FROM '\d{5}')        AS "code_postal"
     FROM source,
         LATERAL JSONB_PATH_QUERY(source.data, '$.serviceCommun.communesDesservies[*]') AS communes_desservies (data)
 )
