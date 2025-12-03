792476c30d215930ed5fe2e4a8d7b26373384b0a
Valentin Matton <taq@protonmail.ch>
2025-12-03 15:51:19 +0100

    feat(pipeline): broaden carif oref

--- pipeline/dbt/models/staging/sources/carif_oref/_stg_carif_oref__source_filtered.sql
+++ pipeline/dbt/models/staging/sources/carif_oref/_stg_carif_oref__source_filtered.sql
@@ -1,5 +1,8 @@
 WITH source AS (
-    {{ stg_source_header('carif_oref', 'formations') }}
+    {{ stg_source_header('carif_oref', 'formations') }}),
+
+domaines AS (
+    SELECT * FROM {{ ref('stg_carif_oref__formacode_v14__domaines') }}
 ),
 
 final AS (
@@ -12,17 +15,11 @@ final AS (
             FROM JSONB_ARRAY_ELEMENTS(source.data -> 'domaine-formation' -> 'code-FORMACODE') AS codes (data)
         )
         &&
-        ARRAY[
-            -- français mise à niveau
-            -- https://formacode.centre-inffo.fr/spip.php?page=thesaurus&recherche_libre=15040
-            '15040',
-            -- alphabétisation
-            -- https://formacode.centre-inffo.fr/spip.php?page=thesaurus&recherche_libre=15043
-            '15043',
-            -- français langue étrangère
-            -- https://formacode.centre-inffo.fr/spip.php?page=thesaurus&recherche_libre=15235
-            '15235'
-        ]
+        ARRAY(
+            SELECT domaines.code
+            FROM domaines
+            WHERE domaines.selected
+        )
 )
 
 SELECT * FROM final
