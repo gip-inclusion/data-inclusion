3cde320092cdd16a920a02142439932863a5cbfc
Valentin Matton <taq@protonmail.ch>
2026-01-15 17:47:32 +0100

    feat(pipeline): add udf to convert html to markdown

---
+++ pipeline/dbt/macros/udfs/html_to_markdown/udf__html_to_markdown.sql
@@ -0,0 +1,32 @@
+{# !!! THIS FILE IS AUTO-GENERATED. DO NOT EDIT MANUALLY. !!! #}
+
+{% macro udf__html_to_markdown() %}
+
+DROP FUNCTION IF EXISTS processings.html_to_markdown;
+
+CREATE OR REPLACE FUNCTION processings.html_to_markdown(input TEXT)
+RETURNS TEXT
+AS $$
+
+import markdownify
+
+
+def html_to_markdown(content: str | None) -> str | None:
+    """Convert HTML content to Markdown format."""
+
+    if content is None or content == "":
+        return content
+
+    return markdownify.markdownify(
+        html=content,
+        bullets="*",
+        heading_style="ATX",  # use # for headings
+        strip=["img"],  # ignore images
+    )
+
+
+return html_to_markdown(input)
+
+$$ LANGUAGE plpython3u;
+
+{% endmacro %}
