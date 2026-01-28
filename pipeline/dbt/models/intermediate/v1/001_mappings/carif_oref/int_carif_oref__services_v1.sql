79e0e197cf8c647b2314be5aa97f3608bf487aed
Victor Perron <victor@iso3103.net>
2026-01-28 15:14:11 +0100

    chore(pipeline): dedup carif-oref actions
    Users did complain about way too many "services" from the CARIF OREF.
    
    We used one of the examples they gave us to narrow down what could be
    done and decided to dedcup actions of the same formations.

--- pipeline/dbt/models/intermediate/v1/001_mappings/carif_oref/int_carif_oref__services_v1.sql
+++ pipeline/dbt/models/intermediate/v1/001_mappings/carif_oref/int_carif_oref__services_v1.sql
@@ -72,52 +72,102 @@ thematiques AS (
     FROM formations
 ),
 
+codes_public_vise AS (
+    SELECT
+        numero_action,
+        ARRAY_AGG(DISTINCT code_public_vise ORDER BY code_public_vise) AS "codes_public_vise"
+    FROM actions__publics
+    GROUP BY numero_action
+),
+
+-- Formations can have many closely related actions with only a few differences, if not identical.
+-- Here we remove actions that are closely related to another action of the same formation.
+-- If all columns listed in the following DISTINCT ON match, then we consider the two actions as
+-- closely related, and we keep only one of them. More columns means more actions.
+deduplicated_actions AS (
+    SELECT DISTINCT ON (
+        actions.numero_formation,
+        actions.prix_total_ttc,
+        actions.detail_conditions_prise_en_charge,
+        actions.info_public_vise,
+        actions.modalites_recrutement,
+        actions.modalites_enseignement,
+        actions.code_perimetre_recrutement,
+        actions.duree_hebdo,
+        actions.numero_organisme_formateur,
+        codes_public_vise.codes_public_vise,
+        adresses_lieu_de_formation.ville
+    ) actions.*
+    FROM actions
+    LEFT JOIN codes_public_vise
+        ON actions.numero = codes_public_vise.numero_action
+    LEFT JOIN coordonnees AS coordonnees_lieu_de_formation
+        ON actions.hash_coordonnees_lieu_de_formation_principal = coordonnees_lieu_de_formation.hash_
+    LEFT JOIN adresses AS adresses_lieu_de_formation
+        ON coordonnees_lieu_de_formation.hash_adresse = adresses_lieu_de_formation.hash_
+    ORDER BY
+        actions.numero_formation ASC,
+        actions.prix_total_ttc ASC,
+        actions.detail_conditions_prise_en_charge ASC,
+        actions.info_public_vise ASC,
+        actions.modalites_recrutement ASC,
+        actions.modalites_enseignement ASC,
+        actions.code_perimetre_recrutement ASC,
+        actions.duree_hebdo ASC,
+        actions.numero_organisme_formateur ASC,
+        codes_public_vise.codes_public_vise ASC,
+        adresses_lieu_de_formation.ville ASC,
+
+        -- the following criteria are used to select one action among closely related actions
+        actions.date_maj DESC NULLS LAST, -- most recent action
+        actions.numero ASC -- stability if same date_maj
+),
+
 final AS (
-    SELECT DISTINCT ON (actions.numero, organismes_formateurs.numero)
-        'carif-oref'                                    AS "source",
-        COALESCE(actions.date_maj, formations.date_maj) AS "date_maj",
-        'carif-oref--' || actions.numero                AS "id",
-        'carif-oref--' || COALESCE(
-            coordonnees_lieu_de_formation.hash_adresse,
-            coordonnees_organisme_formateur.hash_adresse
-        )                                               AS "adresse_id",
-        'carif-oref--' || organismes_formateurs.numero  AS "structure_id",
-        -- NOTE(vperron): I don't RTRIM the trailing dot but there are quite a few
+    SELECT DISTINCT ON (actions.numero)
+        'carif-oref'                                                 AS "source",
+        COALESCE(actions.date_maj, formations.date_maj)              AS "date_maj",
+        'carif-oref--' || actions.numero                             AS "id",
+        'carif-oref--' || coordonnees_lieu_de_formation.hash_adresse AS "adresse_id",
+        'carif-oref--' || organismes_formateurs.numero               AS "structure_id",
         CASE
             WHEN LENGTH(formations.intitule_formation) > 150
                 THEN LEFT(formations.intitule_formation, 149) || '…'
             ELSE formations.intitule_formation
-        END                                             AS "nom",
+        END                                                          AS "nom",
         ARRAY_TO_STRING(
-            ARRAY[
-                '### Objectif de la formation' || E'\n\n' || formations.objectif_formation,
-                '### Contenu de la formation' || E'\n\n' || formations.contenu_formation
-            ],
+            ARRAY_REMOVE(
+                ARRAY[
+                    '### Objectif de la formation' || E'\n\n' || formations.objectif_formation,
+                    '### Contenu de la formation' || E'\n\n' || formations.contenu_formation
+                ],
+                NULL
+            ),
             E'\n\n'
-        )                                               AS "description",
-        FORMAT(
+        )                                                            AS "description",
+        COALESCE(formations.url_formation[1], FORMAT(
             'https://www.intercariforef.org/formations/%s/formation-%s_%s.html',
-            SLUGIFY('formations.intitule_formation'),
+            SLUGIFY(formations.intitule_formation),
             formations.numero,
             SUBSTRING(actions.numero, 4)
-        )                                               AS "lien_source",
-        'formation'                                     AS "type",
-        thematiques.thematiques                         AS "thematiques",
+        ))                                                           AS "lien_source",
+        'formation'                                                  AS "type",
+        thematiques.thematiques                                      AS "thematiques",
         CASE
             WHEN actions.prix_total_ttc = 0 THEN 'gratuit'
             WHEN actions.prix_total_ttc > 0 THEN 'payant'
-        END                                             AS "frais",
+        END                                                          AS "frais",
         CASE
             WHEN actions.detail_conditions_prise_en_charge IS NOT NULL
                 THEN FORMAT('Prise en charge: %s', actions.detail_conditions_prise_en_charge)
-        END                                             AS "frais_autres",
+        END                                                          AS "frais_autres",
         CASE
             WHEN publics.publics @> ARRAY['tous-publics']
                 THEN ARRAY['tous-publics']
             ELSE publics.publics
-        END                                             AS "publics",
-        actions.info_public_vise                        AS "publics_precisions",
-        NULLIF(actions.conditions_specifiques, '')      AS "conditions_acces",
+        END                                                          AS "publics",
+        actions.info_public_vise                                     AS "publics_precisions",
+        NULLIF(actions.conditions_specifiques, '')                   AS "conditions_acces",
         NULLIF(
             ARRAY_REMOVE(
                 ARRAY[
@@ -146,9 +196,9 @@ final AS (
                 NULL
             ),
             '{}'
-        )                                               AS "modes_mobilisation",
-        actions.modalites_recrutement                   AS "mobilisation_precisions",
-        ARRAY['professionnels']                         AS "mobilisable_par",
+        )                                                            AS "modes_mobilisation",
+        actions.modalites_recrutement                                AS "mobilisation_precisions",
+        ARRAY['professionnels']                                      AS "mobilisable_par",
         COALESCE(
             coordonnees_organisme_formateur.telfixe[1],
             coordonnees_organisme_formateur.portable[1],
@@ -156,18 +206,18 @@ final AS (
             coordonnees_lieu_de_formation.portable[1],
             coordonnees_formation.telfixe[1],
             coordonnees_formation.portable[1]
-        )                                               AS "telephone",
+        )                                                            AS "telephone",
         COALESCE(
             coordonnees_organisme_formateur.courriel,
             coordonnees_lieu_de_formation.courriel,
             coordonnees_formation.courriel
-        )                                               AS "courriel",
-        NULL                                            AS "contact_nom_prenom",
+        )                                                            AS "courriel",
+        NULL                                                         AS "contact_nom_prenom",
         CASE
             WHEN actions.modalites_enseignement = 0 THEN ARRAY['en-presentiel']
             WHEN actions.modalites_enseignement = 1 THEN ARRAY['a-distance']
             WHEN actions.modalites_enseignement = 2 THEN ARRAY['en-presentiel', 'a-distance']
-        END                                             AS "modes_accueil",
+        END                                                          AS "modes_accueil",
         CASE actions.code_perimetre_recrutement
             WHEN '1' THEN ARRAY[communes.code]
             WHEN '2' THEN ARRAY[communes.code_departement]
@@ -179,15 +229,15 @@ final AS (
                 ),
                 '{}'
             )
-        END                                             AS "zone_eligibilite",
-        CAST(actions.duree_hebdo AS FLOAT)              AS "volume_horaire_hebdomadaire",
-        CAST(NULL AS INTEGER)                           AS "nombre_semaines",
-        NULL                                            AS "horaires_accueil",
+        END                                                          AS "zone_eligibilite",
+        CAST(actions.duree_hebdo AS FLOAT)                           AS "volume_horaire_hebdomadaire",
+        CAST(NULL AS INTEGER)                                        AS "nombre_semaines",
+        NULL                                                         AS "horaires_accueil",
         JSONB_BUILD_OBJECT(
             'formation', formations.raw,
             'action', actions.raw
-        )                                               AS "_extra"
-    FROM actions
+        )                                                            AS "_extra"
+    FROM deduplicated_actions AS actions
     INNER JOIN formations
         ON actions.numero_formation = formations.numero
     LEFT JOIN organismes_formateurs
@@ -223,7 +273,6 @@ final AS (
         ON formations.numero = thematiques.numero_formation
     ORDER BY
         actions.numero ASC,
-        organismes_formateurs.numero ASC,
         organismes_formateurs__contacts.type_contact = 3 DESC, -- référent pédagogique
         organismes_formateurs__contacts.type_contact = 0 DESC, -- autre
         organismes_formateurs__contacts.type_contact = 4 DESC -- accueil
