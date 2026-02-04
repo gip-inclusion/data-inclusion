abf93ab4859129b487626376b08065c7f3645990
Valentin Matton <taq@protonmail.ch>
2026-02-04 19:54:35 +0100

    chore(api): load with swap

--- api/src/data_inclusion/api/inclusion_data/v1/commands.py
+++ api/src/data_inclusion/api/inclusion_data/v1/commands.py
@@ -92,54 +92,115 @@ def prepare_dataset(
         score_qualite=structures_df["id"].map(service_scores).astype(float).fillna(0.0)
     )
 
+    if "_extra" in services_df.columns:
+        services_df = services_df.assign(
+            extra=services_df["_extra"].apply(
+                lambda x: json.loads(x) if x is not None else None
+            )
+        )
+
     return structures_df, services_df
 
 
-def load_df_to_table(
-    db_session,
-    df: pd.DataFrame,
-    model,
-):
-    """Replace a table with a DataFrame.
+setup_sql = """
+    DROP SCHEMA IF EXISTS load_tmp CASCADE;
+    DROP SCHEMA IF EXISTS load_old CASCADE;
 
-    Does not commit the transaction, caller must do it.
-    """
-    columns_list = [
-        c
-        for c in sorted(model.__table__.columns, key=lambda c: c.name)
-        # ignore server computed columns
-        if c.server_default is None
-        and c.name not in ["cluster_best_duplicate", "doublons"]
-    ]
+    CREATE SCHEMA load_tmp;
+    CREATE SCHEMA load_old;
 
-    if "_extra" in df.columns:
-        df = df.assign(
-            extra=df["_extra"].apply(lambda x: json.loads(x) if x is not None else None)
-        )
+    CREATE TABLE load_tmp.api__structures_v1 (LIKE public.api__structures_v1 INCLUDING ALL);
+    CREATE TABLE load_tmp.api__services_v1   (LIKE public.api__services_v1   INCLUDING ALL);
+"""  # noqa: E501
 
-    df = df.sort_values(by="id", ascending=True)
-    df = df[[c.name for c in columns_list]]
+swap_sql = """
+    -- prevent this transaction from lasting too long
+    -- it will fail rather than blocking other transactions
+    SET LOCAL lock_timeout = '5s';
 
-    df.to_sql(
-        name=model.__tablename__,
-        con=db_session.connection(),
-        if_exists="append",
-        index=False,
-        dtype={column.name: column.type for column in columns_list},
-    )
+    -- drop foreign keys that point to one of the old tables
+    ALTER TABLE public.api__services_v1
+        DROP CONSTRAINT fk_api__services_v1__structure_id__api__structures_v1;
+
+    -- swap both structures and services tables
+    -- using SET SCHEMA over RENAME in order to keep indexes names consistent
+    -- with the sqlalchemy models managed by alembic
+    ALTER TABLE public.api__structures_v1      SET SCHEMA load_old;
+    ALTER TABLE public.api__services_v1        SET SCHEMA load_old;
+    ALTER TABLE load_tmp.api__structures_v1    SET SCHEMA public;
+    ALTER TABLE load_tmp.api__services_v1      SET SCHEMA public;
+
+    -- recreate foreign keys that point to the new tables
+    ALTER TABLE public.api__services_v1
+        ADD CONSTRAINT fk_api__services_v1__structure_id__api__structures_v1
+        FOREIGN KEY (structure_id)
+        REFERENCES public.api__structures_v1 (id)
+        ON DELETE CASCADE;
+"""
 
 
 def load_dataset(
-    db_session: orm.Session,
+    engine: sqla.Engine,  # manage transactions manually
     structures_df: pd.DataFrame,
     services_df: pd.DataFrame,
 ):
-    db_session.execute(sqla.delete(models.Service))
-    db_session.execute(sqla.delete(models.Structure))
-    load_df_to_table(db_session, structures_df, models.Structure)
-    load_df_to_table(db_session, services_df, models.Service)
+    logger.info("Setting up staging tables...")
+    with engine.connect() as conn:
+        conn.execute(sqla.text(setup_sql))
+        conn.commit()
 
-    db_session.commit()
+    logger.info("Loading data into staging tables...")
+    with engine.connect() as conn:
+        for model, df in [
+            (models.Structure, structures_df),
+            (models.Service, services_df),
+        ]:
+            # compute the list of columns to insert
+            # ignoring server computed columns
+            columns_list = [
+                c
+                for c in model.__table__.columns
+                if c.server_default is None
+                and c.name not in ["cluster_best_duplicate", "doublons"]
+            ]
+
+            df = df.sort_values(by="id", ascending=True)
+            df = df[[c.name for c in columns_list]]
+
+            df.to_sql(
+                name=model.__tablename__,
+                schema="load_tmp",
+                con=conn,
+                if_exists="append",
+                index=False,
+                dtype={column.name: column.type for column in columns_list},
+            )
+        conn.commit()
+
+    # Analyze before swapping to have accurate statistics right away
+    logger.info("Analyzing staging tables...")
+    with engine.connect().execution_options(isolation_level="AUTOCOMMIT") as conn:
+        conn.execute(sqla.text("ANALYZE load_tmp.api__structures_v1"))
+        conn.execute(sqla.text("ANALYZE load_tmp.api__services_v1"))
+
+    # The following requires an exclusive lock (because of the swap).
+    # Therefore it SHOULD stay minimal. Any operations that can be
+    # done without an exclusive lock should be done in other transactions
+    # to prevent locking everything for too long.
+    logger.info("Swapping staging tables with production tables...")
+    with engine.connect() as conn:
+        conn.execute(sqla.text(swap_sql))
+        conn.commit()
+
+    with engine.connect() as conn:
+        structures_count = conn.execute(
+            sqla.select(sqla.func.count()).select_from(models.Structure)
+        )
+        services_count = conn.execute(
+            sqla.select(sqla.func.count()).select_from(models.Service)
+        )
+        logger.info(f"{structures_count.scalar()} structures loaded.")
+        logger.info(f"{services_count.scalar()} services loaded.")
 
 
 def load_inclusion_data(db_session: orm.Session, path: Path):
@@ -161,9 +222,8 @@ def load_inclusion_data(db_session: orm.Session, path: Path):
         services_df=services_df,
     )
 
-    logger.info("Loading data...")
     load_dataset(
-        db_session=db_session,
+        engine=db_session.get_bind().engine,
         structures_df=structures_df,
         services_df=services_df,
     )
