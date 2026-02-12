c073d04cbeedc3713d45eb42f24720396f3ca398
Valentin Matton <taq@protonmail.ch>
2026-02-12 11:34:56 +0100

    fix(pipeline): keep dora adresses for now

--- pipeline/dbt/models/intermediate/v1/003_enrichments/int__adresses_v1.sql
+++ pipeline/dbt/models/intermediate/v1/003_enrichments/int__adresses_v1.sql
@@ -31,14 +31,41 @@ adresses_geocodees AS (
     WHERE geocodages.score > 0.75
 ),
 
+adresses_originales AS (
+    SELECT
+        adresses.id                 AS "id",
+        adresses.complement_adresse AS "complement_adresse",
+        adresses.adresse            AS "adresse",
+        adresses.longitude          AS "longitude",
+        adresses.latitude           AS "latitude",
+        adresses.commune            AS "commune",
+        adresses.code_postal        AS "code_postal",
+        adresses.code_insee         AS "code_insee",
+        CAST(NULL AS FLOAT)         AS "score_geocodage"
+    FROM adresses
+    WHERE SPLIT_PART(adresses.id, '--', 1) = 'dora'
+),
+
+adresses_finales AS (
+    SELECT DISTINCT ON (id) *
+    FROM (
+        SELECT * FROM adresses_geocodees
+        UNION ALL
+        SELECT * FROM adresses_originales
+    )
+    ORDER BY
+        id ASC,
+        score_geocodage DESC NULLS LAST
+),
+
 final AS (
     SELECT
-        adresses_geocodees.*,
+        adresses_finales.*,
         communes.code_departement,
         communes.code_region,
         communes.code_epci
-    FROM adresses_geocodees
-    LEFT JOIN communes ON adresses_geocodees.code_insee = communes.code
+    FROM adresses_finales
+    LEFT JOIN communes ON adresses_finales.code_insee = communes.code
 )
 
 SELECT * FROM final
