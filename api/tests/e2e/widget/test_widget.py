0090def19786730c1fde20c3f40c70d3c697ef78
Valentin Matton <taq@protonmail.ch>
2026-03-11 14:56:53 +0100

    ci(api): skip playwright tests

--- api/tests/e2e/widget/test_widget.py
+++ api/tests/e2e/widget/test_widget.py
@@ -1,10 +1,8 @@
-import os
-
 import pytest
 from playwright import sync_api as pw
 
 
-@pytest.mark.skipif(os.getenv("CI") == "1", reason="Skipping test in CI")
+@pytest.mark.playwright
 def test_widget(page: pw.Page):
     """Necessite un serveur de test comportant des données sur Lille
     en cours d'exécution avec TOKEN_ENABLED = False
