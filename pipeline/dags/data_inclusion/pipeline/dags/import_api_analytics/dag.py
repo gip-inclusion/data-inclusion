a03d2d5caa4a23d527e47c359ead19b03fd82c65
Victor Perron <victor@iso3103.net>
2026-03-16 15:50:19 +0100

    fix(pipeline): fix forever hanging bug in local dev
    I lost some life points on this :'(

--- pipeline/dags/data_inclusion/pipeline/dags/import_api_analytics/dag.py
+++ pipeline/dags/data_inclusion/pipeline/dags/import_api_analytics/dag.py
@@ -10,20 +10,36 @@ from data_inclusion.pipeline.common import dags
     retries=1,
 )
 def import_data():
+    import json
+    import os
     import subprocess
     import tempfile
     from pathlib import Path
 
-    from airflow.providers.amazon.aws.fs import s3 as s3fs
-    from airflow.providers.amazon.aws.hooks import s3
-    from airflow.sdk import Connection
+    import s3fs
 
-    pg_conn = Connection.get(conn_id="pg")
-    s3_hook = s3.S3Hook(aws_conn_id="s3")
+    # https://github.com/apache/airflow/issues/51422#issuecomment-4068304878
+    # This bug is still active :'(
+    # But it does not crash, it HANGS.
+    # This is because we're using task.virtualenv
+    # - In production it works because FabAuthManager loads the entire airflow
+    #   vars and secrets.
+    # - run_main_pipeline uses task.python which uses the airflow environment and
+    #   also has access to vars and secrets.
+    # - import_api_analytics uses task.virtualenv which does not have access to
+    #   vars and secrets.
+    pg_uri = os.environ["AIRFLOW_CONN_PG"]
+    s3_conn = json.loads(os.environ["AIRFLOW_CONN_S3"])
+    s3_extra = s3_conn["extra"]
+    s3_bucket = s3_extra["service_config"]["s3"]["bucket_name"]
 
-    s3fs_client = s3fs.get_fs(conn_id="s3")
+    s3fs_client = s3fs.S3FileSystem(
+        endpoint_url=s3_extra["endpoint_url"],
+        key=s3_extra["aws_access_key_id"],
+        secret=s3_extra["aws_secret_access_key"],
+    )
 
-    BASE_KEY = Path(s3_hook.service_config["bucket_name"]) / "data" / "api"
+    BASE_KEY = Path(s3_bucket) / "data" / "api"
     value = sorted(s3fs_client.ls(BASE_KEY))[-1]  # latest day
     value = sorted(s3fs_client.ls(value))[-1]  # latest run
     value = Path(value) / "analytics.dump"
@@ -35,7 +51,7 @@ def import_data():
 
         command = (
             "pg_restore"
-            f" --dbname={pg_conn.get_uri()}"
+            f" --dbname={pg_uri}"
             " --clean"
             " --if-exists"
             " --no-owner"
@@ -44,7 +60,10 @@ def import_data():
         )
 
         try:
-            print(command.replace(pg_conn.password, "***"))
+            from urllib.parse import urlparse
+
+            parsed = urlparse(pg_uri)
+            print(command.replace(parsed.password or "", "***"))
             subprocess.run(command, shell=True, check=True, capture_output=True)
         except subprocess.CalledProcessError as exc:
             print(exc.stdout)
