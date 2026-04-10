8e99a06cfbbdd4daa9c45f526cdc12085fbcc4b5
Victor Perron <victor@iso3103.net>
2026-04-10 12:02:04 +0200

    chore(pipeline): fix non-empty test in reseau alpha
    A test was blowing up because the description was an empty string.
    Because the process_html_to_markdown() did return '' for <p></p>
    
    And the NULLIF was not applied at the right time

--- pipeline/dbt/models/staging/sources/reseau_alpha/stg_reseau_alpha__formations.sql
+++ pipeline/dbt/models/staging/sources/reseau_alpha/stg_reseau_alpha__formations.sql
@@ -4,30 +4,30 @@ WITH source AS (
 
 final AS (
     SELECT
-        NULLIF(TRIM(source.data ->> 'id'), '')                                   AS "structure_id",
-        NULLIF(TRIM(formations.data ->> 'id'), '')                               AS "id",
-        NULLIF(TRIM(formations.data ->> 'nom'), '')                              AS "nom",
-        NULLIF(TRIM(formations.data ->> 'activite'), '')                         AS "activite",
-        NULLIF(TRIM(formations.data -> 'contact' ->> 'nom'), '')                 AS "contact__nom",
-        NULLIF(TRIM(formations.data -> 'contact' ->> 'prenom'), '')              AS "contact__prenom",
-        NULLIF(TRIM(formations.data -> 'contact' ->> 'email'), '')               AS "contact__email",
-        NULLIF(TRIM(formations.data -> 'contact' ->> 'telephone1'), '')          AS "contact__telephone1",
-        NULLIF(TRIM(formations.data -> 'contact' ->> 'telephone2'), '')          AS "contact__telephone2",
-        CAST(formations.data ->> 'coursEte' AS BOOLEAN)                          AS "cours_ete",
+        NULLIF(TRIM(source.data ->> 'id'), '')                                                                       AS "structure_id",
+        NULLIF(TRIM(formations.data ->> 'id'), '')                                                                   AS "id",
+        NULLIF(TRIM(formations.data ->> 'nom'), '')                                                                  AS "nom",
+        NULLIF(TRIM(formations.data ->> 'activite'), '')                                                             AS "activite",
+        NULLIF(TRIM(formations.data -> 'contact' ->> 'nom'), '')                                                     AS "contact__nom",
+        NULLIF(TRIM(formations.data -> 'contact' ->> 'prenom'), '')                                                  AS "contact__prenom",
+        NULLIF(TRIM(formations.data -> 'contact' ->> 'email'), '')                                                   AS "contact__email",
+        NULLIF(TRIM(formations.data -> 'contact' ->> 'telephone1'), '')                                              AS "contact__telephone1",
+        NULLIF(TRIM(formations.data -> 'contact' ->> 'telephone2'), '')                                              AS "contact__telephone2",
+        CAST(formations.data ->> 'coursEte' AS BOOLEAN)                                                              AS "cours_ete",
         CASE LOWER(NULLIF(TRIM(formations.data ->> 'cout'), ''))
             WHEN '0' THEN 'gratuit'
             WHEN '0€' THEN 'gratuit'
             ELSE LOWER(NULLIF(TRIM(formations.data ->> 'cout'), ''))
-        END                                                                      AS "cout",
-        NULLIF(TRIM(formations.data ->> 'criteresSavoirBase'), '')               AS "criteres_savoir_base",
-        TO_DATE(formations.data ->> 'dateDebut', 'YYYY-MM-DD')                   AS "date_debut",
-        TO_DATE(formations.data ->> 'dateFin', 'YYYY-MM-DD')                     AS "date_fin",
-        CAST(formations.data ->> 'gardeEnfants' AS BOOLEAN)                      AS "garde_enfants",
-        CAST(formations.data ->> 'placeDisponible' AS BOOLEAN)                   AS "place_disponible",
-        processings.html_to_markdown(formations.data ->> 'presentationPublique') AS "presentation_publique",
-        NULLIF(TRIM(formations.data ->> 'slug'), '')                             AS "slug",
-        NULLIF(TRIM(formations.data ->> 'url'), '')                              AS "url",
-        formations.data -> 'joursHorairesDetails'                                AS "jours_horaires_details"
+        END                                                                                                          AS "cout",
+        NULLIF(TRIM(formations.data ->> 'criteresSavoirBase'), '')                                                   AS "criteres_savoir_base",
+        TO_DATE(formations.data ->> 'dateDebut', 'YYYY-MM-DD')                                                       AS "date_debut",
+        TO_DATE(formations.data ->> 'dateFin', 'YYYY-MM-DD')                                                         AS "date_fin",
+        CAST(formations.data ->> 'gardeEnfants' AS BOOLEAN)                                                          AS "garde_enfants",
+        CAST(formations.data ->> 'placeDisponible' AS BOOLEAN)                                                       AS "place_disponible",
+        NULLIF(TRIM(processings.html_to_markdown(NULLIF(TRIM(formations.data ->> 'presentationPublique'), ''))), '') AS "presentation_publique",
+        NULLIF(TRIM(formations.data ->> 'slug'), '')                                                                 AS "slug",
+        NULLIF(TRIM(formations.data ->> 'url'), '')                                                                  AS "url",
+        formations.data -> 'joursHorairesDetails'                                                                    AS "jours_horaires_details"
     FROM
         source,
         JSONB_PATH_QUERY(source.data, '$.formations[*]') AS formations (data)
--- pipeline/dbt/models/staging/sources/reseau_alpha/stg_reseau_alpha__structures.sql
+++ pipeline/dbt/models/staging/sources/reseau_alpha/stg_reseau_alpha__structures.sql
@@ -4,16 +4,16 @@ WITH source AS (
 
 final AS (
     SELECT
-        NULLIF(TRIM(data ->> 'id'), '')                                        AS "id",
-        NULLIF(TRIM(data ->> 'acronyme'), '')                                  AS "acronyme",
-        processings.html_to_markdown(NULLIF(TRIM(data ->> 'description'), '')) AS "description",
-        CAST(data ->> 'estCoordination' AS BOOLEAN)                            AS "est_coordination",
-        NULLIF(TRIM(data ->> 'logo'), '')                                      AS "logo",
-        NULLIF(TRIM(data ->> 'nom'), '')                                       AS "nom",
-        NULLIF(TRIM(data ->> 'slug'), '')                                      AS "slug",
-        NULLIF(TRIM(data ->> 'type'), '')                                      AS "type",
-        NULLIF(TRIM(data ->> 'url'), '')                                       AS "url",
-        NULLIF(TRIM(data ->> 'urlCoordination'), '')                           AS "url_coordination"
+        NULLIF(TRIM(data ->> 'id'), '')                                                          AS "id",
+        NULLIF(TRIM(data ->> 'acronyme'), '')                                                    AS "acronyme",
+        NULLIF(TRIM(processings.html_to_markdown(NULLIF(TRIM(data ->> 'description'), ''))), '') AS "description",
+        CAST(data ->> 'estCoordination' AS BOOLEAN)                                              AS "est_coordination",
+        NULLIF(TRIM(data ->> 'logo'), '')                                                        AS "logo",
+        NULLIF(TRIM(data ->> 'nom'), '')                                                         AS "nom",
+        NULLIF(TRIM(data ->> 'slug'), '')                                                        AS "slug",
+        NULLIF(TRIM(data ->> 'type'), '')                                                        AS "type",
+        NULLIF(TRIM(data ->> 'url'), '')                                                         AS "url",
+        NULLIF(TRIM(data ->> 'urlCoordination'), '')                                             AS "url_coordination"
     FROM source
 )
 
