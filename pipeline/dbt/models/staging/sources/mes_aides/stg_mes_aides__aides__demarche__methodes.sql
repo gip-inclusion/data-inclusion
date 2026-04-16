b004ecdbba462b8d3ab0b311f20506feedbd2aa9
Valentin Matton <taq@protonmail.ch>
2026-04-16 15:45:05 +0200

    chore(pipeline): update mes aides aides

---
+++ pipeline/dbt/models/staging/sources/mes_aides/stg_mes_aides__aides__demarche__methodes.sql
@@ -0,0 +1,11 @@
+WITH source AS (
+    {{ stg_source_header('mes_aides', 'aides') }}),
+
+final AS (
+    SELECT
+        data ->> 'id'                                              AS "aide_id",
+        JSONB_ARRAY_ELEMENTS_TEXT(data -> 'demarche' -> 'methode') AS "value"
+    FROM source
+)
+
+SELECT * FROM final
