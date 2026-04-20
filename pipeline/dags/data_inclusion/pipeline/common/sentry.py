505e0e152dcb3f57a84def9b206f88fe06c5efa6
Valentin Matton <taq@protonmail.ch>
2026-04-20 11:13:25 +0200

    Revert "chore(pipeline): bump to Airflow 3.2, py3.13"
    This reverts commit 7ed8e65e130096e9307a8a103f4cd4c1992cd0da.

--- pipeline/dags/data_inclusion/pipeline/common/sentry.py
+++ pipeline/dags/data_inclusion/pipeline/common/sentry.py
@@ -1,6 +1,6 @@
 import os
 
-import sentry_sdk
+from sentry_sdk import configure_scope
 
 
 def before_send(event, hint):
@@ -28,21 +28,20 @@ def before_send(event, hint):
 def fill_sentry_scope(context):
     github_base_url = "https://github.com/gip-inclusion/data-inclusion"
 
-    dag_id = context.get("dag").dag_id
-    task_instance = context.get("task_instance")
-    task_id = task_instance.task_id
-    commit_sha = os.getenv("AIRFLOW__SENTRY__RELEASE", None)
-
-    scope = sentry_sdk.get_current_scope()
-    scope.set_tag("dag_id", dag_id)
-    scope.set_tag("task_id", task_id)
-    # Fine-tune the fingerprint to force de-grouping issues if not from
-    # the same DAG
-    scope.fingerprint = ["{{ default }}", dag_id]
-    scope.set_context(
-        "custom",
-        {
-            "airflow_logs_url": task_instance.log_url,
-            "github_commit_url": f"{github_base_url}/commit/{commit_sha}",
-        },
-    )
+    with configure_scope() as scope:
+        dag_id = context.get("dag").dag_id
+        task_id = context.get("task_instance").task_id
+        task_instance = context.get("task_instance")
+        commit_sha = os.getenv("AIRFLOW__SENTRY__RELEASE", None)
+        scope.set_tag("dag_id", dag_id)
+        scope.set_tag("task_id", task_id)
+        # Fine-tune the fingerprint to force de-grouping issues if not from
+        # the same DAG
+        scope.fingerprint = ["{{ default }}", dag_id]
+        scope.set_context(
+            "custom",
+            {
+                "airflow_logs_url": task_instance.log_url,
+                "github_commit_url": f"{github_base_url}/commit/{commit_sha}",
+            },
+        )
