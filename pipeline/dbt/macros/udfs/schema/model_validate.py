3d125fc5dfe4027ee563cda185b96a282213b159
Valentin Matton <taq@protonmail.ch>
2026-05-22 09:51:08 +0200

    fix(pipeline): remove possible arbitrary code execution

--- pipeline/dbt/macros/udfs/schema/model_validate.py
+++ pipeline/dbt/macros/udfs/schema/model_validate.py
@@ -1,23 +1,10 @@
-{% macro udf__model_validate() %}
-
-DROP FUNCTION IF EXISTS processings.model_validate;
-
-CREATE OR REPLACE FUNCTION processings.model_validate(model_class TEXT, data JSONB)
-RETURNS
-    TABLE(
-        model_class TEXT,
-        type TEXT,
-        loc TEXT,
-        msg TEXT,
-        input JSONB
-    )
-AS $$
-
-import importlib
 import json
-from typing import TypedDict
+from typing import Literal, TypedDict
 
 import pydantic
+
+from data_inclusion import schema
+
 
 class ValidationErrorDict(TypedDict):
     model_class: str
@@ -26,7 +13,14 @@ class ValidationErrorDict(TypedDict):
     msg: str
     input: str
 
-def model_validate(model_class: str, data: str) -> ValidationErrorDict:
+
+def model_validate(
+    model: Literal[
+        "data_inclusion.schema.v1.Structure",
+        "data_inclusion.schema.v1.Service",
+    ],
+    data: str,
+) -> list[ValidationErrorDict]:
     """Validate the input data against the Pydantic model.
 
     Args:
@@ -37,28 +31,24 @@ def model_validate(model_class: str, data: str) -> ValidationErrorDict:
         ValidationErrorDict: A dictionary containing validation error details,
         or an empty dictionary if validation is successful.
     """
-    module_path, class_name = model_class.rsplit('.', 1)
-    module = importlib.import_module(module_path)
-    model = getattr(module, class_name)
+
+    pydantic_model = {
+        "data_inclusion.schema.v1.Structure": schema.v1.Structure,
+        "data_inclusion.schema.v1.Service": schema.v1.Service,
+    }[model]
 
     try:
-        model.model_validate_json(data)
+        pydantic_model.model_validate_json(data)
     except pydantic.ValidationError as exc:
         return [
             {
-                "model_class": model_class,
+                "model_class": model,
                 "type": error["type"],
                 "loc": ".".join(map(str, error["loc"])),
                 "msg": error["msg"],
-                "input": json.dumps(error["input"])
+                "input": json.dumps(error["input"]),
             }
             for error in exc.errors(include_url=False)
         ]
 
     return []
-
-return model_validate(model_class, data)
-
-$$ LANGUAGE plpython3u;
-
-{% endmacro %}
---
+++ pipeline/dbt/macros/udfs/schema/udf__model_validate.sql
@@ -0,0 +1,78 @@
+{# !!! THIS FILE IS AUTO-GENERATED. DO NOT EDIT MANUALLY. !!! #}
+
+{% macro udf__model_validate() %}
+
+DROP FUNCTION IF EXISTS processings.model_validate;
+
+CREATE OR REPLACE FUNCTION processings.model_validate(model_class TEXT, data JSONB)
+RETURNS
+    TABLE(
+        model_class TEXT,
+        type TEXT,
+        loc TEXT,
+        msg TEXT,
+        input JSONB
+    )
+AS $$
+
+import json
+from typing import Literal, TypedDict
+
+import pydantic
+
+from data_inclusion import schema
+
+
+class ValidationErrorDict(TypedDict):
+    model_class: str
+    type: str
+    loc: str
+    msg: str
+    input: str
+
+
+def model_validate(
+    model: Literal[
+        "data_inclusion.schema.v1.Structure",
+        "data_inclusion.schema.v1.Service",
+    ],
+    data: str,
+) -> list[ValidationErrorDict]:
+    """Validate the input data against the Pydantic model.
+
+    Args:
+        model_class (str): The fully qualified name of the Pydantic model class.
+        data (str): The input data to validate, as a JSON string.
+
+    Returns:
+        ValidationErrorDict: A dictionary containing validation error details,
+        or an empty dictionary if validation is successful.
+    """
+
+    pydantic_model = {
+        "data_inclusion.schema.v1.Structure": schema.v1.Structure,
+        "data_inclusion.schema.v1.Service": schema.v1.Service,
+    }[model]
+
+    try:
+        pydantic_model.model_validate_json(data)
+    except pydantic.ValidationError as exc:
+        return [
+            {
+                "model_class": model,
+                "type": error["type"],
+                "loc": ".".join(map(str, error["loc"])),
+                "msg": error["msg"],
+                "input": json.dumps(error["input"]),
+            }
+            for error in exc.errors(include_url=False)
+        ]
+
+    return []
+
+
+return model_validate(model=model_class, data=data)
+
+$$ LANGUAGE plpython3u;
+
+{% endmacro %}
